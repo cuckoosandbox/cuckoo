97844bb8742365b83f881349fc926f37f9293339
jekil <alessandro@tanasi.it>
2012-05-11 00:32:19 +0200

    Added exception handling in json module

--- modules/reporting/jsondump.py
+++ modules/reporting/jsondump.py
@@ -5,6 +5,12 @@ from lib.cuckoo.common.abstracts import Report
 
 class JsonDump(Report):
     def run(self, results):
-        report = open(os.path.join(self.reports_path, "report.json"), "w")
-        report.write(json.dumps(results, sort_keys=False, indent=4))
-        report.close()
+        try:
+            report = open(os.path.join(self.reports_path, "report.json"), "w")
+            report.write(json.dumps(results, sort_keys=False, indent=4))
+            report.close()
+        except TypeError, why:
+            print "Failed to create JSON: %s" % why
+        except IOError, why:
+            print "Failed writing JSON report: %s" % why
+
