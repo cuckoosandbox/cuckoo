ba9d51cd5d2c28ff8677965f62bc8dea8750b25c
Nex <nex@cuckoobox.org>
2012-05-12 11:40:44 +0200

    Redirected VBoxManage stdout to pipe

--- modules/machinemanagers/virtualbox.py
+++ modules/machinemanagers/virtualbox.py
@@ -7,11 +7,11 @@ from lib.cuckoo.common.abstracts import Dictionary, MachineManager
 class VirtualBox(MachineManager):
     def start(self, label):
         if self.config.getboolean("virtualbox", "headless"):
-            subprocess.call(["VBoxHeadless", "-startvm", label])
+            subprocess.call(["VBoxHeadless", "-startvm", label], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         else:
-            subprocess.call(["VBoxManage", "startvm", label])
+            subprocess.call(["VBoxManage", "startvm", label], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
     def stop(self, label):
-        subprocess.call(["VBoxManage", "controlvm", label, "poweroff"])
+        subprocess.call(["VBoxManage", "controlvm", label, "poweroff"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         time.sleep(3)
-        subprocess.call(["VBoxManage", "snapshot", label, "restorecurrent"])
+        subprocess.call(["VBoxManage", "snapshot", label, "restorecurrent"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
