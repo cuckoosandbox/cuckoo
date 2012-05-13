4a499c5acf4e7113513b5d05bc20b2fb54db6fd5
jekil <alessandro@tanasi.it>
2012-05-13 18:28:52 +0200

    Added error handling

--- modules/machinemanagers/virtualbox.py
+++ modules/machinemanagers/virtualbox.py
@@ -3,19 +3,42 @@ import time
 import subprocess
 
 from lib.cuckoo.common.abstracts import Dictionary, MachineManager
+from lib.cuckoo.common.exceptions import CuckooMachineError
 
 class VirtualBox(MachineManager):
     def start(self, label):
         if self.config.getboolean("virtualbox", "headless"):
-            subprocess.call(["VBoxHeadless", "-startvm", label],
-                            stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            try:
+                if subprocess.call(["VBoxHeadless", "-startvm", label],
+                                   stdout=subprocess.PIPE,
+                                   stderr=subprocess.PIPE):
+                    raise CuckooMachineError("VBoxHeadless exited with error starting vm")
+            except OSError:
+                raise CuckooMachineError("VBoxHeadless OS error starting vm or file not found")
         else:
-            subprocess.call(["VBoxManage", "startvm", label],
-                            stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            try:
+                if subprocess.call(["VBoxManage", "startvm", label],
+                                   stdout=subprocess.PIPE,
+                                   stderr=subprocess.PIPE):
+                    raise CuckooMachineError("VBoxManage exited with error starting vm")
+            except OSError:
+                raise CuckooMachineError("VBoxManage OS error starting vm or file not found")
 
     def stop(self, label):
-        subprocess.call(["VBoxManage", "controlvm", label, "poweroff"],
-                        stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        try:
+            if subprocess.call(["VBoxManage", "controlvm", label, "poweroff"],
+                               stdout=subprocess.PIPE,
+                               stderr=subprocess.PIPE):
+                raise CuckooMachineError("VBoxManage exited with error powering off vm")
+        except OSError:
+            raise CuckooMachineError("VBoxManage OS error powering off vm or file not found")
+
         time.sleep(3)
-        subprocess.call(["VBoxManage", "snapshot", label, "restorecurrent"],
-                        stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+        try:
+            if subprocess.call(["VBoxManage", "snapshot", label, "restorecurrent"],
+                        stdout=subprocess.PIPE,
+                        stderr=subprocess.PIPE):
+                raise CuckooMachineError("VBoxManage exited with error restoging vm's snapshot")
+        except OSError:
+            raise CuckooMachineError("VBoxManage OS error restoring vm's snapshot or file not found")
