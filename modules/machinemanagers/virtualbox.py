f094c87592fa6412d7ca2d4599536ccef0921387
jekil <alessandro@tanasi.it>
2012-05-13 13:49:37 +0200

    PEP  little fix for 80 columns

--- modules/machinemanagers/virtualbox.py
+++ modules/machinemanagers/virtualbox.py
@@ -7,11 +7,15 @@ from lib.cuckoo.common.abstracts import Dictionary, MachineManager
 class VirtualBox(MachineManager):
     def start(self, label):
         if self.config.getboolean("virtualbox", "headless"):
-            subprocess.call(["VBoxHeadless", "-startvm", label], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            subprocess.call(["VBoxHeadless", "-startvm", label],
+                            stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         else:
-            subprocess.call(["VBoxManage", "startvm", label], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            subprocess.call(["VBoxManage", "startvm", label],
+                            stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
     def stop(self, label):
-        subprocess.call(["VBoxManage", "controlvm", label, "poweroff"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        subprocess.call(["VBoxManage", "controlvm", label, "poweroff"],
+                        stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         time.sleep(3)
-        subprocess.call(["VBoxManage", "snapshot", label, "restorecurrent"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        subprocess.call(["VBoxManage", "snapshot", label, "restorecurrent"],
+                        stdout=subprocess.PIPE, stderr=subprocess.PIPE)
