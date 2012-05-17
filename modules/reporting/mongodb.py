1ab56edf32ab32f07b1c42375cfc5ff45999c6d9
jekil <alessandro@tanasi.it>
2012-05-17 23:55:00 +0200

    Added initial MongoDB reporting module

---
+++ modules/reporting/mongodb.py
@@ -0,0 +1,61 @@
+import os
+
+from lib.cuckoo.common.abstracts import Report
+from lib.cuckoo.common.exceptions import CuckooDependencyError, CuckooReportError
+
+try:
+    from pymongo.connection import Connection
+    from pymongo.errors import ConnectionFailure
+    from gridfs import GridFS
+except ImportError:
+    raise CuckooDependencyError("Unable to import pymongo")
+
+
+class MongoDb(Report):
+    """
+    Save report in MongoDB.
+    """
+    
+    def run(self, results):
+        # Connect.
+        self._connect()
+        # Set db.
+        db = self._conn.cuckoo
+        fs = GridFS(db)
+        # Add pcap.
+        pcap_file = os.path.join(self.analysis_path, "dump.pcap")
+        if os.path.exists(pcap_file) and os.path.getsize(pcap_file) != 0:
+            pcap = open(pcap_file, 'r')
+            pcap_id = fs.put(pcap, filename="dump.pcap")
+            results["network"] = {"pcap_id": pcap_id}
+        # Add dropped files
+        if results.has_key("dropped"):
+            for dropped in results["dropped"]:
+                if dropped.has_key("name"):
+                    drop_file = os.path.join(self.analysis_path,
+                                             "files",
+                                             dropped["name"])
+                    if os.path.exists(drop_file) and os.path.getsize(drop_file) != 0:
+                        drop = open(drop_file, 'r')
+                        drop_id = fs.put(drop, filename=dropped["name"])
+                        dropped["dropped_id"] = drop_id
+        # Save results. 
+        db.analysis.save(results)
+
+    def _connect(self):
+        # Set connection parameters.
+        if self.options.has_key("host"):
+            host = self.options["host"]
+        else:
+            host = "localhost"
+        if self.options.has_key("port"):
+            port = self.options["port"]
+        else:
+            port = 27017
+        # Connect.
+        try:
+            self._conn = Connection(host, port)
+        except TypeError:
+            raise CuckooReportError("Mongo connection port must be integer")
+        except ConnectionFailure:
+            raise CuckooReportError("Cannot connect to MongoDB")
