deea1285beb0abdd013d39ac35c4d48628ac5cde
Nex <nex@cuckoobox.org>
2012-05-19 23:39:07 +0200

    Added network analysis module

--- lib/cuckoo/common/abstracts.py
+++ lib/cuckoo/common/abstracts.py
@@ -74,6 +74,7 @@ class Processing(object):
         self.file_path = os.path.join(analysis_path, "binary")
         self.dropped_path = os.path.join(analysis_path, "files")
         self.logs_path = os.path.join(analysis_path, "logs")
+        self.pcap_path = os.path.join(analysis_path, "dump.pcap")
 
     def run(self):
         raise NotImplementedError
