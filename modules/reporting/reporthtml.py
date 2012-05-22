c0ec458217173b67959e46e9030a9edbe4a7e01e
Nex <nex@cuckoobox.org>
2012-05-22 14:08:41 +0200

    Fixed screenshos

--- modules/reporting/reporthtml.py
+++ modules/reporting/reporthtml.py
@@ -1,4 +1,5 @@
 import os
+import base64
 
 from lib.cuckoo.common.constants import CUCKOO_ROOT
 from lib.cuckoo.common.abstracts import Report
@@ -15,6 +16,31 @@ class ReportHTML(Report):
     def run(self, results):
         if not HAVE_MAKO:
             raise CuckooReportError("Failed to generate HTML report: python Mako library is not installed")
+
+        shots_path = os.path.join(self.analysis_path, "shots")
+        if os.path.exists(shots_path):
+            shots = []
+            counter = 1
+            for shot_name in os.listdir(shots_path):
+                if not shot_name.endswith(".jpg"):
+                    continue
+
+                shot_path = os.path.join(shots_path, shot_name)
+
+                if os.path.getsize(shot_path) == 0:
+                    continue
+
+                shot = {}
+                shot["id"] = counter
+                shot["data"] = base64.b64encode(open(shot_path, "rb").read())
+                shots.append(shot)
+
+                counter += 1
+
+            shots.sort(key=lambda shot: shot["id"])
+            results["shots"] = shots
+        else:
+            results["shots"] = []
 
         lookup = TemplateLookup(directories=[os.path.join(CUCKOO_ROOT, "lib/cuckoo/web/")],
                                 output_encoding='utf-8',
