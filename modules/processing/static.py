328a260f6f9e283d5ab8153a31e2939384879095
Nex <nex@cuckoobox.org>
2012-06-02 10:41:46 +0200

    Added data directory

--- modules/processing/static.py
+++ modules/processing/static.py
@@ -8,6 +8,7 @@ try:
 except:
     HAVE_MAGIC = False
 
+from lib.cuckoo.common.constants import CUCKOO_ROOT
 from lib.cuckoo.common.abstracts import Processing
 from lib.cuckoo.common.utils import convert_to_printable, File
 import lib.pefile.pefile as pefile
@@ -41,7 +42,7 @@ class PortableExecutable:
             return None
 
         try:
-            signatures = peutils.SignatureDatabase("lib/pefile/UserDB.TXT")
+            signatures = peutils.SignatureDatabase(os.path.join(CUCKOO_ROOT, "data/peutils/UserDB.TXT"))
             return signatures.match(self.pe, ep_only = True)
         except:
             return None
