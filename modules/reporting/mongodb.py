a931f39803bae7531af43d63430716d7eaec4d09
jekil <alessandro@tanasi.it>
2012-06-12 00:32:28 +0200

    Added screenshots to mongo db

--- modules/reporting/mongodb.py
+++ modules/reporting/mongodb.py
@@ -26,9 +26,11 @@ class MongoDb(Report):
         @raise CuckooReportError: if fails to connect or write to MongoDB.
         """
         self._connect()
+
         # Set an unique index on stored files, to avoid duplicates.
         if not self._db.fs.files.ensure_index("md5", unique=True):
             self._db.fs.files.create_index("md5", unique=True, name="md5_unique")
+
         # Add pcap file, check for dups and in case add only reference.
         pcap_file = os.path.join(self.analysis_path, "dump.pcap")
         if os.path.exists(pcap_file) and os.path.getsize(pcap_file) != 0:
@@ -42,6 +44,7 @@ class MongoDb(Report):
                 results["network"]["pcap_id"] = pcap_id
             else:
                 results["network"] = {"pcap_id": pcap_id}
+
         # Add dropped files, check for dups and in case add only reference.
         if "dropped" in results:
             for dropped in results["dropped"]:
@@ -56,6 +59,22 @@ class MongoDb(Report):
                         except FileExists as e:
                             drop_id = self._db.fs.files.find({"md5": dropped["md5"]})[0][u"_id"]
                         dropped["dropped_id"] = drop_id
+
+        # Add screenshots.
+        results["shots"] = []
+        shots = [f for f in os.listdir(os.path.join(self.analysis_path, "shots")) if f.endswith(".jpg")]
+        for shot_file in shots:
+            shot_path = os.path.join(self.analysis_path, "shots", shot_file)
+            try:
+                shot = File(shot_path)
+            except IOError as e:
+                raise CuckooReportError("Failed to read screenshot %s: %s" % (shot_path, e.message))
+
+            try:
+                shot_id = self._fs.put(shot.get_data(), filename=shot.get_name())
+            except FileExists:
+                shot_id = self._db.fs.files.find({"md5": shot.get_md5()})[0][u"_id"]
+            results["shots"].append(shot_id)
         # Save all remaining results.
         self._db.analysis.save(results)
 
