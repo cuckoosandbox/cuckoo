136811f41b404b4017a64ad03f9a3f35e8225f0f
jekil <alessandro@tanasi.it>
2012-06-17 20:39:31 +0200

    Improved concurrency handling and performance boosted for libvirt virtualization

--- modules/machinemanagers/kvm.py
+++ modules/machinemanagers/kvm.py
@@ -20,13 +20,14 @@ class Kvm(MachineManager):
         @raise CuckooMachineError: if unable to start virtual machine.
         """
         # Connect.
-        self._connect()
+        conn = self._connect()
         # Search.
-        vm = self._lookup(label)
+        vm = self._lookup(conn, label)
         # Get current snapshot.
         try:
             snap = vm.hasCurrentSnapshot(flags=0)
         except libvirt.libvirtError:
+            self._disconnect(conn)
             raise CuckooMachineError("Unable to get current snapshots for virtual machine %s" % label)
         # Revert to latest snapshot.
         if snap:
@@ -34,7 +35,10 @@ class Kvm(MachineManager):
                 vm.revertToSnapshot(vm.snapshotCurrent(flags=0), flags=0)
             except libvirt.libvirtError:
                 raise CuckooMachineError("Unable to restore snapshot on virtual machine %s" % label)
+            finally:
+                self._disconnect(conn)
         else:
+            self._disconnect(conn)
             raise CuckooMachineError("No snapshots found for virtual machine %s" % label)
 
     def stop(self, label):
@@ -43,31 +47,43 @@ class Kvm(MachineManager):
         @raise CuckooMachineError: if unable to stop virtual machine.
         """
         # Connect.
-        self._connect()
+        conn = self._connect()
         # Search.
-        vm = self._lookup(label)
+        vm = self._lookup(conn, label)
         # Force virtual machine shutdown (hardcore way).
         try:
             vm.destroy()
         except libvirt.libvirtError:
             raise CuckooMachineError("Error stopping virtual machine %s" % label)
+        finally:
+            self._disconnect(conn)
 
     def _connect(self):
         """Connects to libvirt subsystem.
         @raise CuckooMachineError: if cannot connect to libvirt.
         """
         try:
-            self.conn = libvirt.open("qemu:///system")
+            return libvirt.open("qemu:///system")
         except libvirt.libvirtError:
             raise CuckooMachineError("Cannot connect to libvirt")
 
-    def _lookup(self, label):
+    def _disconnect(self, conn):
+        """Disconnects to libvirt subsystem.
+        @raise CuckooMachineError: if cannot disconnect from libvirt.
+        """
+        try:
+            conn.close()
+        except libvirt.libvirtError:
+            raise CuckooMachineError("Cannot disconnect from libvirt")
+
+    def _lookup(self, conn, label):
         """Search for a virtual machine.
+        @param conn: libvirt connection handle.
         @param label: virtual machine name.
         @raise CuckooMachineError: if virtual machine is not found.
         """
         try:
-            vm = self.conn.lookupByName(label)
+            vm = conn.lookupByName(label)
         except libvirt.libvirtError:
                 raise CuckooMachineError("Cannot found machine %s" % label)
         return vm
@@ -76,10 +92,13 @@ class Kvm(MachineManager):
         """List available virtual machines.
         @raise CuckooMachineError: if unable to list virtual machines.
         """
+        conn = self._connect()
         try:
-            names = self.conn.listDefinedDomains()
+            names = conn.listDefinedDomains()
         except libvirt.libvirtError:
             raise CuckooMachineError("Cannot list domains")
+        finally:
+            self._disconnect(conn)
         return names
 
     def _version_check(self):
