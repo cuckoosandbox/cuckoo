d61ccac33e349b82df6a0d48f02d3c067cc88ed8
Nex <nex@cuckoobox.org>
2012-07-06 22:50:34 +0200

    Updated HTML report and web utility

--- utils/web.py
+++ utils/web.py
@@ -20,13 +20,13 @@ from lib.cuckoo.common.constants import CUCKOO_ROOT
 from lib.bottle import route, run, static_file, redirect, request
 
 # This directory will be created in $tmppath (see store_and_submit)
-TMPSUBDIR = 'cuckoo-web'
+TMPSUBDIR = "cuckoo-web"
 BUFSIZE = 1024
 
 # Templates directory
 lookup = TemplateLookup(directories=[os.path.join(CUCKOO_ROOT, "data", "html")],
-                        output_encoding='utf-8',
-                        encoding_errors='replace',
+                        output_encoding="utf-8",
+                        encoding_errors="replace",
                         strict_undefined=False)
 
 def store_and_submit_fileobj(fobj, filename, package="", options="", timeout=0, priority=1, machine="", platform=""):
@@ -36,8 +36,8 @@ def store_and_submit_fileobj(fobj, filename, package="", options="", timeout=0, 
     if not os.path.exists(targetpath): os.mkdir(targetpath)
 
     # Upload will be stored in a tmpdir with the original name
-    tmpdir = tempfile.mkdtemp(prefix='upload_', dir=targetpath)
-    tmpf = open(os.path.join(tmpdir, filename), 'wb')
+    tmpdir = tempfile.mkdtemp(prefix="upload_", dir=targetpath)
+    tmpf = open(os.path.join(tmpdir, filename), "wb")
     t = fobj.read(BUFSIZE)
 
     # While reading from client also compute md5hash
@@ -62,68 +62,72 @@ def store_and_submit_fileobj(fobj, filename, package="", options="", timeout=0, 
 
     return task_id
 
-@route('/')
+@route("/")
 def index():
     context = {}
-    template = lookup.get_template("webif/submit.html")
+    template = lookup.get_template("submit.html")
     return template.render(**context)
 
-@route('/browse')
+@route("/browse")
 def browse():
     db = Database()
     context = {}
 
     try:
         db.cursor.execute("SELECT * FROM tasks " \
-                          "ORDER BY status, priority, added_on;")
+                          "ORDER BY status, added_on DESC;")
     except sqlite3.OperationalError as e:
-        context['error'] = 'Could not load tasks from database.'
+        context["error"] = "Could not load tasks from database."
         return template.render(**context)
 
     rows = db.cursor.fetchall()
-    template = lookup.get_template("webif/browse.html")
+    template = lookup.get_template("browse.html")
+    context["cuckoo_root"] = CUCKOO_ROOT
 
     return template.render(os=os, rows=rows, **context)
 
-@route('/static/<filename:path>')
+@route("/static/<filename:path>")
 def server_static(filename):
     return static_file(filename, root=os.path.join(CUCKOO_ROOT, "data", "html"))
 
 # Handle upload form
-@route('/submit', method='POST')
+@route("/submit", method="POST")
 def submit():
     context = {}
     errors = False
 
     # Optional, can be empty
-    package  = request.forms.get('package', "")
-    options  = request.forms.get('options', "")
-    priority = request.forms.get('priority', 1)
-    timeout  = request.forms.get('timeout', "")
+    package  = request.forms.get("package", "")
+    options  = request.forms.get("options", "")
+    priority = request.forms.get("priority", 1)
+    timeout  = request.forms.get("timeout", "")
     data = request.files.file
 
     # Convert priority
-    try: priority = int(priority)
+    try:
+        priority = int(priority)
     except:
-        context['error_priority'] = 'Needs to be a number'
+        context["error_toggle"] = True
+        context["error_priority"] = "Needs to be a number"
         errors = True
 
     # File mandatory
-    if data == None or data == '':
-        context['error_file'] = 'Mandatory'
+    if data == None or data == "":
+        context["error_toggle"] = True
+        context["error_file"] = "Mandatory"
         errors = True
 
     # On errors, tell user
     if errors:
-        template = lookup.get_template("webif/submit.html")
+        template = lookup.get_template("submit.html")
         return template.render(timeout=timeout, priority=priority, options=options, package=package, **context)
     
     # Finally real store and submit
     taskid = store_and_submit_fileobj(data.file,data.filename, timeout=timeout, priority=priority, options=options, package=package)
 
     # Show result
-    template = lookup.get_template("webif/success.html")
+    template = lookup.get_template("success.html")
     return template.render(taskid=taskid, submitfile=data.filename)
 
-if __name__ == '__main__':
-    run(host='0.0.0.0', port=8080, debug=True, reloader=True)
+if __name__ == "__main__":
+    run(host="0.0.0.0", port=8080, debug=True, reloader=True)
