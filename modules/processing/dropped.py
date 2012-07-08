caed576bd3dc1cec701f31399787d57c9206d675
Nex <nex@cuckoobox.org>
2012-07-08 23:07:41 +0200

    Minor fixes

--- modules/processing/dropped.py
+++ modules/processing/dropped.py
@@ -17,12 +17,10 @@ class Dropped(Processing):
         self.key = "dropped"
         dropped_files = []
 
-        if not os.path.exists(self.dropped_path):
-            return dropped_files
-
-        for file_name in os.listdir(self.dropped_path):
-            file_path = os.path.join(self.dropped_path, file_name)
-            file_info = File(file_path).get_all()
-            dropped_files.append(file_info)
+        if os.path.exists(self.dropped_path):
+            for file_name in os.listdir(self.dropped_path):
+                file_path = os.path.join(self.dropped_path, file_name)
+                file_info = File(file_path).get_all()
+                dropped_files.append(file_info)
 
         return dropped_files
