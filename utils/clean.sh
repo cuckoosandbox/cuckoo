18f8a570a725947efab1c3dd5ff68d44bd5c90f2
Jurriaan Bremer <jurriaanbremer@gmail.com>
2012-10-16 09:21:58 +0200

    updated clean.sh to support running from ./cuckoo directory as well

--- utils/clean.sh
+++ utils/clean.sh
@@ -3,5 +3,12 @@
 # This file is part of Cuckoo Sandbox - http://www.cuckoosandbox.org
 # See the file 'docs/LICENSE' for copying permission.
 
-rm -rf ../db/ ../log/ ../storage/
-find ../ -name '*.pyc' -exec rm {} \;
+# I'm sure this can be done easier, but I'm not very familiar with bash
+# scripting.. So, here we go. Also, this only works from "./cuckoo" and
+# "./cuckoo/utils" directory, but it's still better than before.
+if [[ $PWD/ = */utils/ ]]; then
+    export PWD=${PWD:0:${#PWD}-6}
+fi
+
+rm -rf $PWD/db/ $PWD/log/ $PWD/storage/
+find $PWD/ -name '*.pyc' -exec rm {} \;
