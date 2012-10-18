06bf8512dd69a604aa3753df5b05c9b13d27f6e6
Nex <nex@cuckoobox.org>
2012-10-18 15:36:50 +0200

    Revert "Improved Python version check"
    This reverts commit 95b54be2c7db2b7278061fdf42fbdd501d9553a3.

--- lib/cuckoo/core/startup.py
+++ lib/cuckoo/core/startup.py
@@ -21,7 +21,8 @@ def check_python_version():
     """Checks if Python version is supported by Cuckoo.
     @raise CuckooStartupError: if version is not supported.
     """
-    if sys.version_info[:2] != (2, 7):
+    version = sys.version.split()[0]
+    if version < "2.7" or version >= "3":
         raise CuckooStartupError("You are running an incompatible version of Python, please use 2.7")
 
 def check_working_directory():
