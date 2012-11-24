f65de47ca63c9f65c37c6559ebbb570d7c310044
Nex <nex@cuckoobox.org>
2012-11-24 12:56:06 +0100

    Added module to extract strings from file

---
+++ modules/processing/strings.py
@@ -0,0 +1,24 @@
+# Copyright (C) 2010-2012 Cuckoo Sandbox Developers.
+# This file is part of Cuckoo Sandbox - http://www.cuckoosandbox.org
+# See the file 'docs/LICENSE' for copying permission.
+
+import re
+
+from lib.cuckoo.common.abstracts import Processing
+from lib.cuckoo.common.exceptions import CuckooProcessingError
+
+class Strings(Processing):
+    """Extract strings from analyzed file."""
+
+    def run(self):
+        """Run extract of printable strings.
+        @return: list of printable strings.
+        """
+        self.key = "strings"
+        strings = []
+
+        if self.cfg.analysis.category == "file":
+            data = open(self.file_path, "r").read()
+            strings = re.findall("[\x1f-\x7e]{6,}", data)
+
+        return strings
