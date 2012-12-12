7ab4ce155098781f464f7fbc2bb3db575cd555a1
Nex <nex@cuckoobox.org>
2012-12-12 14:33:12 +0100

    Fixed yara module logging hierarchy

--- modules/processing/yarasignatures.py
+++ modules/processing/yarasignatures.py
@@ -26,27 +26,33 @@ class YaraSignatures(Processing):
         self.key = "yara"
         matches = []
 
-        if HAVE_YARA and self.cfg.analysis.category == "file":
-            try:
-                rules = yara.compile(filepath=os.path.join(CUCKOO_ROOT, "data", "yara", "index.yar"))
-                for match in rules.match(self.file_path):
-                    strings = []
-                    for s in match.strings:
-                        # Beware, spaghetti code ahead.
-                        try:
-                            new = s[2].encode("utf-8")
-                        except UnicodeDecodeError:
-                            s = s[2].lstrip("uU").encode("hex").upper()
-                            s = " ".join(s[i:i+2] for i in range(0, len(s), 2))
-                            new = "{ %s }" % s
+        if self.cfg.analysis.category == "file":
+            if HAVE_YARA:
+                try:
+                    rules = yara.compile(filepath=os.path.join(CUCKOO_ROOT,
+                                                               "data",
+                                                               "yara",
+                                                               "index.yar"))
+                    for match in rules.match(self.file_path):
+                        strings = []
+                        for s in match.strings:
+                            # Beware, spaghetti code ahead.
+                            try:
+                                new = s[2].encode("utf-8")
+                            except UnicodeDecodeError:
+                                s = s[2].lstrip("uU").encode("hex").upper()
+                                s = " ".join(s[i:i+2] for i in range(0, len(s), 2))
+                                new = "{ %s }" % s
 
-                        if new not in strings:
-                            strings.append(new)
+                            if new not in strings:
+                                strings.append(new)
 
-                    matches.append({"name" : match.rule, "meta" : match.meta, "strings" : strings})
-            except yara.Error as e:
-                log.warning("Unable to match Yara signatures: %s" % e)
-        else:
-            log.warning("Yara is not installed, skip")
+                        matches.append({"name" : match.rule,
+                                        "meta" : match.meta,
+                                        "strings" : strings})
+                except yara.Error as e:
+                    log.warning("Unable to match Yara signatures: %s" % e)
+            else:
+                log.warning("Yara is not installed, skip")
 
         return matches
