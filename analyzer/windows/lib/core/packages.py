1496161acbad66017dd5eca08f75b996ad9b90c4
Nex <nex@cuckoobox.org>
2012-12-30 11:46:40 +0100

    Added HTML analysis package

--- analyzer/windows/lib/core/packages.py
+++ analyzer/windows/lib/core/packages.py
@@ -20,5 +20,7 @@ def choose_package(file_type, file_name):
         return "doc"
     elif "Microsoft Office Excel" in file_type or file_name.endswith(".xlsx"):
         return "xls"
+    elif "HTML" in file_type:
+        return "html"
     else:
         return None
