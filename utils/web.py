0f2323cf1b6cffeae58a1e3fe0f43378f4c68bcb
Nex <nex@cuckoobox.org>
2013-01-10 11:41:21 +0100

    Updated copyright notice

--- utils/web.py
+++ utils/web.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# Copyright (C) 2010-2012 Cuckoo Sandbox Developers.
+# Copyright (C) 2010-2013 Cuckoo Sandbox Developers.
 # This file is part of Cuckoo Sandbox - http://www.cuckoosandbox.org
 # See the file 'docs/LICENSE' for copying permission.
 
