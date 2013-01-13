5192694afc1c52c895887c84f0b55758653e9153
Nex <nex@cuckoobox.org>
2013-01-13 19:50:09 +0100

    Fixed extraction of file name

--- modules/processing/targetinfo.py
+++ modules/processing/targetinfo.py
@@ -18,6 +18,7 @@ class TargetInfo(Processing):
 
         if self.task["category"] == "file":
             target_info["file"] = File(self.file_path).get_all()
+            target_info["file"]["name"] = File(self.task.target).get_name()
         elif self.task["category"] == "url":
             target_info["url"] = self.task.target
 
