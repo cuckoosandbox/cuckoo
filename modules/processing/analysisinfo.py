c5e0dc86cc61e594145b9917fc1a2d1573d11fb9
rep <ms@mwcollect.org>
2013-02-19 13:16:40 +0100

    sanely handle task times (could result in problems before)

--- modules/processing/analysisinfo.py
+++ modules/processing/analysisinfo.py
@@ -3,11 +3,15 @@
 # See the file 'docs/LICENSE' for copying permission.
 
 import time
+import logging
 from datetime import datetime
 
 from lib.cuckoo.common.constants import CUCKOO_VERSION
 from lib.cuckoo.common.abstracts import Processing
 from lib.cuckoo.core.database import Database
+
+log = logging.getLogger(__name__)
+
 
 class AnalysisInfo(Processing):
     """General information about analysis session."""
@@ -18,15 +22,21 @@ class AnalysisInfo(Processing):
         """
         self.key = "info"
 
-        started = datetime.fromtimestamp(time.mktime(time.strptime(self.task["started_on"], "%Y-%m-%d %H:%M:%S")))
-        ended = datetime.fromtimestamp(time.mktime(time.strptime(self.task["completed_on"], "%Y-%m-%d %H:%M:%S")))
-        duration =  ended - started
+        try:
+            started = datetime.fromtimestamp(time.mktime(time.strptime(self.task["started_on"], "%Y-%m-%d %H:%M:%S")))
+            ended = datetime.fromtimestamp(time.mktime(time.strptime(self.task["completed_on"], "%Y-%m-%d %H:%M:%S")))
+        except:
+            log.critical("Failed to get start/end time from Task.")
+            # just set it to default timeout
+            duration = -1
+        else:
+            duration = (ended - started).seconds
 
         info = {
             "version" : CUCKOO_VERSION,
             "started" : self.task["started_on"],
-            "ended" : self.task["completed_on"],
-            "duration" : duration.seconds,
+            "ended" : self.task.get("completed_on", "none"),
+            "duration" : duration,
             "id" : int(self.task["id"]),
             "category" : self.task["category"]
         }
