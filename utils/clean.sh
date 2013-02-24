6c919303bb5c39927f588a26799fcddc2cf29e1c
Nex <nex@cuckoobox.org>
2013-02-24 20:30:53 +0100

    Restored clean.sh to working stage

--- utils/clean.sh
+++ utils/clean.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
-# Copyright (C) 2010-2013 Cuckoo Sandbox Developers.
+# Copyright (C) 2010-2012 Cuckoo Sandbox Developers.
 # This file is part of Cuckoo Sandbox - http://www.cuckoosandbox.org
 # See the file 'docs/LICENSE' for copying permission.
 
-# PWD equals top level folder "cuckoo"
-PWD=${PWD/cuckoo*/cuckoo\/}
-
-if [[ $PWD = */cuckoo*/ ]]; then
-    rm -rf $PWD/db/ $PWD/log/ $PWD/storage/
-    find $PWD/ -name '*.pyc' -exec rm {} \;
+# I'm sure this can be done easier, but I'm not very familiar with bash
+# scripting.. So, here we go. Also, this only works from "./cuckoo" and
+# "./cuckoo/utils" directory, but it's still better than before.
+if [[ $PWD/ = */utils/ ]]; then
+    export PWD=${PWD:0:${#PWD}-6}
 fi
+
+rm -rf $PWD/db/ $PWD/log/ $PWD/storage/
+find $PWD/ -name '*.pyc' -exec rm {} \;
