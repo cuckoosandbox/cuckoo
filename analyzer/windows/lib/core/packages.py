be1ce8bae6a05aea3b1167b182f0cbe1ac0adde0
Nex <nex@cuckoobox.org>
2013-05-23 13:11:40 +0200

    Improved package detection

--- analyzer/windows/lib/core/packages.py
+++ analyzer/windows/lib/core/packages.py
@@ -14,13 +14,15 @@ def choose_package(file_type, file_name):
         return "dll"
     elif "PE32" in file_type or "MS-DOS" in file_type:
         return "exe"
-    elif "PDF" in file_type:
+    elif "PDF" in file_type or file_name.endswith(".pdf"):
         return "pdf"
-    elif "Rich Text Format" in file_type or "Microsoft Office Word" in file_type or ("Composite Document File" in file_type and not "Installer" in file_type) or file_name.endswith(".docx"):
+    elif "Rich Text Format" in file_type or "Microsoft Office Word" in file_type or ("Composite Document File" in file_type and not "Installer" in file_type) or file_name.endswith(".docx") or file_name.endswith(".doc"):
         return "doc"
-    elif "Microsoft Office Excel" in file_type or file_name.endswith(".xlsx"):
+    elif "Microsoft Office Excel" in file_type or file_name.endswith(".xlsx") or file_name.endswith(".xls"):
         return "xls"
-    elif "HTML" in file_type:
+    elif "HTML" in file_type or file_name.endswith(".htm") or file_name.endswith(".html"):
         return "html"
+    elif file_name.endswith(".jar"):
+        return "jar"
     else:
         return None
