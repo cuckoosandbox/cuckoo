1c8adaa609902af30b9d69103bb3e1c7c762179a
Nex <nex@cuckoobox.org>
2013-06-04 16:38:39 +0200

    Updated processing util

--- utils/process.py
+++ utils/process.py
@@ -14,26 +14,21 @@ sys.path.append(os.path.join(os.path.abspath(os.path.dirname(__file__)), ".."))
 
 from lib.cuckoo.common.constants import CUCKOO_ROOT
 from lib.cuckoo.core.startup import init_modules
-from lib.cuckoo.core.processor import Processor
-from lib.cuckoo.core.reporter import Reporter
+from lib.cuckoo.core.plugins import RunProcessing, RunSignatures, RunReporting
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("id", type=str, help="ID of the analysis to process")
     parser.add_argument("-r", "--report", help="Re-generate report", action="store_true", required=False)
-    parser.add_argument("-f", "--failed", help="Mark the analysis as failed", action="store_true", required=False)
     args = parser.parse_args()
 
     init_modules()
 
-    if args.failed:
-        results = {"success" : False}
-    else:
-        results = Processor(args.id).run()
-        results["success"] = True
-
+    results = RunProcessing(task_id=args.id).run()
+    RunSignatures(results=results).run()
+    
     if args.report:
-        Reporter(args.id).run(results)
+        RunReporting(task_id=args.id, results=results).run()
 
 if __name__ == "__main__":
     main()
