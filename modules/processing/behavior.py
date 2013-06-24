40847eec69a5c0c231e570ed15a26e1f696f21ba
Nex <nex@cuckoobox.org>
2013-06-24 15:28:06 +0200

    Merged enhanched results in behavior module

--- modules/processing/behavior.py
+++ modules/processing/behavior.py
@@ -404,6 +404,424 @@ class ProcessTree:
 
         return self.proctree
 
+class Enhanced(object):
+
+    def __init__(self, proc_results, details=False):
+        """
+        @param proc_results: enumerated processes results.
+        @param details: Also add some (not so relevant) Details to the log
+        """
+        self.proc_results = proc_results
+        self.currentdir = "C: "
+        self.eid = 0
+        self.details = details
+        self.filehandles = {}
+        self.keyhandles = {
+            "0x80000000" : "HKEY_CLASSES_ROOT\\",
+            "0x80000001" : "HKEY_CURRENT_USER\\",
+            "0x80000002" : "HKEY_LOCAL_MACHINE\\",
+            "0x80000003" : "HKEY_USERS\\",
+            "0x80000004" : "HKEY_PERFORMANCE_DATA\\",
+            "0x80000005" : "HKEY_CURRENT_CONFIG\\",
+            "0x80000006" : "HKEY_DYN_DATA\\"
+        }
+        self.modules = {}
+        self.procedures = {}
+
+    def _add_procedure(self, mbase, name, base):
+        """
+        Add a procedure address
+        """
+        self.procedures[base] = "{0}:{1}".format(self._get_loaded_module(mbase), name)
+
+    def _get_procedure(self, base):
+        """
+        Get the name of a procedure
+        """
+        return self.procedures.get(base, "")
+
+    def _add_loaded_module(self, name, base):
+        """
+        Add a loaded module to the internal database
+        """
+        self.modules[base] = name
+
+    def _get_loaded_module(self, base):
+        """
+        Get the name of a loaded module from the internal db
+        """
+        return self.modules.get(base, "")
+
+    # Registry
+    def _add_keyhandle(self, registry, subkey, handle):
+        """
+        @registry: returned, new handle
+        @handle: handle to base key
+        @subkey: subkey to add
+        """
+        if handle != 0 and handle in self.keyhandles:
+            return self.keyhandles[handle]
+
+        name = ""
+        if registry and registry != "0x00000000" and\
+            registry in self.keyhandles:
+            name = self.keyhandles[registry]
+
+        nkey = name + subkey
+        nkey = self._fix_key(nkey)
+
+        self.keyhandles[handle] = nkey
+
+        return nkey
+
+    def _remove_keyhandle(self, handle):
+        key = self._get_keyhandle(handle)
+        try:
+            self.keyhandles.pop(handle)
+        except KeyError:
+            pass
+        return key
+
+    def _get_keyhandle(self, handle):
+        try:
+            return self.keyhandles[handle]
+        except KeyError:
+            return ""
+
+    def _fix_key(self, key):
+        """ Fix a registry key to have it normalized
+        """
+        res = key
+        if key.lower().startswith("registry\\machine\\"):
+            res = "HKEY_LOCAL_MACHINE\\" + key[17:]
+        elif key.lower().startswith("registry\\user\\"):
+            res = "HKEY_USERS\\" + key[14:]
+
+        if not res.endswith("\\\\"):
+            res = res + "\\"
+        return res
+
+    def _process_call(self, call):
+        """ Gets files calls
+        @return: information list
+        """
+        def _load_args(call):
+            """
+            Load arguments from call
+            """
+            res = {}
+            for argument in call["arguments"]:
+                res[argument["name"]] = argument["value"]
+
+            return res
+
+        def _generic_handle_details(self, call, item):
+            """
+            Generic handling of api calls
+
+            @call: the call dict
+            @item: Generic item to process
+            """
+            event = None
+            if call["api"] in item["apis"]:
+                args = _load_args(call)
+                self.eid += 1
+                event = {
+                    "event" : item["event"],
+                    "object" : item["object"],
+                    "timestamp": call["timestamp"],
+                    "eid": self.eid
+                }
+                for (logname, dataname) in item["args"]:
+                    event[logname] = args.get(dataname, None)
+                return event
+
+        def _generic_handle(self, data, call):
+            """
+            Generic handling of api calls
+            """
+            for item in data:
+                event = _generic_handle_details(self, call, item)
+                if event:
+                    return event
+
+            return None
+
+        # File handles
+        def _add_file_handle(handles, handle, filename):
+            handles[handle] = filename
+
+        def _remove_file_handle(handles, handle):
+            try:
+                handles.pop(handle)
+            except KeyError:
+                pass
+
+        def _get_file_handle(handles, handle):
+            try:
+                return handles[handle]
+            except KeyError:
+                return None
+
+        event = None
+
+        gendat = [
+            {
+                "event" : "move",
+                "object" : "file",
+                "apis" : [
+                    "MoveFileWithProgressW",
+                    "MoveFileExA",
+                    "MoveFileExW"
+                ],
+                "args" : [
+                    ("from", "ExistingFileName"),
+                    ("to", "NewFileName")
+                ]
+            },
+            {
+                "event" : "copy",
+                "object" : "file",
+                "apis" : [
+                    "CopyFileA",
+                    "CopyFileW",
+                    "CopyFileExW",
+                    "CopyFileExA"
+                ],
+                "args" : [
+                    ("from", "ExistingFileName"),
+                    ("to", "NewFileName")
+                ]
+            },
+            {
+                "event" : "delete",
+                "object" : "file",
+                "apis" : [
+                    "DeleteFileA",
+                    "DeleteFileW",
+                    "NtDeleteFile"
+                ],
+                "args" : [("file", "FileName")]
+            },
+            {
+                "event" : "delete",
+                "object" : "dir",
+                "apis" : [
+                    "RemoveDirectoryA",
+                    "RemoveDirectoryW"
+                ],
+                "args" : [("file", "DirectoryName")]
+            },
+            {
+                "event" : "create",
+                "object" : "dir",
+                "apis" : [
+                    "CreateDirectoryW",
+                    "CreateDirectoryExW"
+                ],
+                "args" : [("file", "DirectoryName")]
+            },
+            {
+                "event" : "write",
+                "object" : "file",
+                "apis" : [
+                    "URLDownloadToFileW",
+                    "URLDownloadToFileA"
+                ],
+                "args" : [("file", "FileName")]
+            },
+            {
+                "event" : "execute",
+                "object" : "file",
+                "apis" : [
+                    "CreateProcessAsUserA",
+                    "CreateProcessAsUserW",
+                    "CreateProcessA",
+                    "CreateProcessW",
+                    "NtCreateProcess",
+                    "NtCreateProcessEx"
+                ],
+                "args" : [("file", "FileName")]
+            },
+            {
+                "event" : "load",
+                "object" : "library",
+                "apis" : [
+                    "LoadLibraryA",
+                    "LoadLibraryW",
+                    "LoadLibraryExA",
+                    "LoadLibraryExW",
+                    "LdrLoadDll",
+                    "LdrGetDllHandle"
+                ],
+                "args" : [
+                    ("file", "FileName"),
+                    ("pathtofile", "PathToFile"),
+                    ("moduleaddress", "BaseAddress")
+                ]
+            },
+            {
+                "event" : "findwindow",
+                "object" : "windowname",
+                "apis" : [
+                    "FindWindowA",
+                    "FindWindowW",
+                    "FindWindowExA",
+                    "FindWindowExW"
+                ],
+                "args" : [
+                    ("classname", "ClassName"),
+                    ("windowname", "WindowName")
+                ]
+            },
+            {
+                "event" : "read",
+                "object" : "file",
+                "apis" : [
+                    "NtReadFile",
+                    "ReadFile"
+                ],
+                "args" : []
+            },
+            {
+                "event" : "write",
+                "object" : "file",
+                "apis" : ["NtWriteFile"],
+                "args" : []
+            },
+            {
+                "event" : "delete",
+                "object" : "registry",
+                "apis" : [
+                    "RegDeleteKeyA",
+                    "RegDeleteKeyW"
+                ],
+                "args" : []
+            },
+            {
+                "event" : "write",
+                "object" : "registry",
+                "apis" : [
+                    "RegSetValueExA",
+                    "RegSetValueExW"
+                ],
+                "args" : [
+                    ("content", "Buffer"),
+                    ("object", "object")
+                ]
+            },
+            {
+                "event" : "read",
+                "object" : "registry",
+                "apis" : [
+                    "RegQueryValueExA",
+                    "RegQueryValueExW",
+                    "NtQueryValueKey"
+                ],
+                "args" : []
+            },
+            {
+                "event" : "delete",
+                "object" : "registry",
+                "apis" : [
+                    "RegDeleteValueA",
+                    "RegDeleteValueW",
+                    "NtDeleteValueKey"
+                ],
+                "args" : []
+            },
+            {
+                "event" : "create",
+                "object" : "windowshook",
+                "apis" : ["SetWindowsHookExA"],
+                "args" : [
+                    ("id", "HookIdentifier"),
+                    ("moduleaddress", "ModuleAddress"),
+                    ("procedureaddress", "ProcedureAddress")
+                ]
+            },                  
+        ]
+
+        # Not sure I really want this, way too noisy anyway and doesn't bring
+        # much value.
+        #if self.details:
+        #    gendata = gendata + [{"event" : "get",
+        #           "object" : "procedure",
+        #           "apis" : ["LdrGetProcedureAddress"],
+        #           "args": [("name", "FunctionName"), ("ordinal", "Ordinal")]
+        #          },]
+
+        event = _generic_handle(self, gendat, call)
+        args = _load_args(call)
+
+        if event:
+            if call["api"] in ["NtReadFile", "ReadFile", "NtWriteFile"]:
+                event["file"] = _get_file_handle(self.filehandles, args["FileHandle"])
+
+            elif call["api"] in ["RegDeleteKeyA", "RegDeleteKeyW"]:
+                event["regkey"] = "{0}{1}".format(self._get_keyhandle(args.get("Handle", "")), args.get("SubKey", ""))
+
+            elif call["api"] in ["RegSetValueExA", "RegSetValueExW"]:
+                event["regkey"] = "{0}{1}".format(self._get_keyhandle(args.get("Handle", "")), args.get("ValueName", ""))
+
+            elif call["api"] in ["RegQueryValueExA", "RegQueryValueExW", "RegDeleteValueA", "RegDeleteValueW", "NtDeleteValueKey"]:
+                event["regkey"] = "{0}{1}".format(self._get_keyhandle(args.get("Handle", "UNKNOWN")), args.get("ValueName", ""))
+
+            elif call["api"] in ["NtQueryValueKey"]:
+                event["regkey"] = "{0}{1}".format(self._get_keyhandle(args.get("KeyHandle", "UNKNOWN")), args.get("ValueName", ""))
+
+            elif call["api"] in ["LoadLibraryA", "LoadLibraryW", "LoadLibraryExA", "LoadLibraryExW", "LdrGetDllHandle"] and call["status"]:
+                self._add_loaded_module(args.get("FileName", ""), args.get("ModuleHandle", ""))
+
+            elif call["api"] in ["LdrLoadDll"] and call["status"]:
+                self._add_loaded_module(args.get("FileName", ""), args.get("BaseAddress", ""))
+
+            elif call["api"] in ["LdrGetProcedureAddress"] and call["status"]:
+                self._add_procedure(args.get("ModuleHandle", ""), args.get("FunctionName", ""), args.get("FunctionAddress", ""))
+                event["module"] = self._get_loaded_module(args.get("ModuleHandle", ""))
+
+            elif call["api"] in ["SetWindowsHookExA"]:
+                event["module"] = self._get_loaded_module(args.get("ModuleAddress", ""))
+
+            return event
+
+        elif call["api"] in ["SetCurrentDirectoryA", "SetCurrentDirectoryW"]:
+            self.currentdir = args["Path"]
+
+        elif call["api"] in ["NtCreateFile", "NtOpenFile"]:
+            _add_file_handle(self.filehandles, args["FileHandle"], args["FileName"])
+
+        elif call["api"] in ["CreateFileW"]:
+            _add_file_handle(self.filehandles, call["return"], args["FileName"])
+
+        elif call["api"] in ["NtClose", "CloseHandle"]:
+            _remove_file_handle(self.filehandles, args["Handle"])
+
+        elif call["api"] in ["RegOpenKeyExA", "RegOpenKeyExW", "RegCreateKeyExA", "RegCreateKeyExW"]:
+            regkey = self._add_keyhandle(args.get("Registry", ""), args.get("SubKey", ""), args.get("Handle", ""))
+
+        elif call["api"] in ["NtOpenKey"]:
+            regkey = self._add_keyhandle(None, args.get("ObjectAttributes", ""), args.get("KeyHandle", ""))
+
+        elif call["api"] in ["RegCloseKey"]:
+            regkey = self._remove_keyhandle(args.get("Handle", ""))
+
+        return event
+
+    def run(self):
+        """Get registry keys, mutexes and files.
+        @return: Summary of keys, mutexes and files.
+        """
+        events = []
+
+        for entry in self.proc_results:
+            for call in entry["calls"]:
+                event = self._process_call(call)
+                if event:
+                    events.append(event)
+
+        return events
+
 class BehaviorAnalysis(Processing):
     """Behavior Analyzer."""
 
@@ -414,8 +832,9 @@ class BehaviorAnalysis(Processing):
         self.key = "behavior"
 
         behavior = {}
-        behavior["processes"]   = Processes(self.logs_path).run()
+        behavior["processes"] = Processes(self.logs_path).run()
         behavior["processtree"] = ProcessTree(behavior["processes"]).run()
-        behavior["summary"]     = Summary(behavior["processes"]).run()
+        behavior["summary"] = Summary(behavior["processes"]).run()
+        behavior["enhanced"] = Enhanced(behavior["processes"]).run()
 
         return behavior
