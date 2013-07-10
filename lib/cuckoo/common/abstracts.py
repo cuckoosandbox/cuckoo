1be8f8d2ef736b3f575e5cf206076117d85f60c3
Nex <nex@cuckoobox.org>
2013-07-10 13:39:02 +0200

    Fuck 80 columns

--- lib/cuckoo/common/abstracts.py
+++ lib/cuckoo/common/abstracts.py
@@ -510,8 +510,7 @@ class Processing(object):
         """
         self.analysis_path = analysis_path
         self.log_path = os.path.join(self.analysis_path, "analysis.log")
-        self.file_path = os.path.realpath(os.path.join(self.analysis_path,
-                                                       "binary"))
+        self.file_path = os.path.realpath(os.path.join(self.analysis_path, "binary"))
         self.dropped_path = os.path.join(self.analysis_path, "files")
         self.logs_path = os.path.join(self.analysis_path, "logs")
         self.shots_path = os.path.join(self.analysis_path, "shots")
