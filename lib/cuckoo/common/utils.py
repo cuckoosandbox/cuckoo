f66c286056b680e9d59ceb8375a9fd60c3ad196c
jekil <alessandro@tanasi.it>
2013-07-13 11:46:25 +0200

    Added utils method to decode strings in any encoding and encode to unicode

--- lib/cuckoo/common/utils.py
+++ lib/cuckoo/common/utils.py
@@ -7,6 +7,7 @@ import time
 import shutil
 import ntpath
 import string
+import chardet
 import tempfile
 import xmlrpclib
 from datetime import datetime
@@ -180,3 +181,52 @@ def time_from_cuckoomon(s):
     @return: datetime object
     """
     return datetime.strptime(s, "%Y-%m-%d %H:%M:%S,%f")
+
+def to_unicode(str):
+    """Attempt to fix non uft-8 string into utf-8. It tries to guess input encoding,
+    if fail retry with a replace strategy (so undetectable chars will be escaped).
+    @see: fuller list of encodings at http://docs.python.org/library/codecs.html#standard-encodings
+    """
+
+    def brute_enc(str):
+        """Trying to decode via simple brute forcing."""
+        result = None
+        encodings = ("ascii", "utf8", "latin1")
+        for enc in encodings:
+            if result:
+                break
+            try:
+                result = unicode(str, enc)
+            except UnicodeDecodeError:
+                pass
+        return result
+
+    def chardet_enc(str):
+        """Guess encoding via chardet."""
+        result = None
+        enc = chardet.detect(str)["encoding"]
+
+        try:
+            result = unicode(str, enc)
+        except UnicodeDecodeError:
+            pass
+
+        return result
+
+    # If already in unicode, skip.
+    if isinstance(str, unicode):
+        return str
+
+    # First try to decode against a little set of common encodings.
+    result = brute_enc(str)
+
+    # Try via chardet.
+    if not result:
+        result = chardet_enc(str)
+
+    # If not possible to convert the input string, try again with
+    # a replace strategy.
+    if not result:
+        result = unicode(str, errors="replace")
+
+    return result
