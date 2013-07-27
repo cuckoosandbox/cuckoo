35ef2f66e2517269163979a9bac21fbcc108102c
jekil <alessandro@tanasi.it>
2013-07-27 21:42:58 +0200

    Added copyleft header

--- web/submission/urls.py
+++ web/submission/urls.py
@@ -1,3 +1,7 @@
+# Copyright (C) 2010-2013 Cuckoo Sandbox Developers.
+# This file is part of Cuckoo Sandbox - http://www.cuckoosandbox.org
+# See the file 'docs/LICENSE' for copying permission.
+
 from django.conf.urls import patterns, url
 
 urlpatterns = patterns("",
--- web/web/__init__.py
+++ web/web/__init__.py
@@ -1,3 +1,7 @@
+# Copyright (C) 2010-2013 Cuckoo Sandbox Developers.
+# This file is part of Cuckoo Sandbox - http://www.cuckoosandbox.org
+# See the file 'docs/LICENSE' for copying permission.
+
 from pymongo.connection import Connection
 
 db = Connection().cuckoo
