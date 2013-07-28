11310aa90c00773b7ae690fddc038172ed026360
jekil <alessandro@tanasi.it>
2013-07-28 13:23:23 +0200

    Added DEBUG configuration

--- web/web/local_settings.py
+++ web/web/local_settings.py
@@ -36,6 +36,11 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
+# Allow verbose debug error message in case of application fault.
+# It's strongly suggested to set it to False if you are serving the
+# web application from a web server fron-end (i.e. Apache).
+DEBUG = True
+
 # A list of strings representing the host/domain names that this Django site
 # can serve.
 # Values in this list can be fully qualified names (e.g. 'www.example.com').
