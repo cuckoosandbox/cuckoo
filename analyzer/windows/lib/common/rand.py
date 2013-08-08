dfdb83cdd4ccd0cfcbc0558960790c4a84b36dfe
Nex <nex@cuckoobox.org>
2013-08-08 04:29:37 +0200

    Added random integer

--- analyzer/windows/lib/common/rand.py
+++ analyzer/windows/lib/common/rand.py
@@ -7,3 +7,8 @@ def random_string(minimum, maximum=None):
 
     count = random.randint(minimum, maximum)
     return "".join(random.choice(string.ascii_letters) for x in xrange(count))
+
+def random_integer(digits):
+    start = 10 ** (digits - 1)
+    end = (10 ** digits) - 1
+    return random.randint(start, end)
