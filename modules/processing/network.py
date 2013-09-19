71dc0d27c0b4a1e9c31484f2bfe60cbcd3448c9e
Nex <nex@cuckoobox.org>
2013-09-19 17:43:03 +0200

    Filtering hosts belonging to private networks

--- modules/processing/network.py
+++ modules/processing/network.py
@@ -4,6 +4,7 @@
 
 import os
 import re
+import struct
 import socket
 import logging
 from urlparse import urlunparse
@@ -30,7 +31,9 @@ class Pcap:
         """
         self.filepath = filepath
 
-        # List containing all IP addresses involved in the analysis.
+        # List of all hosts.
+        self.hosts = []
+        # List containing all non-private IP addresses.
         self.unique_hosts = []
         # List of unique domains.
         self.unique_domains = []
@@ -51,19 +54,70 @@ class Pcap:
         # Dictionary containing all the results of this processing.
         self.results = {}
 
+    def _is_private_ip(self, ip):
+        """Check if the IP belongs to private network blocks.
+        @param ip: IP address to verify.
+        @return: boolean representing whether the IP belongs or not to
+                 a private network block.
+        """
+        networks = [
+            "0.0.0.0/8",
+            "10.0.0.0/8",
+            "100.64.0.0/10",
+            "127.0.0.0/8",
+            "169.254.0.0/16",
+            "172.16.0.0/12",
+            "192.0.0.0/24",
+            "192.0.2.0/24",
+            "192.88.99.0/24",
+            "192.168.0.0/16",
+            "198.18.0.0/15",
+            "198.51.100.0/24",
+            "203.0.113.0/24",
+            "240.0.0.0/4",
+            "255.255.255.255/32",
+            "224.0.0.0/4"
+        ]
+
+        for network in networks:
+            ipaddr = struct.unpack(">I", socket.inet_aton(ip))[0]
+
+            netaddr, bits = network.split("/")
+
+            network_low = struct.unpack(">I", socket.inet_aton(netaddr))[0]
+            network_high = network_low | 1 << (32 - int(bits)) - 1
+
+            if ((ipaddr <= network_high) and (ipaddr >= network_low)):
+                return True
+
+        return False
+
     def _add_hosts(self, connection):
         """Add IPs to unique list.
         @param connection: connection data
         """
         try:
             if connection["src"] not in self.unique_hosts:
-                self.unique_hosts.append(convert_to_printable(connection["src"]))
-            if connection["dst"] not in self.unique_hosts:
-                self.unique_hosts.append(convert_to_printable(connection["dst"]))
-        except Exception:
-            return False
+                ip = convert_to_printable(connection["src"])
+                if ip in self.hosts:
+                    return
+                else:
+                    self.hosts.append(ip)
 
-        return True
+                if not self._is_private_ip(ip):
+                    self.unique_hosts.append(ip)
+
+            if connection["dst"] not in self.unique_hosts:
+                ip = convert_to_printable(connection["dst"])
+                if ip in self.hosts:
+                    return
+                else:
+                    self.hosts.append(ip)
+
+                if not self._is_private_ip(ip):
+                    self.unique_hosts.append()
+        except:
+            pass
 
     def _dns_gethostbyname(self, name):
         """Get host by name wrapper.
