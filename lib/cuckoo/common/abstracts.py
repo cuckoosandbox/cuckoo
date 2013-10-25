39a180b599ab67278f43d033ca9c2eff763f9468
Christopher Schmitt <chris.schmitt@mandiant.com>
2013-10-25 00:09:05 -0400

    added .create() to LibVirtMachinery's start method

--- lib/cuckoo/common/abstracts.py
+++ lib/cuckoo/common/abstracts.py
@@ -322,6 +322,8 @@ class LibVirtMachinery(Machinery):
             self._disconnect(conn)
             raise CuckooMachineError("No snapshot found for virtual machine {0}".format(label))
 
+        # Start the VM
+        self.vms[label].create()
         # Check state.
         self._wait_status(label, self.RUNNING)
 
