33af4ba0248a0c393cd73048e794868ce3ef1053
David Maciejak <david.maciejak@gmail.com>
2013-10-26 08:03:34 +0200

    Just put copyright to me

--- web/analysis/templatetags/string.py
+++ web/analysis/templatetags/string.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2010-2013 Cuckoo Sandbox Developers.
+# Copyright (C) 2013 David Maciejak
 # This file is part of Cuckoo Sandbox - http://www.cuckoosandbox.org
 # See the file "docs/LICENSE" for copying permission.
 
