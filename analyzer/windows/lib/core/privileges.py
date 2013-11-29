b6ecf675a3f58747ebd9a6136b7ed36fc92db1ff
Jurriaan Bremer <jurriaanbremer@gmail.com>
2013-11-29 16:41:41 +0100

    tweak grant-debug-privilege a bit

--- analyzer/windows/lib/core/privileges.py
+++ analyzer/windows/lib/core/privileges.py
@@ -2,37 +2,36 @@
 # This file is part of Cuckoo Sandbox - http://www.cuckoosandbox.org
 # See the file 'docs/LICENSE' for copying permission.
 
-from ctypes import sizeof, byref, c_int, c_ulong, wintypes
-import ctypes
+from ctypes import wintypes, POINTER
 
-from lib.common.defines import *
+from lib.common.defines import ADVAPI32, KERNEL32, SE_PRIVILEGE_ENABLED
+from lib.common.defines import LUID, TOKEN_PRIVILEGES, PROCESS_ALL_ACCESS
+from lib.common.defines import TOKEN_ALL_ACCESS, LUID_AND_ATTRIBUTES
 
-def grant_debug_privilege(pid=0):
+def grant_debug_privilege(pid=None):
     """Grant debug privileges.
     @param pid: PID.
     @return: operation status.
     """
     ADVAPI32.OpenProcessToken.argtypes = (wintypes.HANDLE,
                                           wintypes.DWORD,
-                                          ctypes.POINTER(wintypes.HANDLE))
+                                          POINTER(wintypes.HANDLE))
 
     ADVAPI32.LookupPrivilegeValueW.argtypes = (wintypes.LPWSTR,
                                                wintypes.LPWSTR,
-                                               ctypes.POINTER(LUID))
+                                               POINTER(LUID))
+
     ADVAPI32.AdjustTokenPrivileges.argtypes = (wintypes.HANDLE,
                                                wintypes.BOOL,
-                                               ctypes.POINTER(TOKEN_PRIVILEGES),
+                                               POINTER(TOKEN_PRIVILEGES),
                                                wintypes.DWORD,
-                                               ctypes.POINTER(TOKEN_PRIVILEGES),
-                                               ctypes.POINTER(wintypes.DWORD))
+                                               POINTER(TOKEN_PRIVILEGES),
+                                               POINTER(wintypes.DWORD))
 
-    h_process = None
-    if pid == 0:
+    if pid is None:
         h_process = KERNEL32.GetCurrentProcess()
     else:
-        h_process = KERNEL32.OpenProcess(PROCESS_ALL_ACCESS,
-                                         False,
-                                         pid)
+        h_process = KERNEL32.OpenProcess(PROCESS_ALL_ACCESS, False, pid)
 
     if not h_process:
         return False    
@@ -53,18 +52,13 @@ def grant_debug_privilege(pid=0):
     luid_attributes.Luid = se_original_luid
     luid_attributes.Attributes = SE_PRIVILEGE_ENABLED
     token_privs = TOKEN_PRIVILEGES()
-    token_privs.PrivilegeCount = 1;
-    token_privs.Privileges = luid_attributes; 
-    
-    if not ADVAPI32.AdjustTokenPrivileges(h_current_token,
-                                          False,
-                                          token_privs,
-                                          0,
-                                          None,
-                                          None):
+    token_privs.PrivilegeCount = 1
+    token_privs.Privileges = luid_attributes
+
+    if not ADVAPI32.AdjustTokenPrivileges(h_current_token, False, token_privs,
+                                          0, None, None):
         return False
     
     KERNEL32.CloseHandle(h_current_token)
     KERNEL32.CloseHandle(h_process)
-
     return True
