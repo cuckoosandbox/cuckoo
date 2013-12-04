667e9e4f780ee694d6ea8b74e25321ce6d61df6c
Nex <nex@cuckoobox.org>
2013-12-04 15:43:37 +0100

    Added .rtf extension

--- analyzer/windows/lib/core/packages.py
+++ analyzer/windows/lib/core/packages.py
@@ -16,7 +16,13 @@ def choose_package(file_type, file_name):
         return "exe"
     elif "PDF" in file_type or file_name.endswith(".pdf"):
         return "pdf"
-    elif "Rich Text Format" in file_type or "Microsoft Word" in file_type or "Microsoft Office Word" in file_type or ("Composite Document File" in file_type and not "Installer" in file_type) or file_name.endswith(".docx") or file_name.endswith(".doc"):
+    elif "Rich Text Format" in file_type or \
+         "Microsoft Word" in file_type or \
+         "Microsoft Office Word" in file_type or \
+         ("Composite Document File" in file_type and not "Installer" in file_type) or \
+         file_name.endswith(".docx") or \
+         file_name.endswith(".doc") or \
+         file_name.endswith(".rtf"):
         return "doc"
     elif "Microsoft Office Excel" in file_type or file_name.endswith(".xlsx") or file_name.endswith(".xls"):
         return "xls"
