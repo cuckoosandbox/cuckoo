4b7686e0db2fc03601357f4d01c284357b4b05de
carlo <mcarlod333@gmail.com>
2013-12-09 16:10:56 -0500

    Changes to allow --tool_dir to be independant

--- analyzer/windows/analyzer.py
+++ analyzer/windows/analyzer.py
@@ -502,7 +502,7 @@ class Analyzer:
             # If the analysis target is a file, we choose the package according
             # to the file format.
             if self.config.category == "file":
-                if self.config.tool:
+                if self.config.tool or self.config.tool_dir:
                     package = "tool"
                 else:
                     package = choose_package(self.config.file_type, self.config.file_name)
