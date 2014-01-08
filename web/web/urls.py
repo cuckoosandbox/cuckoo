dafb93a937608000952e3b023869e365deec5126
Nex <nex@cuckoobox.org>
2014-01-08 09:24:48 +0100

    Made dashboard homepage

--- web/web/urls.py
+++ web/web/urls.py
@@ -5,7 +5,7 @@
 from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns("",
-    url(r"^$", "analysis.views.index"),
+    url(r"^$", "dashboard.views.index"),
     url(r"^analysis/", include("analysis.urls")),
     url(r"^submit/", include("submission.urls")),
     url(r"^file/(?P<category>\w+)/(?P<object_id>\w+)/$", "analysis.views.file"),
