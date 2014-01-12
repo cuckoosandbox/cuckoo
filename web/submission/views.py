ac93b2065d607a9b24ad3225e11cb8dfbc523862
wzr <jorge@capmany.net>
2014-01-12 22:15:32 +0100

    Adding a dropdown menu for the VM selection in the submission page, populated by the name of the machine and its tags. Also adding the possibility to send to all machines at once, for exploit-like samples that need to be tested on different software versions/machines.

--- web/submission/views.py
+++ web/submission/views.py
@@ -57,21 +57,38 @@ def index(request):
             path = request.FILES["sample"].temporary_file_path()
 
             db = Database()
+            task_ids = []
+            if machine == 'ALL':
+                for m in db.list_machines():
+                    task_ids.append(db.add_path(file_path=path,
+                                          package=package,
+                                          timeout=timeout,
+                                          options=options,
+                                          priority=priority,
+                                          machine=m,
+                                          custom=custom,
+                                          memory=memory,
+                                          enforce_timeout=enforce_timeout,
+                                          tags=tags))
+            else:
+                task_ids.append(db.add_path(file_path=path,
+                                      package=package,
+                                      timeout=timeout,
+                                      options=options,
+                                      priority=priority,
+                                      machine=machine,
+                                      custom=custom,
+                                      memory=memory,
+                                      enforce_timeout=enforce_timeout,
+                                      tags=tags))
 
-            task_id = db.add_path(file_path=path,
-                                  package=package,
-                                  timeout=timeout,
-                                  options=options,
-                                  priority=priority,
-                                  machine=machine,
-                                  custom=custom,
-                                  memory=memory,
-                                  enforce_timeout=enforce_timeout,
-                                  tags=tags)
-
-            if task_id:
+            if len(task_ids) == 1: 
                 return render_to_response("success.html",
-                                          {"message": "The analysis task was successfully added with ID {0}.".format(task_id)},
+                                          {"message": "The analysis task was successfully added with ID {0}.".format(task_ids[0])},
+                                          context_instance=RequestContext(request))
+            elif len(task_ids) > 1:
+                return render_to_response("success.html",
+                                          {"message": "The analysis task were successfully added with IDs {0}.".format(', '.join(str(i) for i in task_ids))},
                                           context_instance=RequestContext(request))
             else:
                 return render_to_response("error.html",
@@ -85,21 +102,37 @@ def index(request):
                                           context_instance=RequestContext(request))
 
             db = Database()
-
-            task_id = db.add_url(url=url,
-                                 package=package,
-                                 timeout=timeout,
-                                 options=options,
-                                 priority=priority,
-                                 machine=machine,
-                                 custom=custom,
-                                 memory=memory,
-                                 enforce_timeout=enforce_timeout,
-                                 tags=tags)
-
-            if task_id:
+            task_ids = []
+            if machine == "ALL":
+                for m in db.list_machines():
+                    task_ids.append(db.add_url(url=url,
+                                     package=package,
+                                     timeout=timeout,
+                                     options=options,
+                                     priority=priority,
+                                     machine=m,
+                                     custom=custom,
+                                     memory=memory,
+                                     enforce_timeout=enforce_timeout,
+                                     tags=tags))
+            else:
+                task_ids.append(db.add_url(url=url,
+                                     package=package,
+                                     timeout=timeout,
+                                     options=options,
+                                     priority=priority,
+                                     machine=machine,
+                                     custom=custom,
+                                     memory=memory,
+                                     enforce_timeout=enforce_timeout,
+                                     tags=tags))
+            if len(task_ids) == 1: 
                 return render_to_response("success.html",
-                                          {"message": "The analysis task was successfully added with ID {0}.".format(task_id)},
+                                          {"message": "The analysis task was successfully added with ID {0}.".format(task_ids[0])},
+                                          context_instance=RequestContext(request))
+            elif len(task_ids) > 1:
+                return render_to_response("success.html",
+                                          {"message": "The analysis task were successfully added with IDs {0}.".format(', '.join(str(i) for i in task_ids))},
                                           context_instance=RequestContext(request))
             else:
                 return render_to_response("error.html",
@@ -107,6 +140,27 @@ def index(request):
                                           context_instance=RequestContext(request))
     else:
         files = os.listdir(os.path.join(settings.CUCKOO_PATH, "analyzer", "windows", "modules", "packages"))
+
+        db = Database()
+        machines = []
+        machine_list = db.list_machines()
+
+        # Prepare a list of VM names, description label based on tags
+        for m in machine_list:
+            tags = []
+
+            for t in m.tags:
+                tags.append(t.name)
+
+            if len(tags) > 0:
+                label = m.name + ": " + ", ".join(tags) 
+            else: label = m.name
+
+            machines.append((m.name, label))
+
+        # Prepend ALL/ANY options
+        machines.insert(0, ("","First Available"))
+        machines.insert(1, ("ALL","ALL"))
 
         packages = []
         for name in files:
@@ -117,5 +171,6 @@ def index(request):
             packages.append(name)
 
         return render_to_response("submission/index.html",
-                                  {"packages": sorted(packages)},
+                                  {"packages": sorted(packages),
+                                   "machines": machines,},
                                   context_instance=RequestContext(request))
