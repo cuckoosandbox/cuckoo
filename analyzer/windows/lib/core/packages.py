c2fc088d5008b2e2fe5d9a4c0eb41d5bfea843f6
Nex <nex@cuckoobox.org>
2014-01-30 01:16:12 +0100

    Removed composite file check

--- analyzer/windows/lib/core/packages.py
+++ analyzer/windows/lib/core/packages.py
@@ -21,7 +21,6 @@ def choose_package(file_type, file_name):
     elif "Rich Text Format" in file_type or \
          "Microsoft Word" in file_type or \
          "Microsoft Office Word" in file_type or \
-         ("Composite Document File" in file_type and not "Installer" in file_type) and \
          file_name.endswith(".docx") or \
          file_name.endswith(".doc") or \
          file_name.endswith(".rtf"):
