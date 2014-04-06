30e12bfd58d32d2c28852e6562265389720de501
jekil <alessandro@tanasi.it>
2014-04-07 01:43:13 +0200

    Get rid of Alembic shit, it was impossible to have portable statments

--- utils/db_migration/versions/from_0_6_to_1_1.py
+++ utils/db_migration/versions/from_0_6_to_1_1.py
@@ -20,6 +20,7 @@ import sys
 import sqlalchemy as sa
 
 from dateutil.parser import parse
+from datetime import datetime
 
 try:
     from alembic import op
@@ -77,80 +78,99 @@ def upgrade():
         # TODO: change default value, be aware sqlite doesn't support that kind of ALTER statement.
         op.add_column("machines", sa.Column("resultserver_port", sa.String(length=255), server_default="2042", nullable=False))
 
-        # Add columns to Task.
-        # We don"t provide a default value and leave the column as nullable because o further data migration.
-        op.add_column("tasks", sa.Column("clock", sa.DateTime(timezone=False),nullable=True))
-
-        # Edit task status enumeration in Task.
-        # NOTE: To workaround limitations in SQLite we have to create a temporary table, create the new schema and copy data.
-
-        # Read data.
-        tasks_data = []
-        old_tasks = conn.execute("select id, target, category, timeout, priority, custom, machine, package, options, platform, memory, enforce_timeout, added_on, started_on, completed_on, status, sample_id from tasks").fetchall()
-        for item in old_tasks:
-            d = {}
-            d["id"] = item[0]
-            d["target"] = item[1]
-            d["category"] = item[2]
-            d["timeout"] = item[3]
-            d["priority"] = item[4]
-            d["custom"] = item[5]
-            d["machine"] = item[6]
-            d["package"] = item[7]
-            d["options"] = item[8]
-            d["platform"] = item[9]
-            d["memory"] = item[10]
-            d["enforce_timeout"] = item[11]
-            d["added_on"] = parse(item[12])
-            if item[13]:
-                d["started_on"] = parse(item[13])
-            else:
-                d["started_on"] = item[13]
-            if item[14]:
-                d["completed_on"] = parse(item[14])
-            else:
-                d["completed_on"] = item[14]
-            d["status"] = item[15]
-            d["sample_id"] = item[16]
-
-            # Force clock.
+        # Deal with Alembic shit.
+        # Alembic is so ORMish that it was impossible to write code which works on different DBMS.
+        if conn.engine.driver == "psycopg2":
+            # We don"t provide a default value and leave the column as nullable because o further data migration.
+            op.add_column("tasks", sa.Column("clock", sa.DateTime(timezone=False),nullable=True))
             # NOTE: We added this new column so we force clock time to the added_on for old analyses.
-            d["clock"] = d["added_on"]
-            # Enum migration, "success" isn"t a valid state now.
-            if d["status"] == "success":
-                d["status"] = "completed"
-            tasks_data.append(d)
+            conn.execute("update tasks set clock=added_on")
+            # Add the not null constraint.
+            op.alter_column("tasks", "clock", nullable=False, existing_nullable=True)
+            # Altering status ENUM.
+            # This shit of raw SQL is here because alembic doesn't deal well with alter_colum of ENUM type.
+            op.execute('COMMIT') # Commit because SQLAlchemy doesn't support ALTER TYPE in a transaction.
+            conn.execute("ALTER TYPE status_type ADD VALUE 'completed'")
+            conn.execute("ALTER TYPE status_type ADD VALUE 'reported'")
+            conn.execute("ALTER TYPE status_type ADD VALUE 'recovered'")
+            conn.execute("ALTER TYPE status_type ADD VALUE 'running'")
+            conn.execute("ALTER TYPE status_type RENAME ATTRIBUTE success TO completed")
+            conn.execute("ALTER TYPE status_type DROP ATTRIBUTE IF EXISTS failure")
+        elif conn.engine.driver == "pysqlite":
+            # Edit task status enumeration in Task.
+            # NOTE: To workaround limitations in SQLite we have to create a temporary table, create the new schema and copy data.
+            # Read data.
+            tasks_data = []
+            old_tasks = conn.execute("select id, target, category, timeout, priority, custom, machine, package, options, platform, memory, enforce_timeout, added_on, started_on, completed_on, status, sample_id from tasks").fetchall()
+            for item in old_tasks:
+                d = {}
+                d["id"] = item[0]
+                d["target"] = item[1]
+                d["category"] = item[2]
+                d["timeout"] = item[3]
+                d["priority"] = item[4]
+                d["custom"] = item[5]
+                d["machine"] = item[6]
+                d["package"] = item[7]
+                d["options"] = item[8]
+                d["platform"] = item[9]
+                d["memory"] = item[10]
+                d["enforce_timeout"] = item[11]
+                if isinstance(item[12], datetime):
+                    d["added_on"] = item[12]
+                else:
+                    d["added_on"] = parse(item[12])
+                if isinstance(item[13], datetime):
+                    d["started_on"] = item[13]
+                else:
+                    d["started_on"] = parse(item[13])
+                if isinstance(item[14], datetime):
+                    d["completed_on"] = item[14]
+                else:
+                    d["completed_on"] = parse(item[14])
+                d["status"] = item[15]
+                d["sample_id"] = item[16]
 
-        # Rename original table.
-        op.rename_table("tasks", "old_tasks")
-        # Create new table with 1.0 schema.
-        op.create_table(
-            "tasks",
-            sa.Column("id", sa.Integer(), nullable=False),
-            sa.Column("target", sa.String(length=255), nullable=False),
-            sa.Column("category", sa.String(length=255), nullable=False),
-            sa.Column("timeout", sa.Integer(), server_default="0", nullable=False),
-            sa.Column("priority", sa.Integer(), server_default="1", nullable=False),
-            sa.Column("custom", sa.String(length=255), nullable=True),
-            sa.Column("machine", sa.String(length=255), nullable=True),
-            sa.Column("package", sa.String(length=255), nullable=True),
-            sa.Column("options", sa.String(length=255), nullable=True),
-            sa.Column("platform", sa.String(length=255), nullable=True),
-            sa.Column("memory", sa.Boolean(), nullable=False, default=False),
-            sa.Column("enforce_timeout", sa.Boolean(), nullable=False, default=False),
-            sa.Column("clock", sa.DateTime(timezone=False), server_default=sa.func.now(), nullable=False),
-            sa.Column("added_on", sa.DateTime(timezone=False), nullable=False),
-            sa.Column("started_on", sa.DateTime(timezone=False), nullable=True),
-            sa.Column("completed_on", sa.DateTime(timezone=False), nullable=True),
-            sa.Column("status", sa.Enum("pending", "running", "completed", "reported", "recovered", name="status_type"), server_default="pending", nullable=False),
-            sa.Column("sample_id", sa.Integer, sa.ForeignKey("samples.id"), nullable=True),
-            sa.PrimaryKeyConstraint("id")
-        )
+                # Force clock.
+                # NOTE: We added this new column so we force clock time to the added_on for old analyses.
+                d["clock"] = d["added_on"]
+                # Enum migration, "success" isn"t a valid state now.
+                if d["status"] == "success":
+                    d["status"] = "completed"
+                tasks_data.append(d)
 
-        # Insert data.
-        op.bulk_insert(db.Task.__table__, tasks_data)
-        # Drop old table.
-        op.drop_table("old_tasks")
+            # Rename original table.
+            op.rename_table("tasks", "old_tasks")
+            # Drop old table.
+            op.drop_table("old_tasks")
+            # Drop old Enum.
+            sa.Enum(name="status_type").drop(op.get_bind(), checkfirst=False)
+            # Create new table with 1.0 schema.
+            op.create_table(
+                "tasks",
+                sa.Column("id", sa.Integer(), nullable=False),
+                sa.Column("target", sa.String(length=255), nullable=False),
+                sa.Column("category", sa.String(length=255), nullable=False),
+                sa.Column("timeout", sa.Integer(), server_default="0", nullable=False),
+                sa.Column("priority", sa.Integer(), server_default="1", nullable=False),
+                sa.Column("custom", sa.String(length=255), nullable=True),
+                sa.Column("machine", sa.String(length=255), nullable=True),
+                sa.Column("package", sa.String(length=255), nullable=True),
+                sa.Column("options", sa.String(length=255), nullable=True),
+                sa.Column("platform", sa.String(length=255), nullable=True),
+                sa.Column("memory", sa.Boolean(), nullable=False, default=False),
+                sa.Column("enforce_timeout", sa.Boolean(), nullable=False, default=False),
+                sa.Column("clock", sa.DateTime(timezone=False), server_default=sa.func.now(), nullable=False),
+                sa.Column("added_on", sa.DateTime(timezone=False), nullable=False),
+                sa.Column("started_on", sa.DateTime(timezone=False), nullable=True),
+                sa.Column("completed_on", sa.DateTime(timezone=False), nullable=True),
+                sa.Column("status", sa.Enum("pending", "running", "completed", "reported", "recovered", name="status_type"), server_default="pending", nullable=False),
+                sa.Column("sample_id", sa.Integer, sa.ForeignKey("samples.id"), nullable=True),
+                sa.PrimaryKeyConstraint("id")
+            )
+
+            # Insert data.
+            op.bulk_insert(db.Task.__table__, tasks_data)
 
     # Migrate mongo.
     mongo_upgrade()
