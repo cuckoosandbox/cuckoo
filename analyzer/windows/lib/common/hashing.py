73bab3b7b820aebdb327285f1f23228e5e1cf109
jekil <alessandro@tanasi.it>
2014-05-20 22:56:49 +0200

    Added docstring

--- analyzer/windows/lib/common/hashing.py
+++ analyzer/windows/lib/common/hashing.py
@@ -6,6 +6,11 @@ BUFSIZE = 1024*1024
 
 
 def hash_file(method, path):
+    """Calculates an hash on a file by path.
+    @param method: callable hashing method
+    @param path: file path
+    @return: computed hash string
+    """
     f = open(path, "rb")
     h = method()
     while True:
