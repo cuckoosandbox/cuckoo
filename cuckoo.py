fb1bf476703bea802e7901b6745e5961c54c927c
Jurriaan Bremer <jurriaanbremer@gmail.com>
2014-06-10 14:50:25 +0200

    start on making cuckoo callable from within other projects

--- cuckoo.py
+++ cuckoo.py
@@ -3,13 +3,14 @@
 # This file is part of Cuckoo Sandbox - http://www.cuckoosandbox.org
 # See the file 'docs/LICENSE' for copying permission.
 
+import os
 import sys
 import logging
 import argparse
 
 try:
     from lib.cuckoo.common.logo import logo
-    from lib.cuckoo.common.constants import CUCKOO_VERSION
+    from lib.cuckoo.common.constants import CUCKOO_VERSION, CUCKOO_ROOT
     from lib.cuckoo.common.exceptions import CuckooCriticalError
     from lib.cuckoo.common.exceptions import CuckooDependencyError
     from lib.cuckoo.core.startup import check_working_directory, check_configs
@@ -22,22 +23,17 @@ except (CuckooDependencyError, ImportError) as e:
 
 log = logging.getLogger()
 
-def main():
+def cuckoo_main(quiet=False, debug=False, artwork=False, test=False):
+    cur_path = os.getcwd()
+    os.chdir(CUCKOO_ROOT)
+
     logo()
     check_working_directory()
     check_configs()
     check_version()
     create_structure()
 
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-q", "--quiet", help="Display only error messages", action="store_true", required=False)
-    parser.add_argument("-d", "--debug", help="Display debug messages", action="store_true", required=False)
-    parser.add_argument("-v", "--version", action="version", version="You are running Cuckoo Sandbox {0}".format(CUCKOO_VERSION))
-    parser.add_argument("-a", "--artwork", help="Show artwork", action="store_true", required=False)
-    parser.add_argument("-t", "--test", help="Test startup", action="store_true", required=False)
-    args = parser.parse_args()
-
-    if args.artwork:
+    if artwork:
         import time
         try:
             while True:
@@ -48,9 +44,9 @@ def main():
 
     init_logging()
 
-    if args.quiet:
+    if quiet:
         log.setLevel(logging.WARN)
-    elif args.debug:
+    elif debug:
         log.setLevel(logging.DEBUG)
 
     init_modules()
@@ -58,7 +54,7 @@ def main():
 
     # This is just a temporary hack, we need an actual test suite to integrate
     # with Travis-CI.
-    if args.test:
+    if test:
         return
 
     Resultserver()
@@ -69,9 +65,20 @@ def main():
     except KeyboardInterrupt:
         sched.stop()
 
+    os.chdir(cur_path)
+
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-q", "--quiet", help="Display only error messages", action="store_true", required=False)
+    parser.add_argument("-d", "--debug", help="Display debug messages", action="store_true", required=False)
+    parser.add_argument("-v", "--version", action="version", version="You are running Cuckoo Sandbox {0}".format(CUCKOO_VERSION))
+    parser.add_argument("-a", "--artwork", help="Show artwork", action="store_true", required=False)
+    parser.add_argument("-t", "--test", help="Test startup", action="store_true", required=False)
+    args = parser.parse_args()
+
     try:
-        main()
+        cuckoo_main(quiet=args.quiet, debug=args.debug, artwork=args.artwork,
+                    test=args.test)
     except CuckooCriticalError as e:
         message = "{0}: {1}".format(e.__class__.__name__, e)
         if len(log.handlers) > 0:
