c4734149ec1e21795633c3b0a4a6219e78281b17
Jurriaan Bremer <jurriaanbremer@gmail.com>
2014-06-12 05:37:20 +0200

    transform the integrity script to use the unittest library

--- tests/integrity.py
+++ tests/integrity.py
@@ -11,6 +11,7 @@ import os.path
 import subprocess
 import sys
 import tempfile
+import unittest
 
 CUCKOO_ROOT = os.path.join(os.path.abspath(os.path.dirname(__file__)), "..")
 sys.path.append(CUCKOO_ROOT)
@@ -20,19 +21,52 @@ from cuckoo import cuckoo_init, cuckoo_main
 
 log = logging.getLogger()
 
-def run_analysis(src):
-    _, path = tempfile.mkstemp(suffix='.py')
-    open(path, 'wb').write(src)
-    db.add_path(file_path=path, package='python', timeout=30)
 
-    try:
-        cuckoo_main(max_analysis_count=1)
-    except Exception as e:
-        print 'Error running analysis..', e
+class TestIntegrity(unittest.TestCase):
+    def run_analysis(self, src):
+        _, path = tempfile.mkstemp(suffix='.py')
+        # A simple if statement to fix indentation errors.
+        open(path, 'wb').write('if True:\n' + src)
+        db.add_path(file_path=path, package='python', timeout=30)
 
-    report = os.path.join(CUCKOO_ROOT, 'storage', 'analyses',
-                          'latest', 'reports', 'report.json')
-    return json.load(open(report, 'rb'))
+        try:
+            cuckoo_main(max_analysis_count=1)
+        except Exception as e:
+            print 'Error running analysis..', e
+
+        report = os.path.join(CUCKOO_ROOT, 'storage', 'analyses',
+                              'latest', 'reports', 'report.json')
+        return json.load(open(report, 'rb'))
+
+    def test_hello_world(self):
+        report = self.run_analysis("""
+            import time
+            print 'hello!!1'
+            time.sleep(3)
+        """)
+        self.assertTrue('behavior' in report)
+        self.assertTrue('processes' in report['behavior'])
+        self.assertEqual(len(report['behavior']['processes']), 1)
+
+        buf = ''
+        for row in report['behavior']['processes'][0]['calls']:
+            if row['api'] != 'WriteConsoleA':
+                continue
+
+            args = dict((arg['name'], arg['value']) for arg in row['arguments'])
+            buf += args['Buffer']
+
+        self.assertEqual(buf, 'hello!!1\r\n')
+
+    def test_file_write(self):
+        report = self.run_analysis("""
+            open('a.txt', 'wb').write('Hello World')
+        """)
+        self.assertTrue('dropped' in report)
+        self.assertEqual(len(report['dropped']), 1)
+        self.assertEqual(open(report['dropped'][0]['path']).read(),
+                         'Hello World')
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -43,29 +77,8 @@ if __name__ == '__main__':
     subprocess.Popen([os.path.join(CUCKOO_ROOT, 'utils', 'clean.sh')],
                      cwd=CUCKOO_ROOT).wait()
 
-    cuckoo_init()
+    cuckoo_init(quiet=True)
 
     db = Database()
 
-    report = run_analysis("import time; print 'hello!!1'; time.sleep(3)")
-    assert 'behavior' in report
-    assert 'processes' in report['behavior']
-    assert len(report['behavior']['processes']) == 1
-
-    buf = ''
-    for row in report['behavior']['processes'][0]['calls']:
-        if row['api'] != 'WriteConsoleA':
-            continue
-
-        args = dict((arg['name'], arg['value']) for arg in row['arguments'])
-        buf += args['Buffer']
-
-    assert buf == 'hello!!1\r\n'
-    log.info('Passed first test!')
-
-    report = run_analysis("open('a.txt', 'wb').write('Hello World')")
-    assert 'dropped' in report
-    assert len(report['dropped']) == 1
-    assert open(report['dropped'][0]['path']).read() == 'Hello World'
-
-    log.info('Passed second test!')
+    unittest.main()
