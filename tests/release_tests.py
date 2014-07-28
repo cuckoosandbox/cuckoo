f93f9d8d6b89a7e0e800f67d57ad1579b5dd873c
Tobias Jarmuzek <tobias.jarmuzek@rwth-aachen.de>
2014-07-28 13:08:57 +0200

    pdf package test added

--- tests/release_tests.py
+++ tests/release_tests.py
@@ -18,19 +18,17 @@ import threading
 
 CUCKOO_ROOT = os.path.join(os.path.abspath(os.path.dirname(__file__)), "..")
 sys.path.append(CUCKOO_ROOT)
-
 from lib.cuckoo.core.database import Database
 from cuckoo import cuckoo_init, cuckoo_main
-
 log = logging.getLogger()
 
 
 class TestRelease(unittest.TestCase):
-    def run_analysis(self, files, packages, url=None):
+    def run_analysis(self, file, package, url=None):
         if url:
-            db.add_url(url=url, package="ie", timeout=30)
+            db.add_url(url=url, package="ie", timeout=args.timeout)
         else:
-            db.add_path(file_path=files, package=packages, timeout=30)
+            db.add_path(file_path=file, package=package, timeout=args.timeout)
 
         try:
             cuckoo_main(max_analysis_count=1)
@@ -62,15 +60,25 @@ class TestRelease(unittest.TestCase):
         for d in hosts:                    
             self.assertTrue(d in host_lookups, "Host %s not looked up" %(d))
 
+    # check for network connections in report
+    def check_network(self, report, network_items):
+        for p in report["behavior"]["processes"]:
+            for c in p.get("calls"):
+                if c["category"] == "network":
+                    for a in c["arguments"]:
+                        try:
+                            network_items.remove({c["api"]:{a["name"]:a["value"]}})
+                        except:
+                            pass
+        self.assertFalse(network_items, "Not all network items found. Not found: %s" %(network_items))
+
 
     # verify dns requests in report
     def check_registry(self, report, reg_items, reg_keys):
-        reg_items_accessed = []
         for p in report["behavior"]["processes"]:
             for c in p.get("calls"):
                 if c["category"] == "registry":
                     for a in c["arguments"]:
-                        reg_items_accessed.append({c["api"]:{a["name"]:a["value"]}})
                         try:
                             reg_items.remove({c["api"]:{a["name"]:a["value"]}})
                         except:
@@ -88,7 +96,7 @@ class TestRelease(unittest.TestCase):
         report = self.run_analysis(os.path.abspath("test_samples/python.py"), "python")
 
         # verify that a process has been injected at all
-        self.assertTrue(report.get("behavior").get("processes") != [])
+        self.assertTrue(report.get("behavior").get("processes") != [], "No injected process found.")
 
         # check for loaded dlls
         self.check_loaded_dlls(report, ["kernel32","msvcrt"])
@@ -106,19 +114,21 @@ class TestRelease(unittest.TestCase):
 
     # analysis test for the Internet Explorer analysis package
     def test_ie(self):
+        # start http server serving the exploit
         host_ip = "192.168.56.1"
         host_port = 8089
-
-        # start http server serving the exploit
         handler = SimpleHTTPServer.SimpleHTTPRequestHandler
         httpd = SocketServer.TCPServer(("", host_port), handler)
         server_thread = threading.Thread(target=httpd.serve_forever)
         server_thread.daemon = True
         server_thread.start()
-        
 
+        # start analysis
         report = self.run_analysis("", "ie", "http://%s:%d/tests/test_samples/ie_exploit.html" %(host_ip, host_port))
         httpd.shutdown()
+
+        # verify that a process has been injected at all
+        self.assertTrue(report.get("behavior").get("processes") != [], "No injected process found.")
 
         self.assertTrue(False, "Not yet implemented")
 
@@ -128,24 +138,51 @@ class TestRelease(unittest.TestCase):
 
     # analysis test for the pdf analysis package
     def test_pdf(self):
+        host_ip = "192.168.56.1"
+        host_port = 8089
+        handler = SimpleHTTPServer.SimpleHTTPRequestHandler
+        httpd = SocketServer.TCPServer(("", host_port), handler)
+        server_thread = threading.Thread(target=httpd.serve_forever)
+        server_thread.daemon = True
+        server_thread.start()
+
+        report = self.run_analysis(os.path.abspath("test_samples/dl_exe.pdf"), "pdf")
+        httpd.shutdown()
+
+        # verify that a process has been injected at all
+        self.assertTrue(report.get("behavior").get("processes") != [], "No injected process found.")
+
+        self.check_network(report, [
+                {"InternetConnectA":{"ServerName":"192.168.56.1"}}, 
+                {"InternetConnectA":{"ServerPort":"8089"}}
+            ])
+
+
+    # analysis test for the doc analysis package
+    def test_doc(self):
+        self.assertTrue(False, "Not yet implemented")
+
+    # analysis test for the xls analysis package
+    def test_xls(self):
         self.assertTrue(False, "Not yet implemented")
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--packages", type=str, nargs="+", help="Test against specific packages only.")
-    parser.add_argument("--verbosity", type=int, default=2, help="Set test-verbosity.")
-    parser.add_argument("--clean", type=bool, default=False, help="Clean storage before tests.")
+    parser.add_argument("-p", "--packages", type=str, nargs="+", help="Test against specific packages only.")
+    parser.add_argument("-v", "--verbosity", type=int, default=0, help="Set test-verbosity.")
+    parser.add_argument("-t", "--timeout", type=int, default=30, help="Sets the analysis timeout.")
+    parser.add_argument("-c", "--clean", action="store_true", help="Clean storage before tests.")
     args = parser.parse_args()
 
     if args.clean:
         subprocess.Popen([os.path.join(CUCKOO_ROOT, 'utils', 'clean.sh')],
             cwd=CUCKOO_ROOT).wait()
 
-    # init cuckoo
-    cuckoo_init(quiet=False)
-    db = Database()
 
+    # init cuckoo
+    cuckoo_init(quiet=True if args.verbosity == 0 else False)
+    db = Database()
 
     # run test suite
     if (args.packages):
@@ -153,7 +190,6 @@ if __name__ == "__main__":
         for p in args.packages:
             suite.addTest(TestRelease("test_%s" %(p)))
         runner = unittest.TextTestRunner(verbosity=args.verbosity).run(suite)
-
     else:
         suite = unittest.TestLoader().loadTestsFromTestCase(TestRelease)    
         unittest.TextTestRunner(verbosity=args.verbosity).run(suite)
