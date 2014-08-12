a787f8d0a5f3563155132d9a7705ae9590543e87
Jurriaan Bremer <jurriaanbremer@gmail.com>
2014-08-12 05:12:30 +0200

    bson sanity check

--- cuckoo.py
+++ cuckoo.py
@@ -15,7 +15,8 @@ try:
     from lib.cuckoo.common.exceptions import CuckooDependencyError
     from lib.cuckoo.core.startup import check_working_directory, check_configs
     from lib.cuckoo.core.startup import check_version, create_structure
-    from lib.cuckoo.core.startup import init_logging, init_modules, init_tasks, init_yara
+    from lib.cuckoo.core.startup import init_logging, init_modules
+    from lib.cuckoo.core.startup import init_tasks, init_yara, sanity_checks
     from lib.cuckoo.core.scheduler import Scheduler
     from lib.cuckoo.core.resultserver import ResultServer
 except (CuckooDependencyError, ImportError) as e:
@@ -52,6 +53,8 @@ def cuckoo_init(quiet=False, debug=False, artwork=False, test=False):
     init_modules()
     init_tasks()
     init_yara()
+
+    sanity_checks()
 
     # This is just a temporary hack, we need an actual test suite to integrate
     # with Travis-CI.
