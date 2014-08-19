2efdd458b52ccf2e3f7b86b53f92993f2b90c71f
jekil <alessandro@tanasi.it>
2014-08-19 20:26:36 +0200

    Changed message

--- lib/cuckoo/core/scheduler.py
+++ lib/cuckoo/core/scheduler.py
@@ -466,10 +466,10 @@ class Scheduler:
                          "PostgreSQL or MySQL when running multiple VMs.")
 
         if len(machinery.machines()) > 3 and self.cfg.cuckoo.process_results:
-            log.warning("When running multiple virtual machines it is "
-                        "recommended to process the results in a separate "
-                        "process to increase throughput and stability. Please "
-                        "read the documentation on ./utils/process.py.")
+            log.warning("When running many virtual machines it is recommended "
+                        "to process the results in a separate process.py to "
+                        "increase throughput and stability. Please read the "
+                        "documentation about the `Processing Utility`.")
 
     def stop(self):
         """Stop scheduler."""
