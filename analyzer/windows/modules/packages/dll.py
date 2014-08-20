01addf52f164180d066a81cef0c34be1918cb057
Nex <nex@nex.sx>
2014-08-20 11:54:37 +0200

    Rename file with .dll extension if missing (Close: #354)

--- analyzer/windows/modules/packages/dll.py
+++ analyzer/windows/modules/packages/dll.py
@@ -2,6 +2,8 @@
 # This file is part of Cuckoo Sandbox - http://www.cuckoosandbox.org
 # See the file 'docs/LICENSE' for copying permission.
 
+import os
+
 from lib.common.abstracts import Package
 
 class Dll(Package):
@@ -14,6 +16,16 @@ class Dll(Package):
         rundll32 = self.get_path("rundll32.exe")
         function = self.options.get("function", "DllMain")
         arguments = self.options.get("arguments")
+
+        # Check file extension.
+        ext = os.path.splitext(path)[-1].lower()
+        # If the file doesn't have the proper .dll extension force it
+        # and rename it. This is needed for rundll32 to execute correctly.
+        # See ticket #354 for details.
+        if ext != ".dll":
+            new_path = path + ".dll"
+            os.rename(path, new_path)
+            path = new_path
 
         args = "{0},{1}".format(path, function)
         if arguments:
