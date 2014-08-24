8ae793bb3caf31bbbb548b8e0286ea9ae787141a
jekil <alessandro@tanasi.it>
2014-08-24 19:56:24 +0200

    Message whatever

--- lib/cuckoo/core/database.py
+++ lib/cuckoo/core/database.py
@@ -384,8 +384,9 @@ class Database(object):
             self.engine = create_engine(connection_string, poolclass=NullPool)
         except ImportError as e:
             lib = e.message.split()[-1]
-            raise CuckooDependencyError("Unable to import %s (install with "
-                                        "`pip install %s`)" % (lib, lib))
+            raise CuckooDependencyError("Missing database driver, unable to "
+                                        "import %s (install with `pip "
+                                        "install %s`)" % (lib, lib))
 
     def _get_or_create(self, session, model, **kwargs):
         """Get an ORM instance or create it if not exist.
