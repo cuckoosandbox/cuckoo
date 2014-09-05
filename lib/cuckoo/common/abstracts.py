07a61c7b0e912ba88953761efad7fa587445a498
Jurriaan Bremer <jurriaanbremer@gmail.com>
2014-09-05 21:11:43 +0200

    remove unused import

--- lib/cuckoo/common/abstracts.py
+++ lib/cuckoo/common/abstracts.py
@@ -10,7 +10,6 @@ import time
 import xml.etree.ElementTree as ET
 
 from lib.cuckoo.common.config import Config
-from lib.cuckoo.common.constants import CUCKOO_ROOT
 from lib.cuckoo.common.exceptions import CuckooCriticalError
 from lib.cuckoo.common.exceptions import CuckooMachineError
 from lib.cuckoo.common.exceptions import CuckooOperationalError
