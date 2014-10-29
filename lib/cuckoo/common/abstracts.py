4e05fb525239f928155ee4a591a47450dd717999
Thorsten Sick <thorsten.sick@avira.com>
2014-10-29 16:09:50 +0100

    enhanced getting argument

--- lib/cuckoo/common/abstracts.py
+++ lib/cuckoo/common/abstracts.py
@@ -989,10 +989,13 @@ class Signature(object):
         """Retrieves the value of a specific argument from an API call.
         @param call: API call object.
         @param name: name of the argument to retrieve.
-        @return: value of the requried argument.
+        @return: value of the argument or None
         """
 
-        return call["arguments"][name]
+        try:
+            return call["arguments"][name]
+        except KeyError:
+            return None
 
 
     def quickout(self):
