5c0ac828c09cb9a926ee81f3474de3882cb4ff95
Thorsten Sick <thorsten.sick@avira.com>
2014-11-14 09:10:35 +0100

    Full split for more flexibility later when adding features

--- utils/setup.sh
+++ utils/setup.sh
@@ -17,74 +17,11 @@ if [ "$(id -u)" -ne 0 ]; then
     exit 1
 fi
 
-# Update apt repository and install required packages.
-apt-get update -y
-apt-get install -y sudo git python-dev python-pip postgresql libpq-dev \
-    python-dpkt vim tcpdump libcap2-bin genisoimage
-
-# Install the most up-to-date version of VirtualBox available at the moment.
-if [ ! -e "/usr/bin/VirtualBox" ]; then
-    # Update our apt repository with "contrib".
-    DEBVERSION="$(lsb_release -cs)"
-    echo "deb http://download.virtualbox.org/virtualbox/debian " \
-        "$DEBVERSION contrib" >> /etc/apt/sources.list
-
-    # Add the VirtualBox public key to our apt repository.
-    wget -q https://www.virtualbox.org/download/oracle_vbox.asc \
-        -O- | apt-key add -
-
-    # Install the most recent VirtualBox.
-    apt-get update -y
-    apt-get install -y virtualbox-4.3
-fi
-
-# Allow tcpdump to dump packet captures when executed as a normal user.
-setcap cap_net_raw,cap_net_admin=eip /usr/sbin/tcpdump
-
-# Setup a Cuckoo user.
-useradd cuckoo
-
-CUCKOO="/home/cuckoo/cuckoo/"
-VMTEMP="$(mktemp -d "/home/cuckoo/XXXXXX")"
-
-# Fetch Cuckoo and VMCloak.
-git clone git://github.com/cuckoobox/cuckoo.git "$CUCKOO"
-
-mkdir -p "$VMTEMP"
-
-chown -R cuckoo:cuckoo "/home/cuckoo/" "$CUCKOO" "$VMTEMP"
-chmod 755 "/home/cuckoo/" "$CUCKOO" "$VMTEMP"
-
-# Install required packages part two.
-pip install psycopg2 vmcloak -r "$CUCKOO/requirements.txt"
-
-# Create a random password.
-PASSWORD="$(tr -dc "[:alnum:]" < /dev/urandom|head -c ${1:-16})"
-
-sql_query() {
-    echo "$1"|sudo -u postgres psql
-}
-
-sql_query "DROP DATABASE cuckoo"
-sql_query "CREATE DATABASE cuckoo"
-
-sql_query "DROP USER cuckoo"
-sql_query "CREATE USER cuckoo WITH PASSWORD '$PASSWORD'"
-
-# Setup the VirtualBox hostonly network.
-if [ -z "$(VBoxManage list hostonlyifs)" ]; then
-    VBoxManage hostonlyif create
-    VBoxManage hostonlyif ipconfig vboxnet0 --ip 192.168.56.1
-fi
-
-# Run the magic iptables script that turns hostonly networks into full
-# internet access.
-vmcloak-iptables
+./install.sh
 
 if [ "$#" -eq 4 ]; then
     ./vmcloak-setup.sh "$@"
 fi
 
 
-echo "PostgreSQL connection string:  " \
-    "postgresql://cuckoo:$PASSWORD@localhost/cuckoo"
+
