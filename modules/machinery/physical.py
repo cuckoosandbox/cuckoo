e1d946edb575c54588938ce2749b855b7806e0c7
Jurriaan Bremer <jurriaanbremer@gmail.com>
2014-11-17 23:58:31 +0100

    code cleanup

--- modules/machinery/physical.py
+++ modules/machinery/physical.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2012-2013 The MITRE Corporation.
+# Copyright (C) 2012-2014 The MITRE Corporation.
 # This file is part of Cuckoo Sandbox - http://www.cuckoosandbox.org
 # See the file 'docs/LICENSE' for copying permission.
 
@@ -9,8 +9,8 @@ import subprocess
 
 from lib.cuckoo.core.guest import GuestManager
 from lib.cuckoo.common.abstracts import Machinery
-from lib.cuckoo.common.exceptions import CuckooMachineError
 from lib.cuckoo.common.exceptions import CuckooCriticalError
+from lib.cuckoo.common.exceptions import CuckooMachineError
 
 log = logging.getLogger(__name__)
 
@@ -25,7 +25,8 @@ class Physical(Machinery):
 
     def _initialize_check(self):
         """Ensures that credentials have been entered into the config file.
-        @raise CuckooMachineError: if VBoxManage is not found.
+        @raise CuckooCriticalError: if no credentials were provided or if
+            one or more physical machines are offline.
         """
         if not self.options.physical.user or not self.options.physical.password:
             raise CuckooCriticalError("Physical machine credentials are missing, please add it to the config file")
@@ -35,10 +36,10 @@ class Physical(Machinery):
                 raise CuckooCriticalError("Physical machine is currently offline")
 
     def _get_machine(self, label):
-        """Retreive all machine info given a machine's name.
+        """Retrieve all machine info given a machine's name.
         @param label: machine name.
         @return: machine dictionary (id, ip, platform, ...).
-        @raises CuckooMachineError: if no machine with given label.
+        @raises CuckooMachineError: if no machine is available with the given label.
         """
         for m in self.machines():
             if label == m.label:
@@ -52,16 +53,16 @@ class Physical(Machinery):
         @raise CuckooMachineError: if unable to start.
         """
         # Check to ensure a given machine is running
-        log.debug("Checking if %s is running." % label)
+        log.debug("Checking if machine %r is running.", label)
         status = self._status(label)
         if status == self.RUNNING:
-            log.debug("Machine already running: %s." % label)
+            log.debug("Machine already running: %s.", label)
 
         elif status == self.STOPPED:
             self._wait_status(label, self.RUNNING)
 
         else:
-            raise CuckooMachineError("Error occured while starting: "
+            raise CuckooMachineError("Error occurred while starting: "
                                      "%s (STATUS=%s)" % (label, status))
 
     def stop(self, label):
@@ -73,18 +74,22 @@ class Physical(Machinery):
         # actually be rebooted to kick off the re-imaging process
         n = self.options.physical.user
         p = self.options.physical.password
-        creds = str(n) + "%" + str(p)
+        creds = "%s%%%s" % (n, p)
         status = self._status(label)
 
         if status == self.RUNNING:
-            log.debug("Rebooting machine: %s." % label)
+            log.debug("Rebooting machine: %s.", label)
             machine = self._get_machine(label)
-            shutdown = subprocess.Popen(["net", "rpc", "shutdown", "-I", machine.ip, "-U", creds, "-r", "-f", "--timeout=5"], stdout=subprocess.PIPE)
-            output = shutdown.communicate()[0]
+
+            args = [
+                "net", "rpc", "shutdown", "-I", machine.ip,
+                "-U", creds, "-r", "-f", "--timeout=5"
+            ]
+            shutdown = subprocess.Popen(args, stdout=subprocess.PIPE)
+            output, _ = shutdown.communicate()
 
             if "Shutdown of remote machine succeeded" not in output:
                 raise CuckooMachineError("Unable to initiate RPC request")
-
             else:
                 log.debug("Reboot success: %s." % label)
 
@@ -100,14 +105,14 @@ class Physical(Machinery):
         return active_machines
 
     def _status(self, label):
-        """Gets current status of a vm.
-        @param label: virtual machine name.
+        """Gets current status of a physical machine.
+        @param label: physical machine name.
         @return: status string.
         """
         # For physical machines, the agent can either be contacted or not.
         # However, there is some information to be garnered from potential
         # exceptions.
-        log.debug("Getting status for machine: %s." % label)
+        log.debug("Getting status for machine: %s.", label)
         machine = self._get_machine(label)
         guest = GuestManager(machine.id, machine.ip, machine.platform)
 
@@ -118,25 +123,23 @@ class Physical(Machinery):
         else:
             try:
                 status = guest.server.get_status()
-
             except xmlrpclib.Fault as e:
-                # Contacted Agent, but it threw an error
-                log.debug("Agent error: %s (%s) (Error: %s)."
-                          % (machine.id, machine.ip, e))
+                # Contacted Agent, but it threw an error.
+                log.debug("Agent error: %s (%s) (Error: %s).",
+                          machine.id, machine.ip, e)
                 return self.ERROR
-
             except socket.error as e:
-                # Could not contact agent
-                log.debug("Agent unresponsive: %s (%s) (Error: %s)."
-                          % (machine.id, machine.ip, e))
+                # Could not contact agent.
+                log.debug("Agent unresponsive: %s (%s) (Error: %s).",
+                          machine.id, machine.ip, e)
                 return self.STOPPED
-
             except Exception as e:
-                # TODO: Handle this better
-                log.debug("Received unknown exception: %s." % e)
+                # TODO Handle this better.
+                log.debug("Received unknown exception: %s.", e)
                 return self.ERROR
 
-        # If the agent responded successfully, the machine is running
+        # If the agent responded successfully, then the physical machine
+        # is running
         if status:
             return self.RUNNING
 
