d6e5d1bfc411352e49535e096db40457c8aaa3ad
Jurriaan Bremer <jurriaanbremer@gmail.com>
2014-12-01 09:36:00 +0100

    only use upstart under ubuntu

--- utils/service.sh
+++ utils/service.sh
@@ -3,7 +3,7 @@
 # TODO Pre-start run vmcloak-vboxnet0.
 # TODO Load Virtual Machines into tmpfs, if enabled.
 
-_install() {
+_install_upstart() {
     cat > /etc/init/cuckoo.conf << EOF
 # Cuckoo daemon service.
 
@@ -37,23 +37,35 @@ exec ./utils/process.py auto 2>> log/process.log
 EOF
 }
 
-_remove() {
+_remove_upstart() {
     rm -f /etc/init/cuckoo.conf
     rm -f /etc/init/cuckoo-api.conf
     rm -f /etc/init/cuckoo-process.conf
 }
 
-_reload() {
+_reload_upstart() {
     initctl reload-configuration
 }
 
-_start() {
+_start_upstart() {
     initctl start cuckoo
 }
 
-_stop() {
+_stop_upstart() {
     initctl stop cuckoo
 }
+
+if [ "$(lsb_release -is)" = "Ubuntu" ]; then
+    echo "Using Upstart.."
+    _install=_install_upstart
+    _remove=_remove_upstart
+    _reload=_reload_upstart
+    _start=_start_upstart
+    _stop=_stop_upstart
+else
+    echo "Unsupported Linux distribution.."
+    exit 1
+fi
 
 if [ "$#" -eq 0 ]; then
     echo "Usage: $0 <install|remove|start|stop>"
