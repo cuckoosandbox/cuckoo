2b781d7a838c52e6afb55bf344701006826ec723
jekil <alessandro@tanasi.it>
2015-01-07 00:32:05 +0100

    Happy new year

--- modules/machinery/vmware.py
+++ modules/machinery/vmware.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2010-2014 Cuckoo Foundation.
+# Copyright (C) 2010-2015 Cuckoo Foundation.
 # This file was originally produced by Mike Tu.
 # This file is part of Cuckoo Sandbox - http://www.cuckoosandbox.org
 # See the file 'docs/LICENSE' for copying permission.
--- utils/dist.py
+++ utils/dist.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# Copyright (C) 2010-2014 Cuckoo Foundation.
+# Copyright (C) 2010-2015 Cuckoo Foundation.
 # This file is part of Cuckoo Sandbox - http://www.cuckoosandbox.org
 # See the file 'docs/LICENSE' for copying permission.
 
