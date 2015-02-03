d6f062a6d2b84395de5014afe317d5d975808804
jekil <alessandro@tanasi.it>
2015-02-03 23:42:46 +0100

    Added more python paths

--- analyzer/windows/modules/packages/python.py
+++ analyzer/windows/modules/packages/python.py
@@ -6,8 +6,15 @@ from lib.common.abstracts import Package
 
 class Python(Package):
     """Python analysis package."""
+
     PATHS = [
+        ("HomeDrive", "Python24", "python.exe"),
+        ("HomeDrive", "Python25", "python.exe"),
+        ("HomeDrive", "Python26", "python.exe"),
         ("HomeDrive", "Python27", "python.exe"),
+        ("HomeDrive", "Python32", "python.exe"),
+        ("HomeDrive", "Python33", "python.exe"),
+        ("HomeDrive", "Python34", "python.exe"),
     ]
 
     def start(self, path):
