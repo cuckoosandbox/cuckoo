b945f04e7e850551a522376524e44129ac0f9dea
jekil <alessandro@tanasi.it>
2015-02-10 00:09:07 +0100

    Fixed test

--- tests/database_tests.py
+++ tests/database_tests.py
@@ -4,36 +4,31 @@
 
 import tempfile
 
-from nose.tools import assert_equal, raises, assert_not_equal
+from sqlalchemy.exc import OperationalError
+from nose.tools import assert_equal, raises
 
-from lib.cuckoo.core.database import Database, Machine, Sample, Task
+from lib.cuckoo.core.database import Database, Sample, Task
 
 
-class TestDatabase:
+class TestDropDatabase:
+    """Tests database creation, adding a couple of tasks and dropping the db."""
 
     def setUp(self):
         self.d = Database(dsn="sqlite://")
 
-    def test_machine_add_clean(self):
-        # Add.
-        self.d.add_machine("a", "a", "1.1.1.1", "win", "", "", "", "", "")
-        session = self.d.Session()
-        assert_equal(session.query(Machine).count(), 1)
-        # Delete.
-        self.d.clean_machines()
-        assert_equal(session.query(Machine).count(), 0)
-
-    def test_task_add_del(self):
-        # Add.
+    # When dropping a db, any other query raises a (OperationalError) no such table.
+    @raises(OperationalError)
+    def test_drop(self):
+        # Add task.
         sample_path = tempfile.mkstemp()[1]
         self.d.add_path(sample_path)
         session = self.d.Session()
         assert_equal(session.query(Sample).count(), 1)
         assert_equal(session.query(Task).count(), 1)
-        # Drop tasks.
-        self.d.drop_tasks()
-        assert_equal(session.query(Task).count(), 0)
+        # Add url.
+        self.d.add_url("http://foo.bar")
         assert_equal(session.query(Sample).count(), 1)
-        # Drop samples.
-        self.d.drop_samples()
-        assert_equal(session.query(Sample).count(), 0)
+        assert_equal(session.query(Task).count(), 2)
+        # Drop.
+        self.d.drop()
+        assert_equal(session.query(Task).count(), 0)
