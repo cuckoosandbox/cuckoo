12e785e34571d69c59a456bf2e53f5ed64c508da
jekil <alessandro@tanasi.it>
2015-02-15 01:30:32 +0100

    Now when interface is using the custom mongo database name in reporting.conf

--- web/analysis/views.py
+++ web/analysis/views.py
@@ -23,7 +23,7 @@ sys.path.append(settings.CUCKOO_PATH)
 from lib.cuckoo.core.database import Database, TASK_PENDING
 from lib.cuckoo.common.constants import CUCKOO_ROOT
 
-results_db = pymongo.MongoClient(settings.MONGO_HOST, settings.MONGO_PORT).cuckoo
+results_db = pymongo.MongoClient(settings.MONGO_HOST, settings.MONGO_PORT)[settings.MONGO_DB]
 fs = GridFS(results_db)
 
 @require_safe
