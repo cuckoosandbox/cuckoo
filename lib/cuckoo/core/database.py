745ebf0fda34b7909f5d52777437802de940ffb7
jekil <alessandro@tanasi.it>
2015-03-22 23:13:07 +0100

    Fixed database revision id forgotten in the owner migration

--- lib/cuckoo/core/database.py
+++ lib/cuckoo/core/database.py
@@ -29,7 +29,7 @@ except ImportError:
 
 log = logging.getLogger(__name__)
 
-SCHEMA_VERSION = "495d5a6edef3"
+SCHEMA_VERSION = "3aa42d870199"
 TASK_PENDING = "pending"
 TASK_RUNNING = "running"
 TASK_COMPLETED = "completed"
