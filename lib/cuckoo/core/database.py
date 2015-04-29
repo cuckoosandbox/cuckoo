e8a5364d9717905ee39efecbf83eda8986d4762f
Jurriaan Bremer <jurriaanbremer@gmail.com>
2015-04-29 09:28:15 +0200

    code cleanup

--- lib/cuckoo/core/database.py
+++ lib/cuckoo/core/database.py
@@ -521,7 +521,7 @@ class Database(object):
         row = None
         try:
             if machine != "":
-                row = session.query(Task).filter_by(status=TASK_PENDING).filter(Machine.name==machine).order_by(Task.priority.desc(), Task.added_on.asc()).first()
+                row = session.query(Task).filter_by(status=TASK_PENDING).filter(Machine.name == machine).order_by(Task.priority.desc(), Task.added_on.asc()).first()
             else:
                 row = session.query(Task).filter_by(status=TASK_PENDING).order_by(Task.priority.desc(), Task.added_on.asc()).first()
 
