4683c2537b995e7e22f586bd25e9b5c24e68c7d9
Dmitry Rodionov <i.am.rodionovd@gmail.com>
2015-05-06 21:32:35 +0600

    Add a dummy darwin analyzer

---
+++ analyzer/darwin/analyzer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# Copyright (C) 2015 Dmitry Rodionov
+# This file is part of my GSoC'15 project for Cuckoo Sandbox:
+#	http://www.cuckoosandbox.org
+# This software may be modified and distributed under the terms
+# of the MIT license. See the LICENSE file for details.
+
+if __name__ == "__main__":
+	pass
