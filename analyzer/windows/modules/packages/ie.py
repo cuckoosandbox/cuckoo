ea84c62e065bb9b4f4d2a8c3ebbac1cca4645977
Jurriaan Bremer <jurriaanbremer@gmail.com>
2015-05-12 16:44:34 +0200

    generalize registry key initialization

--- analyzer/windows/modules/packages/ie.py
+++ analyzer/windows/modules/packages/ie.py
@@ -2,14 +2,9 @@
 # This file is part of Cuckoo Sandbox - http://www.cuckoosandbox.org
 # See the file 'docs/LICENSE' for copying permission.
 
-from _winreg import OpenKey, SetValueEx, CloseKey
 from _winreg import HKEY_LOCAL_MACHINE, HKEY_CURRENT_USER
-from _winreg import KEY_SET_VALUE, REG_DWORD, REG_SZ
 
 from lib.common.abstracts import Package
-from lib.common.exceptions import CuckooPackageError
-
-IESETTINGS = "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Internet Settings"
 
 class IE(Package):
     """Internet Explorer analysis package."""
@@ -59,38 +54,24 @@ class IE(Package):
         ],
     ]
 
-    def init_iexplore(self):
-        """Sets various Internet Explorer related registry settings in case
-        the user has not taken care of it already."""
-        for rootkey, subkey, values in self.REGKEYS:
-            key_handle = OpenKey(rootkey, subkey, 0, KEY_SET_VALUE)
-
-            for key, value in values.items():
-                if isinstance(value, str):
-                    SetValueEx(key_handle, key, 0, REG_SZ, value)
-                elif isinstance(value, int):
-                    SetValueEx(key_handle, key, 0, REG_DWORD, value)
-                else:
-                    raise CuckooPackageError("Invalid value type: %r" % value)
-
-            CloseKey(key_handle)
-
     def setup_proxy(self, proxy_host):
         """Configure Internet Explorer to route all traffic through a
         proxy."""
-        key = OpenKey(HKEY_CURRENT_USER, IESETTINGS, 0, KEY_SET_VALUE)
-        SetValueEx(key, "MigrateProxy", 0, REG_DWORD, 1)
-        SetValueEx(key, "ProxyEnable", 0, REG_DWORD, 1)
-        SetValueEx(key, "ProxyHttp1.1", 0, REG_DWORD, 0)
-        SetValueEx(key, "ProxyServer", 0, REG_SZ, "http://%s" % proxy_host)
-        SetValueEx(key, "ProxyOverride", 0, REG_SZ, "<local>")
-        CloseKey(key)
+        self.init_regkeys([[
+            HKEY_CURRENT_USER,
+            "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Internet Settings",
+            {
+                "MigrateProxy": 1,
+                "ProxyEnable": 1,
+                "ProxyHttp1.1": 0,
+                "ProxyServer": "http://%s" % proxy_host,
+                "ProxyOverride": "<local>",
+            },
+        ]])
 
     def start(self, url):
         if "proxy" in self.options:
             self.setup_proxy(self.options["proxy"])
-
-        self.init_iexplore()
 
         iexplore = self.get_path("Internet Explorer")
         return self.execute(iexplore, args=[url])
