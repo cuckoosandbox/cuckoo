a03298dfdb437bdee85b3c25564a99b91039443c
depaolism <metindepaolis@yahoo.com>
2015-05-18 16:15:15 -0400

    Clean up of reporting modules. Processing module modified to allow more detailed report header

--- modules/processing/tool.py
+++ modules/processing/tool.py
@@ -15,15 +15,20 @@ class Tool(Processing):
         @return: list of tool output files with related information.
         """
         self.key = "tool"
-        tool_output = []
+        tool_output = {}
+        output = []
         tool_output_dir = os.path.join(self.analysis_path, "tool_output")
+        name = ""
 
         for dir_name, dir_names, file_names in os.walk(tool_output_dir):
             for file_name in file_names:
-                value = {'name': file_name, 'contents':  ''}
+                value = {'name': file_name, 'contents': ''}
                 if file_name == "tool_output.log":
                     with open(os.path.join(tool_output_dir, file_name), "r") as logfile:
                         value = {'name': file_name, 'contents': logfile.read()}
-                tool_output.append(value)
-
+                        last_line = value['contents'].split('\n')[-2]
+                        name = last_line[last_line.find('tool')+6:last_line.find(',')-1]
+                output.append(value)
+        tool_output = {'tool': name, 'output': output}
+        
         return tool_output
