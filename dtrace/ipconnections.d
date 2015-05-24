968abf9445cd1c3570751e045f1d7c065716ceb1
Dmitry Rodionov <i.am.rodionovd@gmail.com>
2015-05-24 15:13:54 +0600

    ipconnections.d: make it clear why we need to play with pointers here
    Since the second argument for ip::send/receive (csinfo_t) is always
    filled
    with zeros [0], the only way to get host and remote ports is to treat
    the
    third argument as a raw pointer to struct ip and access stuff from
    there.
    
    [0]: from
    http://www.opensource.apple.com/source/xnu/xnu-2782.1.97/bsd/netinet/ip_
    output.c:
    ------------------------------------------
    DTRACE_IP6(send, struct mbuf *, m, struct inpcb *, NULL,
    struct ip *, ip, struct ifnet *, ifp,
    struct ip *, ip, struct ip6_hdr *, NULL);
    ------------------------------------------
    Note the NULL passed as a value for struct inpcb*
    (it will become csinfo_t in dtrace).
    
    Thanks to Quinn "The Eskimo!" from Apple DTS team for this trick!

--- dtrace/ipconnections.d
+++ dtrace/ipconnections.d
@@ -12,8 +12,22 @@ ip:::receive
         this->protocol = args[2]->ip_ver == 4 ? args[4]->ipv4_protostr : args[5]->ipv6_nextstr;
         this->host = args[2]->ip_daddr;
         this->remote = args[2]->ip_saddr;
-        /* The only way to get host and remote ports is to treat the third argument
-         * as a raw pointer to struct ip and access stuff from there. */
+        /* Since the second argument (csinfo_t) is always filled with zeros [0],
+         * the only way to get host and remote ports is to treat the third argument
+         * as a raw pointer to struct ip and access stuff from there.
+         *
+         * Thanks to Quinn "The Eskimo!" from Apple DTS team for this trick!
+         *
+         * [0]:
+         * From http://www.opensource.apple.com/source/xnu/xnu-2782.1.97/bsd/netinet/ip_output.c:
+         * ------------------------------------------
+         * DTRACE_IP6(send, struct mbuf *, m, struct inpcb *, NULL,
+	     * struct ip *, ip, struct ifnet *, ifp,
+	     * struct ip *, ip, struct ip6_hdr *, NULL);
+         * ------------------------------------------
+         * Note the NULL passed as a value for struct inpcb* (it will become
+         * csinfo_t in dtrace).
+         */
         this->host_port = ntohs(*(uint16_t *)(arg2 + 22));
         this->remote_port = ntohs(*(uint16_t *)(arg2 + 20));
         /* Convert walltimestamp to unix timestamp */
