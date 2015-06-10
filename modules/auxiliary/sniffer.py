115e7e44e29c58f7a15242aa074e07c96270312f
rep <ms@mwcollect.org>
2015-06-10 11:19:10 +0200

    we dont use stdout/stderr for tcpdump, so we shouldnt pipe it

--- modules/auxiliary/sniffer.py
+++ modules/auxiliary/sniffer.py
@@ -82,8 +82,7 @@ class Sniffer(Auxiliary):
 
         log.debug("Sniffer command line: %r", pargs)
         try:
-            self.proc = subprocess.Popen(pargs, stdout=subprocess.PIPE,
-                                         stderr=subprocess.PIPE)
+            self.proc = subprocess.Popen(pargs)
         except (OSError, ValueError):
             log.exception("Failed to start sniffer (interface=%s, host=%s, "
                           "dump path=%s)", interface, host, file_path)
