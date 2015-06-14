85d15a81314e9953c58a8fa8102d069eb8aa8abb
Jurriaan Bremer <jurriaanbremer@gmail.com>
2015-06-15 01:33:33 +0200

    import mitm functionality

---
+++ data/mitm.py
@@ -0,0 +1,6 @@
+# Copyright (C) 2010-2015 Cuckoo Foundation.
+# This file is part of Cuckoo Sandbox - http://www.cuckoosandbox.org
+# See the file 'docs/LICENSE' for copying permission.
+
+# This is an empty boilerplate script where one can define a mitmdump script
+# for doing man in the middle interception.
