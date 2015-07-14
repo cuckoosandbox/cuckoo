46ca4e8640454ca4a4618d625fd81e864b576ed1
Dmitry Rodionov <i.am.rodionovd@gmail.com>
2015-07-15 02:12:57 +0600

    Update copyright headers
    Remove GSoC mentions
    
    [ci skip]

--- analyzer/darwin/lib/dtrace/dtruss.py
+++ analyzer/darwin/lib/dtrace/dtruss.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python
 # Copyright (C) 2015 Dmitry Rodionov
-# This file is part of my GSoC'15 project for Cuckoo Sandbox:
-#	http://www.cuckoosandbox.org
 # This software may be modified and distributed under the terms
 # of the MIT license. See the LICENSE file for details.
 
