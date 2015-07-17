7d6f9a75793bb9208662e78d4e7bb76dd3a45372
Jurriaan Bremer <jurriaanbremer@gmail.com>
2015-07-17 20:49:25 +0200

    also start/stop the dist instances

--- utils/stop-distributed.sh
+++ utils/stop-distributed.sh
@@ -7,5 +7,8 @@ for worker in $(curl -s "$DISTADDR:9003/api/node?mode=workers"); do
     sudo stop cuckoo-distributed-instance "INSTANCE=$worker"
 done
 
+sudo stop cuckoo-distributed-instance INSTANCE=dist.status
+sudo stop cuckoo-distributed-instance INSTANCE=dist.scheduler
+
 sudo service uwsgi stop cuckoo-distributed
 sudo service nginx stop
