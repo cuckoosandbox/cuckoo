258693aea7a4f3ca203a277ce00637eb9b4f384e
Dmitry Rodionov <i.am.rodionovd@gmail.com>
2015-07-18 21:43:12 +0600

    analyzer.py: code style

--- analyzer/darwin/analyzer.py
+++ analyzer/darwin/analyzer.py
@@ -3,35 +3,33 @@
 # This software may be modified and distributed under the terms
 # of the MIT license. See the LICENSE file for details.
 
-import os
-import sys
 import logging
-import xmlrpclib
-
-from datetime import datetime
-from pkgutil import iter_modules
+from sys import stderr
+from xmlrpclib import Server
 from traceback import format_exc
+from os import path, getcwd, makedirs
+
 from lib.common.config import Config
 from lib.common.results import NetlogHandler
 from lib.core.constants import PATHS
 from lib.core.packages import choose_package_class, Package
-
 from lib.core.osx import set_wallclock
 from lib.core.host import CuckooHost
 
-class Macalyzer:
+class Macalyzer(object):
     """Cuckoo OS X analyser.
     """
 
     log = logging.getLogger()
+    target = None
 
     def __init__(self, host, configuration=None):
         self.config = configuration
         self.host = host
 
     def _bootstrap(self):
-        self._create_result_folders()
-        self._setup_logging()
+        _create_result_folders()
+        _setup_logging()
         self._detect_target()
 
     def run(self):
@@ -39,7 +37,7 @@ class Macalyzer:
         """
         self._bootstrap()
 
-        self.log.debug("Starting analyzer from %s", os.getcwd())
+        self.log.debug("Starting analyzer from %s", getcwd())
         self.log.debug("Storing results at: %s", PATHS["root"])
 
         package = self._setup_analysis_package()
@@ -57,31 +55,9 @@ class Macalyzer:
     # Implementation details
     #
 
-    def _create_result_folders(self):
-        for name, folder in PATHS.items():
-            if os.path.exists(folder):
-                continue
-            try:
-                os.makedirs(folder)
-            except OSError:
-                pass
-
-    def _setup_logging(self):
-        """ Initialize logger. """
-        logger = logging.getLogger()
-        formatter = logging.Formatter("%(asctime)s [%(name)s] %(levelname)s: %(message)s")
-        sh = logging.StreamHandler()
-        sh.setFormatter(formatter)
-        logger.addHandler(sh)
-
-        nh = NetlogHandler()
-        nh.setFormatter(formatter)
-        logger.addHandler(nh)
-        logger.setLevel(logging.DEBUG)
-
     def _detect_target(self):
         if self.config.category == "file":
-            self.target = os.path.join("/tmp/", str(self.config.file_name))
+            self.target = path.join("/tmp/", str(self.config.file_name))
         else: # It's not a file, but a URL
             self.target = self.config.target
 
@@ -94,7 +70,7 @@ class Macalyzer:
         else:
             suggestion = None
         # Try to figure out what analysis package to use with this target
-        kwargs = { "suggestion" : suggestion }
+        kwargs = {"suggestion" : suggestion}
         package_class = choose_package_class(self.config.file_type,
                                              self.config.file_name, **kwargs)
         # Package initialization
@@ -108,6 +84,31 @@ class Macalyzer:
         package.start()
 
 
+def _create_result_folders():
+    for _, folder in PATHS.items():
+        if path.exists(folder):
+            continue
+        try:
+            makedirs(folder)
+        except OSError:
+            pass
+
+
+def _setup_logging():
+    """ Initialize logger. """
+    logger = logging.getLogger()
+    formatter = logging.Formatter("%(asctime)s [%(name)s] %(levelname)s: %(message)s")
+
+    stream = logging.StreamHandler()
+    stream.setFormatter(formatter)
+    logger.addHandler(stream)
+
+    netlog = NetlogHandler()
+    netlog.setFormatter(formatter)
+    logger.addHandler(netlog)
+    logger.setLevel(logging.DEBUG)
+
+
 
 if __name__ == "__main__":
     success = False
@@ -115,22 +116,23 @@ if __name__ == "__main__":
 
     try:
         config = Config(cfg="analysis.conf")
-        host = CuckooHost(config.ip, config.port)
-        success = Macalyzer(host, config).run()
+        cuckoo = CuckooHost(config.ip, config.port)
+        analyzer = Macalyzer(cuckoo, config)
+        success = analyzer.run()
 
     except KeyboardInterrupt:
         error = "Keyboard Interrupt"
 
-    except Exception as e:
+    except Exception as err:
         error_exc = format_exc()
-        error = str(e)
+        error = str(err)
         if len(analyzer.log.handlers):
             analyzer.log.exception(error_exc)
         else:
-            sys.stderr.write("{0}\n".format(error_exc))
+            stderr.write("{0}\n".format(error_exc))
     # Once the analysis is completed or terminated for any reason, we report
     # back to the agent, notifying that it can report back to the host.
     finally:
         # Establish connection with the agent XMLRPC server.
-        server = xmlrpclib.Server("http://127.0.0.1:8000")
+        server = Server("http://127.0.0.1:8000")
         server.complete(success, error, PATHS["root"])
