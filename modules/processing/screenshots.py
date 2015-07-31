f49175bf456e2e901be185781edf569b7fca4c2e
Jurriaan Bremer <jurriaanbremer@gmail.com>
2015-07-31 21:49:15 +0200

    tesseract improvements

--- modules/processing/screenshots.py
+++ modules/processing/screenshots.py
@@ -2,13 +2,16 @@
 # This file is part of Cuckoo Sandbox - http://www.cuckoosandbox.org
 # See the file 'docs/LICENSE' for copying permission.
 
+import logging
 import os
 import subprocess
 
 from lib.cuckoo.common.abstracts import Processing
 
+log = logging.getLogger(__name__)
+
 class Screenshots(Processing):
-    """Screenshot files analysis."""
+    """Screenshot file OCR analysis."""
 
     def run(self):
         """Run analysis.
@@ -18,6 +21,12 @@ class Screenshots(Processing):
         self.key = "screenshots"
         screenshots = []
 
+        tesseract = self.options.get("tesseract", "/usr/bin/tesseract")
+        if not os.path.exists(tesseract):
+            log.error("Could not find tesseract binary, "
+                      "screenshot OCR aborted.")
+            return []
+
         if os.path.exists(self.shots_path):
             # Walk through the files and select the JPGs.
             for shot_file in sorted(os.listdir(self.shots_path)):
@@ -26,18 +35,15 @@ class Screenshots(Processing):
 
                 # Get path to the screenshot.
                 shot_path = os.path.join(self.shots_path, shot_file)
+
                 # Initialize the entry for the results dict.
                 shot_entry = dict(path=shot_path, ocr="")
 
                 try:
-                    # Try to launch tesseract.
-                    ocr = subprocess.Popen(["tesseract", shot_path, "stdout"],
-                        stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-                except OSError:
-                    # Tesseract is not installed.
-                    pass
-                else:
-                    shot_entry["ocr"] = ocr.stdout.read()
+                    args = [tesseract, shot_path, "stdout"]
+                    shot_entry["ocr"] = subprocess.check_output(args)
+                except subprocess.CalledProcessError as e:
+                    log.info("Error running tesseract: %s", e)
 
                 # Append entry to list of screenshots.
                 screenshots.append(shot_entry)
