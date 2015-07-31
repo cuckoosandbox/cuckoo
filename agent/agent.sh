29ca5ee19d9b98fab37b318e4d327a212003c213
jekil <alessandro@tanasi.it>
2015-07-31 16:54:10 +0200

    Added missing copyright

--- agent/agent.sh
+++ agent/agent.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+# Copyright (C) 2010-2015 Cuckoo Foundation.
+# This file is part of Cuckoo Sandbox - http://www.cuckoosandbox.org
+# See the file 'docs/LICENSE' for copying permission.
+
 FILEPATH=$(readlink -f ${0%})
 FILEPATHDIR=$(dirname $FILEPATH)
 
