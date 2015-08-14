e9e00c31c84eb43d257fc831462e966a5f2a8b54
Dmitry Rodionov <i.am.rodionovd@gmail.com>
2015-08-14 21:25:34 +0600

    Reformat autoprobes tests
    I'm going to do integration tests different way…
    
    [ci skip]

--- tests/test_probesgenerator.py
+++ tests/test_probesgenerator.py
@@ -5,18 +5,19 @@
 
 import filecmp
 import unittest
-from os import remove
+from os import remove, path
 from common import TESTS_DIR
+from os.path import basename
+from difflib import unified_diff
 from analyzer.darwin.lib.dtrace.autoprobes import generate_probes
+from analyzer.darwin.lib.dtrace.autoprobes import dereference_type
+from analyzer.darwin.lib.dtrace.autoprobes import serialize_atomic_type
+from analyzer.darwin.lib.dtrace.autoprobes import serialize_struct_type
+from analyzer.darwin.lib.dtrace.autoprobes import serialize_type_with_template
 
 DEFINITIONS_FILE = None
 
 class ProbesGeneratorTestCase(unittest.TestCase):
-
-    # HELPERS
-
-    def definitions_file(self):
-        return TESTS_DIR + "/assets/probes/" + self._testMethodName + ".json"
 
     def result_file(self):
         return TESTS_DIR + "/assets/probes/" + self._testMethodName + ".d"
@@ -25,208 +26,174 @@ class ProbesGeneratorTestCase(unittest.TestCase):
         return TESTS_DIR + "/assets/probes/" + self._testMethodName + ".d.reference"
 
     def tearDown(self):
-        remove(self.result_file())
+        if path.isfile(self.result_file()):
+            remove(self.result_file())
 
-    # TESTS
+    def assertEmptyDiff(self, diff):
+        if len(diff) > 0:
+            self.fail("Diff is not empty:\n" + diff)
+
+    # UNIT TESTS
+
+    def test_probes_dereference_value_type(self):
+        # given
+        type = "scalar_t"
+        # when
+        output = dereference_type(type)
+        # then
+        self.assertEqual("scalar_t", output)
+
+    def test_probes_dereference_reference_type(self):
+        # given
+        type = "foo *"
+        # when
+        output = dereference_type(type)
+        # then
+        self.assertEqual("foo", output)
+
+    def test_probes_dereference_reference_type_with_random_spaces(self):
+        # given
+        type = "foo  *  "
+        # when
+        output = dereference_type(type)
+        # then
+        self.assertEqual("foo", output)
+
+    def test_probes_dereference_types_that_must_not_be_dereferenced(self):
+        # given
+        type_string  = "char *"
+        type_pointer = "void *"
+        # when
+        output_string  = dereference_type(type_string)
+        output_pointer = dereference_type(type_pointer)
+        # then
+        self.assertEqual("char *", output_string)
+        self.assertEqual("void *", output_pointer)
+
+    def test_probes_atomic_type_serialization(self):
+        # given
+        type = "float"
+        accessor = "self->arg0"
+        # when
+        output = serialize_atomic_type(type, accessor)
+        # then
+        self.assertEqual("(float)(self->arg0)", output)
+
+    def test_probes_atomic_pointer_type_serialization(self):
+        # given
+        type = "int *"
+        accessor = "self->arg0"
+        # when
+        output = serialize_atomic_type(type, accessor)
+        # then
+        self.assertEqual(
+            "self->arg0 == (int)NULL ? (int)NULL : *(int *)copyin(self->arg0, sizeof(int))",
+            output
+        )
+
+    def test_probes_struct_type_serialization(self):
+        # given
+        type = "foo_t"
+        accessor = "self->arg0"
+        types = {
+            "float": {
+                "printf_specifier": "%f",
+                "native": True
+            },
+            "int": {
+                "printf_specifier": "%f",
+                "native": True
+            },
+            "foo_t": {
+                "native": False,
+                "struct": {
+                    "value_f":   "float",
+                    "value_int": "int *"
+                }
+            }
+        }
+        # when
+        output = serialize_struct_type(type, accessor, types)
+        # then
+        self.assertEqual(
+            "(foo_t)(self->arg0).value_int == (int)NULL ? (int)NULL : *(int *)copyin((foo_t)(self->arg0).value_int, sizeof(int)), (float)((foo_t)(self->arg0).value_f)",
+            output
+        )
+
+    def test_probes_struct_pointer_type_serialization(self):
+        # given
+        type = "foo_t *"
+        accessor = "self->arg0"
+        types = {
+            "float": {
+                "printf_specifier": "%f",
+                "native": True
+            },
+            "int": {
+                "printf_specifier": "%f",
+                "native": True
+            },
+            "foo_t": {
+                "native": False,
+                "struct": {
+                    "value_f":   "float",
+                    "value_int": "int *"
+                }
+            }
+        }
+        # when
+        output = serialize_struct_type(type, accessor, types)
+        # then
+        self.assertEqual(
+            "(foo_t *)(self->arg0)->value_int == (int)NULL ? (int)NULL : *(int *)copyin((foo_t *)(self->arg0)->value_int, sizeof(int)), (float)((foo_t *)(self->arg0)->value_f)",
+            output
+        )
+
+    def test_probes_template_type_serialization(self):
+        # given
+        type = "string"
+        accessor = "self->arg0"
+        types = {
+            "string": {
+                "printf_specifier": '"%S"',
+                "native": False,
+                "template": '${ARG} != (int64_t)NULL ? copyinstr(${ARG}) : "<NULL>"'
+            }
+        }
+        # when
+        output = serialize_type_with_template(type, accessor, types)
+        # then
+        self.assertEqual(
+            'self->arg0 != (int64_t)NULL ? copyinstr(self->arg0) : "<NULL>"',
+            output
+        )
+
+    # INTEGRATION TESTS
 
     def test_probes_without_arguments_return_integer(self):
         # given
-        source = self.definitions_file()
+        source = [{
+            "api": "foo",
+            "args": [
+                {"name": "key", "argtype": "void *"},
+                {"name": "hash", "argtype": "uint64_t"},
+                {"name": "rando", "argtype": "foo_t *"}
+            ],
+            "retval_type": "int",
+            "category" : "foobar",
+            "library" : "libfoo"
+        }]
         destination = self.result_file()
         # when
         generate_probes(source, destination)
         # then
-        self.assertTrue(
-            _files_are_equal(self.result_file(), self.reference_file())
-        )
+        self.assertEmptyDiff(file_diff(self.reference_file(), destination))
 
-    def test_probes_without_arguments_return_string(self):
-        # given
-        source = self.definitions_file()
-        destination = self.result_file()
-        # when
-        generate_probes(source, destination)
-        # then
-        self.assertTrue(
-            _files_are_equal(self.result_file(), self.reference_file())
-        )
-
-    def test_probes_without_arguments_return_pointer(self):
-        # given
-        source = self.definitions_file()
-        destination = self.result_file()
-        # when
-        generate_probes(source, destination)
-        # then
-        self.assertTrue(
-            _files_are_equal(self.result_file(), self.reference_file())
-        )
-
-    def test_probes_one_argument_integer_return_integer(self):
-        # given
-        source = self.definitions_file()
-        destination = self.result_file()
-        # when
-        generate_probes(source, destination)
-        # then
-        self.assertTrue(
-            _files_are_equal(self.result_file(), self.reference_file())
-        )
-
-    def test_probes_one_argument_integer_return_string(self):
-        # given
-        source = self.definitions_file()
-        destination = self.result_file()
-        # when
-        generate_probes(source, destination)
-        # then
-        self.assertTrue(
-            _files_are_equal(self.result_file(), self.reference_file())
-        )
-
-    def test_probes_one_argument_integer_return_float(self):
-        # given
-        source = self.definitions_file()
-        destination = self.result_file()
-        # when
-        generate_probes(source, destination)
-        # then
-        self.assertTrue(
-            _files_are_equal(self.result_file(), self.reference_file())
-        )
-
-    def test_probes_one_argument_integer_return_double(self):
-        # given
-        source = self.definitions_file()
-        destination = self.result_file()
-        # when
-        generate_probes(source, destination)
-        # then
-        self.assertTrue(
-            _files_are_equal(self.result_file(), self.reference_file())
-        )
-
-    def test_probes_one_argument_string_return_integer(self):
-        # given
-        source = self.definitions_file()
-        destination = self.result_file()
-        # when
-        generate_probes(source, destination)
-        # then
-        self.assertTrue(
-            _files_are_equal(self.result_file(), self.reference_file())
-        )
-
-    def test_probes_one_argument_float_return_integer(self):
-        # given
-        source = self.definitions_file()
-        destination = self.result_file()
-        # when
-        generate_probes(source, destination)
-        # then
-        self.assertTrue(
-            _files_are_equal(self.result_file(), self.reference_file())
-        )
-
-    def test_probes_one_argument_float_return_pointer(self):
-        # given
-        source = self.definitions_file()
-        destination = self.result_file()
-        # when
-        generate_probes(source, destination)
-        # then
-        self.assertTrue(
-            _files_are_equal(self.result_file(), self.reference_file())
-        )
-
-
-    def test_probes_two_arguments_integer_string_return_integer(self):
-        # given
-        source = self.definitions_file()
-        destination = self.result_file()
-        # when
-        generate_probes(source, destination)
-        # then
-        self.assertTrue(
-            _files_are_equal(self.result_file(), self.reference_file())
-        )
-
-    def test_probes_two_arguments_integer_string_return_pointer(self):
-        # given
-        source = self.definitions_file()
-        destination = self.result_file()
-        # when
-        generate_probes(source, destination)
-        # then
-        self.assertTrue(
-            _files_are_equal(self.result_file(), self.reference_file())
-        )
-
-    def test_probes_two_arguments_float_integer_return_string(self):
-        # given
-        source = self.definitions_file()
-        destination = self.result_file()
-        # when
-        generate_probes(source, destination)
-        # then
-        self.assertTrue(
-            _files_are_equal(self.result_file(), self.reference_file())
-        )
-
-    def test_probes_three_arguments_float_string_int_return_double(self):
-        # given
-        source = self.definitions_file()
-        destination = self.result_file()
-        # when
-        generate_probes(source, destination)
-        # then
-        self.assertTrue(
-            _files_are_equal(self.result_file(), self.reference_file())
-        )
-
-    def test_probes_multiple(self):
-        # given
-        source = self.definitions_file()
-        destination = self.result_file()
-        # when
-        generate_probes(source, destination)
-        # then
-        self.assertTrue(
-            _files_are_equal(self.result_file(), self.reference_file())
-        )
-
-    def test_probes_with_native_types(self):
-        # given
-        source = self.definitions_file()
-        destination = self.result_file()
-        # when
-        generate_probes(source, destination)
-        # then
-        self.assertTrue(
-            _files_are_equal(self.result_file(), self.reference_file())
-        )
-
-    def test_probes_two_arguments_buffer_int_return_int(self):
-        # given
-        source = self.definitions_file()
-        destination = self.result_file()
-        # when
-        generate_probes(source, destination)
-        # then
-        self.assertTrue(
-            _files_are_equal(self.result_file(), self.reference_file())
-        )
-
-    def test_probe_two_arguments_string_int_return_sizet(self):
-        # given
-        source = self.definitions_file()
-        destination = self.result_file()
-        # when
-        generate_probes(source, destination)
-        # then
-        self.assertTrue(
-            _files_are_equal(self.result_file(), self.reference_file())
-        )
-
-def _files_are_equal(a, b):
-    return filecmp.cmp(a, b)
+def file_diff(a, b):
+    with open(a, 'r') as astream, open(b, 'r') as bstream:
+        return "".join(unified_diff(
+            astream.readlines(),
+            bstream.readlines(),
+            basename(a), basename(b),
+            n=0
+        ))
