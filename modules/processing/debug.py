b78903fd071e5dd30b58d9556a0a89e9cf7b0889
cuckoo root <malware-report@skbkontur.ru>
2015-08-17 14:48:58 +0300

    check cp866 and cp1251 file encodings

--- modules/processing/debug.py
+++ modules/processing/debug.py
@@ -23,8 +23,14 @@ class Debug(Processing):
             try:
                 debug["log"] = codecs.open(self.log_path, "rb", "utf8").readlines()
             except ValueError as e:
-                raise CuckooProcessingError("Error decoding %s: %s" %
-                                            (self.log_path, e))
+                try:
+                    debug["log"] = codecs.open(self.log_path, "rb", "cp866").readlines()
+                except ValueError as ee:
+                    try:
+                        debug["log"] = codecs.open(self.log_path, "rb", "cp1251").readlines()
+                    except ValueError as eee:
+                        raise CuckooProcessingError("Error decoding %s: %s, %s, %s" %
+                                                    (self.log_path, e, ee, eee))
             except (IOError, OSError) as e:
                 raise CuckooProcessingError("Error opening %s: %s" %
                                             (self.log_path, e))
