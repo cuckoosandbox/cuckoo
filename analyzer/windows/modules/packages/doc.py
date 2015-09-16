9da8f75533834fa92c23ff1dce0c21d0b7ce2c3b
Jurriaan Bremer <jurriaanbremer@gmail.com>
2015-09-16 11:21:36 +0200

    prune office 2007 welcome message

--- analyzer/windows/modules/packages/doc.py
+++ analyzer/windows/modules/packages/doc.py
@@ -2,6 +2,8 @@
 # This file is part of Cuckoo Sandbox - http://www.cuckoosandbox.org
 # See the file 'docs/LICENSE' for copying permission.
 
+from _winreg import HKEY_CURRENT_USER
+
 from lib.common.abstracts import Package
 
 class DOC(Package):
@@ -14,6 +16,17 @@ class DOC(Package):
         ("ProgramFiles", "Microsoft Office", "Office15", "WINWORD.EXE"),
         ("ProgramFiles", "Microsoft Office 15", "root", "office15", "WINWORD.EXE"),
         ("ProgramFiles", "Microsoft Office", "WORDVIEW.EXE"),
+    ]
+
+    REGKEYS = [
+        [
+            HKEY_CURRENT_USER,
+            "Software\\Microsoft\\Office\\12.0\\Common\\General",
+            {
+                # "Welcome to the 2007 Microsoft Office system"
+                "ShownOptIn": 1,
+            },
+        ],
     ]
 
     def start(self, path):
