13036418fea550f8ce9f00b5d480814023eb49cc
Jurriaan Bremer <jurriaanbremer@gmail.com>
2015-10-12 01:38:36 +0200

    wmi queries in the summary

--- modules/processing/platform/windows.py
+++ modules/processing/platform/windows.py
@@ -169,8 +169,6 @@ class BehaviorReconstructor(object):
     def _api_LdrLoadDll(self, return_value, arguments):
         return ("dll_loaded", arguments["module_name"])
 
-    # File stuff.
-
     def _api_NtCreateFile(self, return_value, arguments):
         self.files[arguments["file_handle"]] = arguments["filepath"]
         return ("file_opened", arguments["filepath"])
@@ -274,3 +272,11 @@ class BehaviorReconstructor(object):
 
     def _api_system(self, return_value, arguments):
         return ("command_line", arguments["command"])
+
+    # WMI stuff.
+
+    def _api_IWbemServices_ExecQuery(self, return_value, arguments):
+        return ("wmi_query", arguments["query"])
+
+    def _api_IWbemServices_ExecQueryAsync(self, return_value, arguments):
+        return ("wmi_query", arguments["query"])
