7ee4e1b5f4cf86d9ca4af67afd1ae088a468f5e2
Jurriaan Bremer <jbr@cuckoo.sh>
2015-11-08 15:14:46 +0100

    network dns uses network dns

--- web/analysis/urls.py
+++ web/analysis/urls.py
@@ -4,7 +4,8 @@
 
 from django.conf.urls import patterns, url
 
-urlpatterns = patterns("",
+urlpatterns = patterns(
+    "",
     url(r"^$", "analysis.views.index"),
     url(r"^(?P<task_id>\d+)/$", "analysis.views.report"),
     url(r"^latest/$", "analysis.views.latest_report"),
