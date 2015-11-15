2ab737cf6ea872ee5c8ff1295b44a956a220e4bc
Jurriaan Bremer <jbr@cuckoo.sh>
2015-11-15 04:18:07 +0100

    get rid of suboptimal task processing scheduler
    Using a locking mechanism we're able to avoid race conditions without
    requiring a scheduler. Note that the SQL statement used has only been
    tested for PostgreSQL and, although it may work for MySQL, it definitely
    does not support SQLite (but that's not really an issue as a SQLite
    database shouldn't be used in parallel anyway).

--- lib/cuckoo/core/database.py
+++ lib/cuckoo/core/database.py
@@ -16,7 +16,7 @@ from lib.cuckoo.common.objects import File, URL
 from lib.cuckoo.common.utils import create_folder, Singleton, classlock, SuperLock
 
 try:
-    from sqlalchemy import create_engine, Column, not_
+    from sqlalchemy import create_engine, Column
     from sqlalchemy import Integer, String, Boolean, DateTime, Enum
     from sqlalchemy import ForeignKey, Text, Index, Table
     from sqlalchemy.ext.declarative import declarative_base
@@ -1216,82 +1216,31 @@ class Database(object):
             session.close()
         return errors
 
-    def count_processing_tasks(self, instance):
-        """Count the amount of pending tasks for this processing instance.
-        @param instance: processing instance
-        """
+    def processing_get_task(self, instance):
+        """Get an available task for processing."""
         session = self.Session()
-        try:
-            count = session.query(TaskProcessing)
-            count = count.filter_by(instance=instance).count()
-        except SQLAlchemyError as e:
-            log.debug("Database error counting processing tasks: {0}".format(e))
-            return 0
-        finally:
-            session.close()
-        return count
 
-    def processing_get_new_tasks(self, count):
-        """Get available tasks that have not been assigned to other instances
-        yet."""
-        session = self.Session()
+        # Please feel free to sqlalchemize the following query, but I didn't
+        # get that far. This seems to be doing a fine job for avoiding race
+        # conditions - especially with the session.commit() thing. But any
+        # improvements are welcome.
+        query = """
+            UPDATE tasks SET processing = '%s'
+            WHERE id IN (
+                SELECT id FROM tasks
+                WHERE status = 'completed' AND processing IS NULL
+                LIMIT 1 FOR UPDATE
+            )
+            RETURNING id
+        """
+
         try:
-            tasks = session.query(Task).filter_by(status=TASK_COMPLETED)
-            tasks = tasks.filter(not_(Task.id.in_(TaskProcessing.task_id)))
-            tasks = tasks.limit(count).all()
+            task = session.execute(query % instance).first()
+            session.commit()
+            return task[0] if task else None
         except SQLAlchemyError as e:
             log.debug("Database error getting new processing tasks: %s", e)
-            return []
+            return
         finally:
             session.close()
-        return tasks
-
-    def add_processing_task(self, tp):
-        """Add a new processing task."""
-        session = self.Session()
-        session.add(tp)
-
-        try:
-            session.commit()
-            session.refresh(tp)
-        except SQLAlchemyError as e:
-            log.debug("Database error adding processing task: %s", e)
-            session.rollback()
-        finally:
-            session.close()
-
-    def delete_processing_task(self, tp):
-        """Delete an existing processing task."""
-        session = self.Session()
-        session.delete(tp)
-
-        try:
-            session.commit()
-        except SQLAlchemyError as e:
-            log.debug("Database error deleting processing task: %s", e)
-            session.rollback()
-        finally:
-            session.close()
-
-    def list_processing_tasks(self, instance, count):
-        """List available processing tasks for a particular instance."""
-        session = self.Session()
-        try:
-            tasks = session.query(TaskProcessing)
-
-            if instance is None:
-                # Filter for instance IDs.
-                tasks = tasks.filter_by(task_id=None)
-            else:
-                # Filter for new tasks for this instance.
-                null = None
-                tasks = tasks.filter_by(instance=instance)
-                tasks = tasks.filter(TaskProcessing.task_id != null)
-
-            tasks = tasks.limit(count).all()
-        except SQLAlchemyError as e:
-            log.debug("Database error getting processing tasks: {0}".format(e))
-            return []
-        finally:
-            session.close()
-        return tasks
+        return
