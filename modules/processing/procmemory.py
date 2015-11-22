c37bade59df8a80faf38562a668d401bbf01bafc
Jurriaan Bremer <jbr@cuckoo.sh>
2015-11-22 12:35:22 +0100

    parse urls from process memory dumps and filter through a whitelist
    It is not uncommon for samples to unpack themselves in-memory. Therefore
    parsing process memory dumps for URLs allows us to find potential
    malicious URLs. Also start on a whitelist to filter out benign results.

--- modules/processing/procmemory.py
+++ modules/processing/procmemory.py
@@ -3,10 +3,22 @@
 # See the file 'docs/LICENSE' for copying permission.
 
 import os
+import re
 
 from lib.cuckoo.common.abstracts import Processing
 from lib.cuckoo.common.objects import File
 from lib.cuckoo.common.constants import CUCKOO_ROOT
+from lib.cuckoo.common.whitelist import is_whitelisted_domain
+
+HTTP_REGEX = (
+    "(https?://)(["
+    "[0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5]\\."
+    "[0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5]\\."
+    "[0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5]\\."
+    "[0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5]"
+    "]|[a-zA-Z0-9\\.-]+)"
+    "(:\\d+)?(/[a-zA-Z0-9_:%?=/\\.-]+)"
+)
 
 class ProcessMemory(Processing):
     """Analyze process memory dumps."""
@@ -23,10 +35,18 @@ class ProcessMemory(Processing):
                 dmp_path = os.path.join(self.pmemory_path, dmp)
                 dmp_file = File(dmp_path)
 
+                # Let's hope the file is not too big.
+                buf = open(dmp_path, "rb").read()
+                urls = set()
+                for url in re.findall(HTTP_REGEX, buf):
+                    if not is_whitelisted_domain(url[1]):
+                        urls.add("".join(url))
+
                 proc = dict(
                     file=dmp_path,
                     pid=os.path.splitext(os.path.basename(dmp_path))[0],
-                    yara=dmp_file.get_yara(os.path.join(CUCKOO_ROOT, "data", "yara", "index_memory.yar"))
+                    yara=dmp_file.get_yara(os.path.join(CUCKOO_ROOT, "data", "yara", "index_memory.yar")),
+                    urls=list(urls),
                 )
 
                 results.append(proc)
