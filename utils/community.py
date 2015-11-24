be0875f3458a49064d7f33e9444549148ce7b0f5
Thibault Marquand <titotix@free.fr>
2015-11-24 14:13:15 +0100

    community.py: fix loop over same file introduced by 73279e29f4c8a47cb3180d7ee49a6c79d71671be. fix few bug monitor installation

--- utils/community.py
+++ utils/community.py
@@ -52,14 +52,15 @@ def installdir(src, dst, force, rewrite, origin=[]):
 
         if not rewrite:
             srcpath = os.path.join(src, file_name)
-            if not os.path.isdir(srcpath):
+            if not os.path.isdir(srcpath) or os.path.islink(srcpath):
                 if os.path.exists(destination):
-                    print("File \"{0}\" already exists, "
-                          "{1}".format(file_name, colors.yellow("skipped")))
+                    print("File \"{0}/{1}\" already exists, {2}".format(
+                        "/".join(origin), file_name, colors.yellow("skipped")))
                     continue
             else:
                 installdir(srcpath, destination, force, rewrite,
                            origin + [file_name])
+                continue
 
         install = False
 
@@ -131,7 +132,7 @@ def install(enabled, force, rewrite, archive):
             print "  No candidates available, continuing."
             continue
 
-        installdir(origin, os.path.join(CUCKOO_ROOT, folder), force, rewrite)
+        installdir(origin, os.path.join(CUCKOO_ROOT, folder), force, rewrite, origin=[folder])
 
     shutil.rmtree(temp)
 
@@ -178,6 +179,8 @@ def main():
             enabled.append("analyzer")
         if args.agent:
             enabled.append("agent")
+        if args.monitor:
+            enabled.append("monitor")
 
     if not enabled:
         print(colors.red("You need to enable some category!\n"))
