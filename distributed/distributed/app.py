88c119dd4c1285502587c0c4f8c955ae67888a48
Jurriaan Bremer <jbr@cuckoo.sh>
2015-12-27 00:17:22 +0100

    missing copyright headers

--- distributed/distributed/app.py
+++ distributed/distributed/app.py
@@ -1,3 +1,7 @@
+# Copyright (C) 2010-2015 Cuckoo Foundation.
+# This file is part of Cuckoo Sandbox - http://www.cuckoosandbox.org
+# See the file 'docs/LICENSE' for copying permission.
+
 import os.path
 import sys
 
