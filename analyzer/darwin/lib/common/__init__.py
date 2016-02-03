a389d74f091efc50a3f53b8ccc401f67f840cd27
Nex <nex@nex.sx>
2016-02-03 14:59:18 +0100

    Updated copyright notice

--- analyzer/darwin/lib/common/__init__.py
+++ analyzer/darwin/lib/common/__init__.py
@@ -1,3 +1,3 @@
-# Copyright (C) 2014-2015 Cuckoo Foundation.
+# Copyright (C) 2014-2016 Cuckoo Foundation.
 # This file is part of Cuckoo Sandbox - http://www.cuckoosandbox.org
 # See the file 'docs/LICENSE' for copying permission.
