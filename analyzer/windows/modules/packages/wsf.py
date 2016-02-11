fc58dffa1532588ad4e0a54ae203f2aa9c940793
Jurriaan Bremer <jbr@cuckoo.sh>
2016-02-12 00:05:45 +0100

    .wsf analysis support
    Recently a blogpost [1] was released covering a "new" technique used
    in-the-wild. Rather than embedding a Javascript .js file, the .wsf
    technique allows one to use the JScript.Encode feature to encode the
    actual Javascript payload further.
    
    As part of our static analysis we provide the decoded Javascript.
    The implementation of this work is a port of scrdec18.c [2], a
    JScript.Encode decoder implemented in C.
    
    [1]: https://medium.com/@networksecurity/ransomware-delivered-using-wsf-windows-script-file-551a6589da47
    [2]: https://gist.githubusercontent.com/bcse/1834878/raw/7483fb72abbb32aa69b853fdcc9f6f72e7568677/scrdec18.c

---
+++ analyzer/windows/modules/packages/wsf.py
@@ -0,0 +1,27 @@
+# Copyright (C) 2016 Cuckoo Foundation.
+# This file is part of Cuckoo Sandbox - http://www.cuckoosandbox.org
+# See the file 'docs/LICENSE' for copying permission.
+
+import logging
+import os
+
+from lib.common.abstracts import Package
+
+log = logging.getLogger(__name__)
+
+class WSF(Package):
+    """Windows Scripting File analysis package."""
+    PATHS = [
+        ("System32", "wscript.exe"),
+    ]
+
+    def start(self, path):
+        wscript = self.get_path("WScript")
+
+        # Enforce the .wsf file extension as is required by wscript.
+        if not path.endswith(".wsf"):
+            os.rename(path, path + ".wsf")
+            path += ".wsf"
+            log.info("Submitted file is missing extension, added .wsf")
+
+        return self.execute(wscript, args=[path])
