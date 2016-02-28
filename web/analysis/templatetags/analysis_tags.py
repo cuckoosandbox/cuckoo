8ea31dd9a65268bda1781c2b7d22b1fe899a3908
Johannes Gilger <heipei@hackvalue.de>
2016-02-28 13:47:54 +0100

    Human-readable file size

--- web/analysis/templatetags/analysis_tags.py
+++ web/analysis/templatetags/analysis_tags.py
@@ -92,3 +92,12 @@ def ensurelist(o):
     if isinstance(o, (tuple, list)):
         return o
     return o,
+
+@register.filter
+def sizeof_fmt(num):
+    suffix='B'
+    for unit in ['','K','M','G','T','P','E','Z']:
+        if abs(num) < 1024.0:
+            return "%3.1f%s%s" % (num, unit, suffix)
+        num /= 1024.0
+    return "%.1f%s%s" % (num, 'Yi', suffix)
