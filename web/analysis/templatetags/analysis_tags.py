5d8373e0dbb816f5f8519d4372fcfaf838cdf530
Jurriaan Bremer <jbr@cuckoo.sh>
2016-03-05 13:40:33 +0100

    code cleanup

--- web/analysis/templatetags/analysis_tags.py
+++ web/analysis/templatetags/analysis_tags.py
@@ -95,9 +95,9 @@ def ensurelist(o):
 
 @register.filter
 def sizeof_fmt(num):
-    suffix='B'
-    for unit in ['','K','M','G','T','P','E','Z']:
+    suffix = "B"
+    for unit in ["", "K", "M", "G", "T", "P", "E", "Z"]:
         if abs(num) < 1024.0:
             return "%3.1f%s%s" % (num, unit, suffix)
         num /= 1024.0
-    return "%.1f%s%s" % (num, 'Yi', suffix)
+    return "%.1f%s%s" % (num, "Yi", suffix)
