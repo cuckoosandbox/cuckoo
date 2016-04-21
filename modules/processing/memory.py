0377c2513b9b41f29f7305242f2cb490b2e90e76
doomedraven <doomedraven@users.noreply.github.com>
2016-04-21 17:22:08 +0200

    small correction

--- modules/processing/memory.py
+++ modules/processing/memory.py
@@ -74,7 +74,7 @@ class VolatilityAPI(object):
         base_conf = {
             "profile": "WinXPSP2x86",
             "use_old_as": None,
-            "kdbg": None,
+vo            "kdbg": None,
             "help": False,
             "kpcr": None,
             "tz": None,
@@ -911,7 +911,7 @@ class VolatilityManager(object):
 
         conf_path = os.path.join(CUCKOO_ROOT, "conf", "memory.conf")
         if not os.path.exists(conf_path):
-            log.error("Configuration file volatility.conf not found".format(conf_path))
+            log.error("Configuration file {0} not found".format(conf_path))
             self.voptions = False
             return
 
