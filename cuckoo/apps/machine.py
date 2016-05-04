ec377eb985eed724a85acb1e21faec041640ab8f
Jurriaan Bremer <jbr@cuckoo.sh>
2016-05-04 11:57:28 +0200

    move machine.py to cuckoo/apps/

--- cuckoo/apps/machine.py
+++ cuckoo/apps/machine.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python
 # Copyright (C) 2014-2016 Cuckoo Foundation.
 # This file is part of Cuckoo Sandbox - http://www.cuckoosandbox.org
 # See the file 'docs/LICENSE' for copying permission.
