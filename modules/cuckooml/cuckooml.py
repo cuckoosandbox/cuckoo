6ae16acb9b1bb30be0035bbf4e55eed29d4eb580
Kacper <ks1591@my.bristol.ac.uk>
2016-06-15 13:55:14 +0100

    Feature extractor placeholders

--- modules/cuckooml/cuckooml.py
+++ modules/cuckooml/cuckooml.py
@@ -61,3 +61,94 @@ class Cuckooml(object):
             self.label = top_label.encode("ascii", "ignore")
         else:
             self.label = "none"
+
+
+    def extract_features(self):
+        """Extract features of the loaded sample."""
+        self.extract_features_static()
+        self.extract_features_dynamic()
+
+
+    def extract_features_static(self):
+        """Extract static features of the loaded sample."""
+        self.feature_static_metadata()
+        self.feature_static_signature()
+        self.feature_static_heuristic()
+        self.feature_static_packer()
+        self.feature_static_pef()
+        self.feature_static_imports()
+
+
+    def extract_features_dynamic(self):
+        """Extract dynamic features of the loaded sample."""
+        self.feature_dynamic_imports()
+        self.feature_dynamic_filesystem()
+        self.feature_dynamic_network()
+        self.feature_dynamic_registry()
+        self.feature_dynamic_windowsapi()
+
+
+    def feature_static_metadata(self):
+        """Create features form extracted binary metadata."""
+        print self.report.get("target").get("file").get("size")
+        print self.report.get("target", {}).get("file", {}).get("type")
+        print self.report.get("static", {}).get("pe_timestamp")
+        print self.report.get("static", {}).get("pe_versioninfo")
+
+
+    def feature_static_signature(self):
+        """Create features form binary signature check."""
+        print self.report.get("static", {}).get("signature")
+        print self.report.get("static", {}).get("pe_versioninfo")
+
+
+    def feature_static_heuristic(self):
+        """Create features form results return by heuristic tools.
+        **Not available for current JSON content.**"""
+        pass
+
+
+    def feature_static_packer(self):
+        """Create feature from information returned by packer/cryptor
+        detectors."""
+        print self.report.get("static", {}).get("peid_signatures")
+
+
+    def feature_static_pef(self):
+        """Create features from information derived form portable executable
+        format."""
+        print self.report.get("static", {}).get("pe_sections")
+        print self.report.get("static", {}).get("pe_resources")
+
+
+    def feature_static_imports(self):
+        """Extract features from static imports like referenced library
+        functions."""
+        print self.report.get("static", {}).get("imported_dll_count")
+        print self.report.get("static", {}).get("pe_imports")
+        print self.report.get("static", {}).get("pe_exports")
+
+
+    def feature_dynamic_imports(self):
+        """Extract features from dynamic imports, mutexes, and processes."""
+        pass
+
+
+    def feature_dynamic_filesystem(self):
+        """Extract features from filesystem operations."""
+        pass
+
+
+    def feature_dynamic_network(self):
+        """Extract features from network operations."""
+        pass
+
+
+    def feature_dynamic_registry(self):
+        """Extract features from registry operations."""
+        pass
+
+
+    def feature_dynamic_windowsapi(self):
+        """Extract features from Windows API calls sequence."""
+        pass
