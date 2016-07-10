da56a87986db947fbbfdda6d8cbee16bef51d3bd
Jurriaan Bremer <jbr@cuckoo.sh>
2016-07-10 17:42:56 +0200

    include peepdf dependency

--- setup.py
+++ setup.py
@@ -34,6 +34,7 @@ setup(
         "jsbeautifier==1.5.10",
         "lxml==3.6.0",
         "oletools==0.42",
+        "peepdf==0.3.2",
         "pefile2==1.2.11",
         "pymisp==2.4.36",
         "pymongo==3.0.3",
