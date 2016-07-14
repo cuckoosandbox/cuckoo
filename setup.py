0be5c34c204e585ae5d153fd2731e5d9ad64697f
Jurriaan Bremer <jbr@cuckoo.sh>
2016-07-14 15:33:33 +0200

    add missing androguard dependency

--- setup.py
+++ setup.py
@@ -56,6 +56,7 @@ setuptools.setup(
     },
     install_requires=[
         "alembic==0.8.0",
+        "androguard==3.0",
         "beautifulsoup4==4.4.1",
         "chardet==2.3.0",
         "click==6.6",
