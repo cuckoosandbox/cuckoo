b38786ad228ab5175120cf0f52e45aa72823214b
Sander Ferdinand <sanfer@cuckoo.sh>
2016-07-23 21:55:09 +0200

    Moved some buttons to the menu. closes #34

--- web/submission/urls.py
+++ web/submission/urls.py
@@ -7,8 +7,8 @@ from . import views
 from django.conf.urls import url
 
 urlpatterns = [
-    url(r"^$", views.index),
-    url(r"status/(?P<task_id>\d+)/$", views.status),
-    url(r"^(?P<task_id>\d+)/$", views.resubmit),
-    url(r"^(?P<task_id>\d+)/dropped/(?P<sha1>[a-f0-9]{40})/$", views.submit_dropped),
+    url(r"^$", views.index, name="submission/index"),
+    url(r"status/(?P<task_id>\d+)/$", views.status, name='submission/status'),
+    url(r"^(?P<task_id>\d+)/$", views.resubmit, name="submission/resubmit"),
+    url(r"^(?P<task_id>\d+)/dropped/(?P<sha1>[a-f0-9]{40})/$", views.submit_dropped, name="submission/submit_dropped"),
 ]
