1b624c09d5a0a6c4ff3482737c802ce6f54d8393
Sander Ferdinand <sanfer@cuckoo.sh>
2016-08-09 04:36:34 +0200

    Sample submission page development

--- web/bin/utils.py
+++ web/bin/utils.py
@@ -5,16 +5,31 @@
 
 import os
 import calendar
+import json
 from datetime import datetime
 
 from django.shortcuts import render
 
 def json_default(obj):
+    if hasattr(obj, "to_dict"):
+        return obj.to_dict()
+
     if isinstance(obj, datetime):
         if obj.utcoffset() is not None:
             obj = obj - obj.utcoffset()
         return calendar.timegm(obj.timetuple()) + obj.microsecond / 1000000.0
     raise TypeError("%r is not JSON serializable" % obj)
+
+class json_default_response(json.JSONEncoder):
+    def default(self, obj):
+        if hasattr(obj, "to_dict"):
+            return obj.to_dict()
+
+        if isinstance(obj, datetime):
+            if obj.utcoffset() is not None:
+                obj = obj - obj.utcoffset()
+            return calendar.timegm(obj.timetuple()) + obj.microsecond / 1000000.0
+        raise TypeError("%r is not JSON serializable" % obj)
 
 def view_error(request, msg):
     return render(request, "error.html", {
