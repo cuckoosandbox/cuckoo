a4a0b93b4f59137d51ff51119301083b41dd023b
Sander Ferdinand <sanfer@cuckoo.sh>
2016-08-20 03:37:49 +0200

    filetree tweaks and informative alertboxes

--- web/static/js/cuckoo/filetree.js
+++ web/static/js/cuckoo/filetree.js
@@ -13,6 +13,20 @@ var FileTree = function () {
         this._convert_data = convert_data;
 
         this._jstree = null;
+        this._filters = {
+            simplify_mime: true,
+            simplify_magic: true,
+            simplify_sizes: true,
+            deselect_duplicates: true
+        };
+
+        this.info = {
+            duplicates: 0,
+            files: 0,
+            containers: 0,
+            directories: 0,
+            executables: 0
+        };
     }
 
     /**
@@ -22,30 +36,35 @@ var FileTree = function () {
 
     _createClass(FileTree, [{
         key: "draw",
-        value: function draw(data) {
+        value: function draw(data, callback) {
             if (!this.sel_target) throw "drawtarget needed";
             this.data = data;
 
             this.init();
+            callback();
         }
     }, {
         key: "_convert_entry",
         value: function _convert_entry(entry) {
             var _self = this;
 
-            // container
+            // Temporary object
             var obj = {
                 filepath: entry.filepath,
                 filename: entry.filename,
                 type: entry.type,
                 state: false, // pre-selected tree item
-                magic: entry.magic,
                 size: entry.size,
-                mime: entry.mime,
-                duplicate: entry.duplicate
+                duplicate: entry.duplicate,
+                opened: false
             };
 
-            // sanatize object properties
+            if (obj.type != "directory") {
+                obj.magic = entry.finger.magic;
+                obj.mime = entry.finger.mime;
+            }
+
+            // Sanitize object properties
             if (obj.magic) {
                 if (obj.magic.length >= 170) {
                     obj.magic = obj.magic.substring(0, 170) + "...";
@@ -58,6 +77,8 @@ var FileTree = function () {
                 if (obj.filepath.endsWith(x)) {
                     obj.type = "exec";
                     obj.state = true;
+
+                    _self.info.executables += 1;
                 }
             });
 
@@ -65,23 +86,32 @@ var FileTree = function () {
                 if (obj.filepath.endsWith(x)) {
                     obj.type = "office";
                     obj.state = true;
+
+                    _self.info.executables += 1;
                 }
             });
 
-            // build jstree JSON object
+            // Build JSTree JSON object
             var data = {
                 text: obj.filename,
-                data: {}
+                data: {},
+                a_attr: {}
             };
 
             if (obj.duplicate) {
                 obj.type = "duplicate";
                 obj.state = false;
-                data.a_attr = { "filetree-duplicate": "true" };
+                data.a_attr.filetree_duplicate = "true";
+
+                _self.info.duplicates += 1;
             }
 
-            // build jstree JSON object
-            data.state = { "selected": obj.state };
+            if (obj.type == "directory") {
+                obj.opened = true;
+                _self.info.directories += 1;
+            } else if (obj.type == "file") {
+                _self.info.files += 1;
+            }
 
             if (obj.type != "directory") {
                 data.data.mime = obj.mime;
@@ -89,13 +119,20 @@ var FileTree = function () {
                 data.data.magic = obj.magic;
 
                 if (entry.children.length >= 1) {
-                    obj.type = "archive";
+                    obj.type = "container";
+                    obj.opened = true;
+                    _self.info.containers += 1;
                 }
             }
 
+            data.a_attr.filetree_type = obj.type;
             data.type = obj.type;
+            data.state = {
+                selected: obj.state,
+                opened: obj.opened
+            };
 
-            // recurse for child entries (make jstree leafs)
+            // Recurse for child entries (make jstree leafs)
             if (entry.children.length >= 1) {
                 entry.children.forEach(function (e) {
                     if (!data.hasOwnProperty("children")) {
@@ -124,7 +161,8 @@ var FileTree = function () {
                 var data_tmp = [];
                 for (var key in data) {
                     if (data.hasOwnProperty(key)) {
-                        data_tmp.push(this._convert_entry(data[key]));
+                        var converted = this._convert_entry(data[key]);
+                        data_tmp.push(converted);
                     }
                 }
 
@@ -141,7 +179,7 @@ var FileTree = function () {
                     }
                 },
                 types: {
-                    "archive": {
+                    "container": {
                         "icon": "fa fa-file-archive-o"
                     },
                     "file": {
@@ -158,17 +196,49 @@ var FileTree = function () {
                     }
                 },
                 grid: {
-                    columns: [{ width: 'auto', header: 'File' }, { width: 'auto', header: 'Mime', value: 'mime' }, { width: 'auto', header: 'Size', value: 'size' }, { width: '10px', header: 'Magic', value: 'magic' }],
+                    columns: [{ width: "auto", header: "File" }, { width: "auto", header: "Mime", value: "mime" }, { width: "auto", header: "Size", value: "size" }, { width: "10px", header: "Magic", value: "magic" }],
                     resizable: true
                 },
                 plugins: ["themes", "types", "checkbox", "grid", "wholerow"]
             })
-            //        .on('open_node.jstree', function (e, data) {
+            //        .on("open_node.jstree", function (e, data) {
             //            data.instance.set_icon(data.node, "fa fa-file-archive-o");
-            //        }).on('close_node.jstree', function (e, data) {
+            //        }).on("close_node.jstree", function (e, data) {
             //            data.instance.set_icon(data.node, "fa fa-file-archive-o");
             //        })
             ;
+        }
+
+        /**
+         * Programtically toggles the highlight of a jstree item
+         * @param {Object} [obj] - A jQuery object of a `a.jstree-grid.col-0` selector
+         * @param {String} [file_category] - "files", "containers", "exec"
+         * @param {Boolean} [highlight] - Wether to highlight or not
+         */
+
+    }], [{
+        key: "highlight",
+        value: function highlight(obj, file_category, _highlight) {
+            var item_type = obj.attr("filetree_type");
+            var item_dup = obj.attr("filetree_duplicate");
+
+            if (file_category == "files") {
+                if (item_type != "directory") {
+                    if (_highlight) obj.addClass("highlight");else obj.removeClass("highlight");
+                }
+            } else if (file_category == "exec") {
+                if (item_type == "exec") {
+                    if (_highlight) obj.addClass("highlight");else obj.removeClass("highlight");
+                }
+            } else if (file_category == "containers") {
+                if (item_type == "container" || item_type == "office") {
+                    if (_highlight) obj.addClass("highlight");else obj.removeClass("highlight");
+                }
+            } else if (file_category == "duplicates") {
+                if (item_dup == "true") {
+                    if (_highlight) obj.addClass("highlight");else obj.removeClass("highlight");
+                }
+            }
         }
     }]);
 
