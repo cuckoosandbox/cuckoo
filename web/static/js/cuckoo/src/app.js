6bbfbcee21c4e928b9daa4a2977cf5f566c7f3aa
Sander Ferdinand <sanfer@cuckoo.sh>
2016-08-20 04:58:51 +0200

    filetree simplification filters

---
+++ web/static/js/cuckoo/src/app.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    $("[data-toggle=popover]").popover();
+});
+
+function alertbox(msg, context, attr_id){
+    if(context) { context = `alert-${context}`; }
+    if(attr_id) { attr_id = `id="${attr_id}"`; }
+    return `<div ${attr_id} class="alert ${context} signature">${msg}</div>`;
+}
