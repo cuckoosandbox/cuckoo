2d83d61fbf440b47c87fa6abf7458e76fd56a817
Sander Ferdinand <sanfer@cuckoo.sh>
2016-08-20 04:58:51 +0200

    filetree simplification filters

--- web/static/js/cuckoo/src/filetree.js
+++ web/static/js/cuckoo/src/filetree.js
@@ -1,8 +1,9 @@
 class FileTree {
-    constructor(target, convert_data) {
+    constructor(target, data, sflock, draw_callback) {
         this.sel_target = target;
-        this.data = null;
-        this._convert_data = convert_data;
+        this.data = data;
+        this._draw_callback = draw_callback;
+        this._convert_from_sflock = sflock;
 
         this._jstree = null;
         this._filters = {
@@ -12,7 +13,7 @@ class FileTree {
             deselect_duplicates: true
         };
 
-        this.info = {
+        this.stats = {
             duplicates: 0,
             files: 0,
             containers: 0,
@@ -21,133 +22,42 @@ class FileTree {
         };
     }
 
-
     /**
      * Draws the table
      */
-    draw(data, callback){
+    draw(){
         if (!this.sel_target) throw "drawtarget needed";
-        this.data = data;
 
-        this.init();
-        callback();
+        this._init();
+        this._draw_callback();
     }
 
-    _convert_entry(entry){
-        let _self = this;
+    refresh(){
+        this._reset_stats();
+        let data = null;
 
-        // Temporary object
-        let obj = {
-            filepath: entry.filepath,
-            filename: entry.filename,
-            type: entry.type,
-            state: false, // pre-selected tree item
-            size: entry.size,
-            duplicate: entry.duplicate,
-            opened: false
-        };
-
-        if(obj.type != "directory"){
-            obj.magic = entry.finger.magic;
-            obj.mime = entry.finger.mime;
-        }
-
-        // Sanitize object properties
-        if(obj.magic){
-            if(obj.magic.length >= 170){ obj.magic = `${obj.magic.substring(0, 170)}...`; }
+        if(this._convert_from_sflock) {
+            data = this._convert_sflock();
         } else {
-            obj.magic = "empty";
+            data = this.data;
         }
 
-        [".exe", ".pdf", ".vbs", ".vba", ".bat", ".py", ".pyc", ".pl", ".rb", "js", ".jse"].forEach(function (x) {
-            if (obj.filepath.endsWith(x)) {
-                obj.type = "exec";
-                obj.state = true;
+        $(this.sel_target).jstree(true).settings.core.data = data;
+        $(this.sel_target).jstree(true).refresh();
 
-                _self.info.executables += 1;
-            }
-        });
-
-        [".doc", ".docx", ".docm", ".dotx", ".dotm", ".docb", ".xltm", ".xls", ".xltx", ".xlsm", ".xlsx", ".xlt", ".ppt", ".pps", ".pot"].forEach(function (x) {
-            if (obj.filepath.endsWith(x)) {
-                obj.type = "office";
-                obj.state = true;
-
-                _self.info.executables += 1;
-            }
-        });
-
-        // Build JSTree JSON object
-        let data = {
-            text: obj.filename,
-            data: {},
-            a_attr: {}
-        };
-
-        if(obj.duplicate) {
-            obj.type = "duplicate";
-            obj.state = false;
-            data.a_attr.filetree_duplicate = "true";
-
-            _self.info.duplicates += 1;
-        }
-
-        if(obj.type == "directory"){
-            obj.opened = true;
-            _self.info.directories += 1;
-        } else if (obj.type == "file"){
-            _self.info.files += 1;
-        }
-
-        if(obj.type != "directory") {
-            data.data.mime = obj.mime;
-            data.data.size = obj.size;
-            data.data.magic = obj.magic;
-
-            if(entry.children.length >= 1) {
-                obj.type = "container";
-                obj.opened = true;
-                _self.info.containers += 1;
-            }
-        }
-
-        data.a_attr.filetree_type = obj.type;
-        data.type = obj.type;
-        data.state = {
-            selected: obj.state,
-            opened: obj.opened
-        };
-        
-        // Recurse for child entries (make jstree leafs)
-        if(entry.children.length >= 1){
-            entry.children.forEach(function(e){
-                if(!data.hasOwnProperty("children")) { data.children = []; }
-                data.children.push(_self._convert_entry(e));
-            })
-        }
-
-        return data;
+        this._draw_callback();
     }
 
     /**
      * Init the table
      */
-    init(){
-        $(this.sel_target).empty();
+    _init(){
+        let data = null;
 
-        let data = this.data;
-
-        // Convert the data from the `sflock` format to jstree JSON data if necessary
-        if(this._convert_data){
-            var data_tmp = [];
-            for (var key in data) {
-                if (data.hasOwnProperty(key)) {
-                    let converted = this._convert_entry(data[key]);
-                    data_tmp.push(converted);
-                }
-            }
-
-            data = data_tmp;
+        if(this._convert_from_sflock) {
+            data = this._convert_sflock();
+        } else {
+            data = this.data;
         }
 
         $(this.sel_target).jstree({
@@ -186,14 +96,151 @@ class FileTree {
                 resizable: true
             },
             plugins: ["themes", "types", "checkbox", "grid", "wholerow"]
-        })
-//        .on("open_node.jstree", function (e, data) {
-//            data.instance.set_icon(data.node, "fa fa-file-archive-o");
-//        }).on("close_node.jstree", function (e, data) {
-//            data.instance.set_icon(data.node, "fa fa-file-archive-o");
-//        })
-        ;
+        });
 
+        $(this.sel_target).bind("ready.jstree", function(){
+            let sel_wrapper = $(".jstree-grid-wrapper");
+            sel_wrapper.css("min-height", sel_wrapper.outerHeight());
+        });
+    }
+
+    /**
+     * Convert data from the `sflock` format to JSTree
+     * @private
+     */
+    _convert_sflock(){
+        let data = $.extend({}, this.data);
+
+        let data_tmp = [];
+        for (let key in data) {
+            if (data.hasOwnProperty(key)) {
+                let converted = this._convert_entry(data[key]);
+                data_tmp.push(converted);
+            }
+        }
+
+        return data_tmp;
+    }
+
+    _convert_entry(entry){
+        let _self = this;
+
+        // Temporary object
+        let obj = {
+            filepath: entry.filepath,
+            filename: entry.filename,
+            type: entry.type,
+            state: false, // pre-selected tree item
+            size: entry.size,
+            duplicate: entry.duplicate,
+            opened: false
+        };
+
+        if(obj.type != "directory"){
+            if(this._filters.simplify_magic){
+                obj.magic = entry.finger.magic_human;
+            } else {
+                obj.magic = entry.finger.magic;
+            }
+
+            if(this._filters.simplify_mime){
+                obj.mime = entry.finger.mime_human;
+            } else{ obj.mime = entry.finger.mime; }
+
+        }
+
+        // Sanitize object properties
+        if(obj.magic){
+            if(obj.magic.length >= 170){ obj.magic = `${obj.magic.substring(0, 170)}...`; }
+        } else {
+            obj.magic = "empty";
+        }
+
+        [".exe", ".pdf", ".vbs", ".vba", ".bat", ".py", ".pyc", ".pl", ".rb", "js", ".jse"].forEach(function (x) {
+            if (obj.filepath.endsWith(x)) {
+                obj.type = "exec";
+                obj.state = true;
+
+                _self.stats.executables += 1;
+            }
+        });
+
+        [".doc", ".docx", ".docm", ".dotx", ".dotm", ".docb", ".xltm", ".xls", ".xltx", ".xlsm", ".xlsx", ".xlt", ".ppt", ".pps", ".pot"].forEach(function (x) {
+            if (obj.filepath.endsWith(x)) {
+                obj.type = "office";
+                obj.state = true;
+
+                _self.stats.executables += 1;
+            }
+        });
+
+        // Build the JSTree JSON return object
+        let data = {
+            text: obj.filename,
+            data: {},
+            a_attr: {}
+        };
+
+        if(obj.duplicate) {
+            obj.type = "duplicate";
+
+            // Deselect duplicate file entries depending on the filter settings
+            if(this._filters.deselect_duplicates){
+                obj.state = false;
+            }
+
+            // Set class for CSS
+            data.a_attr.filetree_duplicate = "true";
+
+            // Update stats
+            _self.stats.duplicates += 1;
+        }
+
+        if(obj.type == "directory"){
+            obj.opened = true;
+            _self.stats.directories += 1;
+        } else if (obj.type == "file"){
+            _self.stats.files += 1;
+        }
+
+        if(obj.type != "directory") {
+            data.data.mime = obj.mime;
+            data.data.size = obj.size;
+            data.data.magic = obj.magic;
+
+            if(entry.children.length >= 1) {
+                obj.type = "container";
+                obj.opened = true;
+                _self.stats.containers += 1;
+            }
+        }
+
+        data.a_attr.filetree_type = obj.type;
+        data.type = obj.type;
+        data.state = {
+            selected: obj.state,
+            opened: obj.opened
+        };
+
+        // Recurse this function for the child entries
+        if(entry.children.length >= 1){
+            entry.children.forEach(function(e){
+                if(!data.hasOwnProperty("children")) { data.children = []; }
+                data.children.push(_self._convert_entry(e));
+            })
+        }
+
+        return data;
+    }
+
+    _reset_stats(){
+        this.stats = {
+            duplicates: 0,
+            files: 0,
+            containers: 0,
+            directories: 0,
+            executables: 0
+        };
     }
 
     /**
@@ -227,5 +274,19 @@ class FileTree {
                 else obj.removeClass("highlight");
             }
         }
+    }
+
+    simplify(state){
+        this._filters.simplify_mime = state;
+        this._filters.simplify_sizes = state;
+        this._filters.simplify_magic = state;
+
+        this.refresh();
+    }
+
+    duplicates(state){
+        this._filters.deselect_duplicates = state;
+
+        this.refresh();
     }
 }
