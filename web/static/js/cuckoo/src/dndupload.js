991167ef1c2d7a165d41a268e82f7793c1c6cba0
Sander Ferdinand <sanfer@cuckoo.sh>
2016-08-23 01:45:33 +0200

    drag&drop firefox fix

--- web/static/js/cuckoo/src/dndupload.js
+++ web/static/js/cuckoo/src/dndupload.js
@@ -39,8 +39,8 @@ class DndUpload {
         $(this._selectors["target"]).empty();
 
         let html = `
-            <div class="dndupload" id="${this._selectors['uid']}">
-                <form id="uploader">
+            <div class="dndupload" id="${this._selectors["uid"]}">
+                <form id="uploader" action="/submit/api/submit" method="POST" enctype="multipart/form-data">
                     <div id="container">
                         <svg xmlns="http://www.w3.org/2000/svg" width="50" height="43" viewBox="0 0 50 43">
                             <path d="M48.4 26.5c-.9 0-1.7.7-1.7 1.7v11.6h-43.3v-11.6c0-.9-.7-1.7-1.7-1.7s-1.7.7-1.7 1.7v13.2c0 .9.7 1.7 1.7 1.7h46.7c.9 0 1.7-.7 1.7-1.7v-13.2c0-1-.7-1.7-1.7-1.7zm-24.5 6.1c.3.3.8.5 1.2.5.4 0 .9-.2 1.2-.5l10-11.6c.7-.7.7-1.7 0-2.4s-1.7-.7-2.4 0l-7.1 8.3v-25.3c0-.9-.7-1.7-1.7-1.7s-1.7.7-1.7 1.7v25.3l-7.1-8.3c-.7-.7-1.7-.7-2.4 0s-.7 1.7 0 2.4l10 11.6z"/>
@@ -198,13 +198,14 @@ class DndUpload {
 
         xhr.onreadystatechange = function(){
             if(xhr.readyState === 4){
-                let response = JSON.parse(xhr.responseText);
-
-                if(xhr.status === 200 && response.status === "OK") {
+                if(xhr.status == 200){
                     _self.display_text("Done");
+
                     setTimeout(function() {
-                        _self._success_callback(response)
+                        _self._success_callback(xhr)
                     }, 600);
+                } else if(xhr.status == 0) {
+
                 } else {
                     _self.display_text(`Error: http.status = ${xhr.status} OR response.status not OK`);
                 }
