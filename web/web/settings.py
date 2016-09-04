f3e40221b5a117c5b0571b9fe417c88056f7ccfe
Jurriaan Bremer <jbr@cuckoo.sh>
2016-09-04 03:48:10 +0200

    Recent analysis page now supports other task statuses + column order and css changed

--- web/web/settings.py
+++ web/web/settings.py
@@ -162,8 +162,7 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
-    'analysis',
-    'compare',
+    'analysis'
 )
 
 LOGIN_REDIRECT_URL = "/"
