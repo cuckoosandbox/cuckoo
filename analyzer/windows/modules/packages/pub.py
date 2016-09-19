ff1eebf1dcc22ae0b8ce32d2359c734ce1fcb1ed
doomedraven <doomedraven@users.noreply.github.com>
2016-09-20 00:39:32 +0200

    typo in description

--- analyzer/windows/modules/packages/pub.py
+++ analyzer/windows/modules/packages/pub.py
@@ -40,7 +40,7 @@ class PUB(Package):
             HKEY_CURRENT_USER,
             "Software\\Microsoft\\Office\\15.0\\Publisher\\Security",
             {
-                # Enable VBA macros in Office 2007.
+                # Enable VBA macros in Office 2013.
                 "VBAWarnings": 1,
                 "AccessVBOM": 1,
 
