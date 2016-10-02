0381d143c6018afc5b7dfa0787d2f6596ad80625
Kacper Sokol <ks1591@my.bristol.ac.uk>
2016-10-02 12:16:38 +0100

    `patterns` list moved from function to class body.

--- modules/cuckooml/cuckooml.py
+++ modules/cuckooml/cuckooml.py
@@ -355,6 +355,38 @@ class ML(object):
         }
     }
 
+    PATTERNS = [r"Armadillo", r"PECompact", r"ASPack", r"ASProtect",
+        r"Upack", r"U(PX|px)", r"FSG", r"BobSoft Mini Delphi",
+        r"InstallShield 2000", r"InstallShield Custom",
+        r"Xtreme\-Protector", r"Crypto\-Lock", r"MoleBox", r"Dev\-C\+\+",
+        r"StarForce", r"Wise Installer Stub", r"SVK Protector",
+        r"eXPressor", r"EXECryptor", r"N(s|S)Pac(k|K)", r"KByS",
+        r"themida", r"Packman", r"EXE Shield", r"WinRAR 32-bit SFX",
+        r"WinZip 32-bit SFX", r"Install Stub 32-bit", r"P(E|e)tite",
+        r"PKLITE32", r"y(o|0)da's (Protector|Crypter)", r"Ste@lth PE",
+        r"PE\-Armor", r"KGB SFX", r"tElock", r"PEBundle", r"Crunch\/PE",
+        r"Obsidium", r"nPack", r"PEX", r"PE Diminisher",
+        r"North Star PE Shrinker", r"PC Guard for Win32", r"W32\.Jeefo",
+        r"MEW [0-9]+", r"InstallAnywhere", r"Anskya Binder",
+        r"BeRoEXEPacker", r"NeoLite", r"SVK\-Protector",
+        r"Ding Boy's PE\-lock Phantasm", r"hying's PEArmor", r"E language",
+        r"NSIS Installer", r"Video\-Lan\-Client", r"EncryptPE",
+        r"HASP HL Protection", r"PESpin", r"CExe", r"UG2002 Cruncher",
+        r"ACProtect", r"Thinstall", r"DBPE", r"XCR", r"PC Shrinker",
+        r"AH(p|P)ack", r"ExeShield Protector",
+        r"\* \[MSLRH\]", r"XJ \/ XPAL", r"Krypton", r"Stealth PE",
+        r"Goats Mutilator", r"PE\-PACK", r"RCryptor", r"\* PseudoSigner",
+        r"Shrinker", r"PC-Guard", r"PELOCKnt", r"WinZip \(32\-bit\)",
+        r"EZIP", r"PeX", r"PE( |\-)Crypt", r"E(XE|xe)()?Stealth",
+        r"ShellModify", r"Macromedia Windows Flash Projector\/Player",
+        r"WARNING ->", r"PE Protector", r"Software Compress",
+        r"PE( )?Ninja", r"Feokt", r"RLPack",
+        r"Nullsoft( PIMP)? Install System", r"SDProtector Pro Edition",
+        r"VProtector", r"WWPack32", r"CreateInstall Stub", r"ORiEN",
+        r"dePACK", r"ENIGMA Protector", r"MicroJoiner", r"Virogen Crypt",
+        r"SecureEXE", r"PCShrink", r"WinZip Self\-Extractor",
+        r"PEiD\-Bundle", r"DxPack", r"Freshbind", r"kkrunchy"]
+
     def __init__(self, context="standalone"):
         """The context variable defines type of operation for the functions
         using visualisations. *standalone* saves figures as files, *notebook*
@@ -547,38 +579,7 @@ class ML(object):
                 my_features[i][":sign:" + j] = 1
 
             # Extract packer
-            patterns = [r"Armadillo", r"PECompact", r"ASPack", r"ASProtect",
-            r"Upack", r"U(PX|px)", r"FSG", r"BobSoft Mini Delphi",
-            r"InstallShield 2000", r"InstallShield Custom",
-            r"Xtreme\-Protector", r"Crypto\-Lock", r"MoleBox", r"Dev\-C\+\+",
-            r"StarForce", r"Wise Installer Stub", r"SVK Protector",
-            r"eXPressor", r"EXECryptor", r"N(s|S)Pac(k|K)", r"KByS",
-            r"themida", r"Packman", r"EXE Shield", r"WinRAR 32-bit SFX",
-            r"WinZip 32-bit SFX", r"Install Stub 32-bit", r"P(E|e)tite",
-            r"PKLITE32", r"y(o|0)da's (Protector|Crypter)", r"Ste@lth PE",
-            r"PE\-Armor", r"KGB SFX", r"tElock", r"PEBundle", r"Crunch\/PE",
-            r"Obsidium", r"nPack", r"PEX", r"PE Diminisher",
-            r"North Star PE Shrinker", r"PC Guard for Win32", r"W32\.Jeefo",
-            r"MEW [0-9]+", r"InstallAnywhere", r"Anskya Binder",
-            r"BeRoEXEPacker", r"NeoLite", r"SVK\-Protector",
-            r"Ding Boy's PE\-lock Phantasm", r"hying's PEArmor", r"E language",
-            r"NSIS Installer", r"Video\-Lan\-Client", r"EncryptPE",
-            r"HASP HL Protection", r"PESpin", r"CExe", r"UG2002 Cruncher",
-            r"ACProtect", r"Thinstall", r"DBPE", r"XCR", r"PC Shrinker",
-            r"AH(p|P)ack", r"ExeShield Protector",
-            r"\* \[MSLRH\]", r"XJ \/ XPAL", r"Krypton", r"Stealth PE",
-            r"Goats Mutilator", r"PE\-PACK", r"RCryptor", r"\* PseudoSigner",
-            r"Shrinker", r"PC-Guard", r"PELOCKnt", r"WinZip \(32\-bit\)",
-            r"EZIP", r"PeX", r"PE( |\-)Crypt", r"E(XE|xe)()?Stealth",
-            r"ShellModify", r"Macromedia Windows Flash Projector\/Player",
-            r"WARNING ->", r"PE Protector", r"Software Compress",
-            r"PE( )?Ninja", r"Feokt", r"RLPack",
-            r"Nullsoft( PIMP)? Install System", r"SDProtector Pro Edition",
-            r"VProtector", r"WWPack32", r"CreateInstall Stub", r"ORiEN",
-            r"dePACK", r"ENIGMA Protector", r"MicroJoiner", r"Virogen Crypt",
-            r"SecureEXE", r"PCShrink", r"WinZip Self\-Extractor",
-            r"PEiD\-Bundle", r"DxPack", r"Freshbind", r"kkrunchy"]
-            regexps = [re.compile(pattern) for pattern in patterns]
+            regexps = [re.compile(pattern) for pattern in self.PATTERNS]
             if features[i]["packer"] is not None:
                 for packer in features[i]["packer"]:
                     for regexp in regexps:
