780f319913722a38f11681573089583f386f992c
Sander Ferdinand <sa.ferdinand@gmail.com>
2016-10-13 23:20:59 +0200

    CuckooWeb miscellaneous functions class

--- cuckoo/web/static/js/cuckoo/app.js
+++ cuckoo/web/static/js/cuckoo/app.js
@@ -1,9 +1,3 @@
-'use strict';
-
-var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
-
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
 /*
  * Copyright (C) 2010-2013 Claudio Guarnieri.
  * Copyright (C) 2014-2016 Cuckoo Foundation.
@@ -15,62 +9,51 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
 /**
  * CuckooWeb miscellaneous functions class.
  */
-var CuckooWeb = function () {
-    function CuckooWeb() {
-        _classCallCheck(this, CuckooWeb);
+class CuckooWeb {
+    // mpen @ http://stackoverflow.com/a/14919494/2054778
+    static human_size(bytes, si) {
+        var thresh = si ? 1000 : 1024;
+        if (Math.abs(bytes) < thresh) {
+            return bytes + ' B';
+        }
+        var units = si ? ['kB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'] : ['KiB', 'MiB', 'GiB', 'TiB', 'PiB', 'EiB', 'ZiB', 'YiB'];
+        var u = -1;
+        do {
+            bytes /= thresh;
+            ++u;
+        } while (Math.abs(bytes) >= thresh && u < units.length - 1);
+        return bytes.toFixed(1) + ' ' + units[u];
     }
 
-    _createClass(CuckooWeb, null, [{
-        key: 'human_size',
+    static api_post(url, params, callback, errback, beforesend) {
+        let data = JSON.stringify(params);
 
-        // mpen @ http://stackoverflow.com/a/14919494/2054778
-        value: function human_size(bytes, si) {
-            var thresh = si ? 1000 : 1024;
-            if (Math.abs(bytes) < thresh) {
-                return bytes + ' B';
+        $.ajax({
+            type: "post",
+            contentType: "application/json",
+            url: url,
+            dataType: "json",
+            data: data,
+            timeout: 20000,
+            beforeSend: function () {
+                if (beforesend) {
+                    beforesend();
+                }
+            },
+            success: function (data) {
+                if (callback) {
+                    callback(data);
+                }
             }
-            var units = si ? ['kB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'] : ['KiB', 'MiB', 'GiB', 'TiB', 'PiB', 'EiB', 'ZiB', 'YiB'];
-            var u = -1;
-            do {
-                bytes /= thresh;
-                ++u;
-            } while (Math.abs(bytes) >= thresh && u < units.length - 1);
-            return bytes.toFixed(1) + ' ' + units[u];
-        }
-    }, {
-        key: 'api_post',
-        value: function api_post(url, params, callback, errback, beforesend) {
-            var data = JSON.stringify(params);
+        }).fail(function (err) {
+            console.log(`ajax post error: ${ err }`);
 
-            $.ajax({
-                type: "post",
-                contentType: "application/json",
-                url: url,
-                dataType: "json",
-                data: data,
-                timeout: 20000,
-                beforeSend: function beforeSend() {
-                    if (beforesend) {
-                        beforesend();
-                    }
-                },
-                success: function success(data) {
-                    if (callback) {
-                        callback(data);
-                    }
-                }
-            }).fail(function (err) {
-                console.log('ajax post error: ' + err);
-
-                if (errback) {
-                    errback(err);
-                }
-            });
-        }
-    }]);
-
-    return CuckooWeb;
-}();
+            if (errback) {
+                errback(err);
+            }
+        });
+    }
+}
 
 $(document).ready(function () {
     $("[data-toggle=popover]").popover();
@@ -78,12 +61,12 @@ $(document).ready(function () {
 
 function alertbox(msg, context, attr_id) {
     if (context) {
-        context = 'alert-' + context;
+        context = `alert-${ context }`;
     }
     if (attr_id) {
-        attr_id = 'id="' + attr_id + '"';
+        attr_id = `id="${ attr_id }"`;
     }
-    return '<div ' + attr_id + ' class="alert ' + context + ' signature">' + msg + '</div>';
+    return `<div ${ attr_id } class="alert ${ context } signature">${ msg }</div>`;
 }
 
 String.prototype.capitalize = function () {
