6ac08c98ae4d91471eb80f1fd7cf031b3326492c
Koen Houtman <automagisch@cuckoo.sh>
2016-10-26 12:32:51 +0200

    fixed babel!

--- cuckoo/web/src/tasks/scripts.js
+++ cuckoo/web/src/tasks/scripts.js
@@ -1,35 +1,47 @@
-var fs 			= require('fs');
-var gulp 		= require('gulp');
-var browserify 	= require('browserify');
-var babelify 	= require('babelify');
-var source 		= require('vinyl-source-stream');
-var buffer 		= require('vinyl-buffer');
-var es 			= require('event-stream');
-var glob 		= require('glob');
-var rename 		= require('gulp-rename');
+// var fs 			= require('fs');
+// var gulp 		= require('gulp');
+// var browserify 	= require('browserify');
+// var babelify 	= require('babelify');
+// var source 		= require('vinyl-source-stream');
+// var buffer 		= require('vinyl-buffer');
+// var es 			= require('event-stream');
+// var glob 		= require('glob');
+// var rename 		= require('gulp-rename');
+
+var gulp = require('gulp');
+var babel = require('gulp-babel');
+var sourcemaps = require('gulp-sourcemaps');
 
 module.exports = function(done) {
 
-	glob('./scripts/*.js', function(err, files) {
+	return gulp.src('./scripts/**/*.js')
+		.pipe(sourcemaps.init())
+		.pipe(babel({
+			presets: 'es2015'
+		}))
+		.pipe(sourcemaps.write('.'))
+		.pipe(gulp.dest('../static/js/cuckoo'));
 
-		if(err) {
-			done(err);
-			return;	
-		}
+	// glob('./scripts/*.js', function(err, files) {
 
-		var tasks = files.map(function(entry) {
-			return browserify({entries: entry})
-				.transform(babelify, { presets: "es2015" })
-				.bundle()
-				.pipe(source(entry))
-				.pipe(rename(function(path) {
-					path.dirname = './';
-				}))
-				.pipe(gulp.dest('../static/js/cuckoo/'));
-		});
+	// 	if(err) {
+	// 		done(err);
+	// 		return;	
+	// 	}
 
-		es.merge(tasks).on('end', done);
+	// 	var tasks = files.map(function(entry) {
+	// 		return browserify({entries: entry})
+	// 			.transform(babelify, { presets: "es2015" })
+	// 			.bundle()
+	// 			.pipe(source(entry))
+	// 			.pipe(rename(function(path) {
+	// 				path.dirname = './';
+	// 			}))
+	// 			.pipe(gulp.dest('../static/js/cuckoo/'));
+	// 	});
 
-	});
+	// 	es.merge(tasks).on('end', done);
+
+	// });
 
 }
--- cuckoo/web/static/js/cuckoo/filetree.js
+++ cuckoo/web/static/js/cuckoo/filetree.js
@@ -1,4 +1,3 @@
-(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 "use strict";
 
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
@@ -389,5 +388,4 @@ var FileTree = function () {
 
     return FileTree;
 }();
-
-},{}]},{},[1]);
+//# sourceMappingURL=filetree.js.map
