688a8322b11a86c04224628b530dc626e280208c
Jurriaan Bremer <jbr@cuckoo.sh>
2016-10-29 15:32:57 +0200

    setup unit testing for postgresql and mysql
    Let's no longer do a drop database unit test, as PostgreSQL and MySQL
    don't like that so much - and why would we anyway. Also note that we're
    not installing PostgreSQL as that's already installed on Travis CI by
    default.

--- tests/test_database.py
+++ tests/test_database.py
@@ -3,65 +3,58 @@
 # This file is part of Cuckoo Sandbox - http://www.cuckoosandbox.org
 # See the file 'docs/LICENSE' for copying permission.
 
+import os
 import pytest
 import tempfile
 
-from sqlalchemy.exc import OperationalError
-
-from cuckoo.core.database import Database, Sample, Task
+from cuckoo.core.database import Database, Task
 from cuckoo.misc import set_cwd
 
-class TestDatabase:
+class DatabaseEngine(object):
     """Tests database stuff."""
+    URI = None
 
     def setup(self):
         set_cwd(tempfile.mkdtemp())
 
         self.d = Database()
-        self.d.connect(dsn="sqlite:///:memory:")
+        self.d.connect(dsn=self.URI)
 
     def add_url(self, url, priority=1, status="pending"):
         task_id = self.d.add_url(url, priority=priority)
         self.d.set_status(task_id, status)
         return task_id
 
-    def test_drop(self):
+    def test_add_tasks(self):
+        fd, sample_path = tempfile.mkstemp()
+        os.write(fd, "hehe")
+        os.close(fd)
+
         # Add task.
-        sample_path = tempfile.mkstemp()[1]
+        count = self.d.Session().query(Task).count()
         self.d.add_path(sample_path)
-        session = self.d.Session()
-        assert session.query(Sample).count() == 1
-        assert session.query(Task).count() == 1
+        assert self.d.Session().query(Task).count() == count + 1
 
         # Add url.
         self.d.add_url("http://foo.bar")
-        assert session.query(Sample).count() == 1
-        assert session.query(Task).count() == 2
-
-        # Drop.
-        self.d.drop()
-
-        # After dropping a database, any other query raises a OperationalError
-        # exception; "no such table".
-        with pytest.raises(OperationalError):
-            assert session.query(Task).count() == 0
+        assert self.d.Session().query(Task).count() == count + 2
 
     def test_processing_get_task(self):
-        self.add_url("http://google.com/1", priority=1, status="completed")
-        self.add_url("http://google.com/2", priority=2, status="completed")
-        self.add_url("http://google.com/3", priority=1, status="completed")
-        self.add_url("http://google.com/4", priority=1, status="completed")
-        self.add_url("http://google.com/5", priority=3, status="completed")
-        self.add_url("http://google.com/6", priority=1, status="completed")
-        self.add_url("http://google.com/7", priority=1, status="completed")
+        t1 = self.add_url("http://google.com/1", priority=1, status="completed")
+        t2 = self.add_url("http://google.com/2", priority=2, status="completed")
+        t3 = self.add_url("http://google.com/3", priority=1, status="completed")
+        t4 = self.add_url("http://google.com/4", priority=1, status="completed")
+        t5 = self.add_url("http://google.com/5", priority=3, status="completed")
+        t6 = self.add_url("http://google.com/6", priority=1, status="completed")
+        t7 = self.add_url("http://google.com/7", priority=1, status="completed")
 
-        assert self.d.processing_get_task("foo") == 5
-        assert self.d.processing_get_task("foo") == 2
-        assert self.d.processing_get_task("foo") == 1
-        assert self.d.processing_get_task("foo") == 3
-        assert self.d.processing_get_task("foo") == 4
-        assert self.d.processing_get_task("foo") == 6
-        assert self.d.processing_get_task("foo") == 7
+        assert self.d.processing_get_task("foo") == t5
+        assert self.d.processing_get_task("foo") == t2
+        assert self.d.processing_get_task("foo") == t1
+        assert self.d.processing_get_task("foo") == t3
+        assert self.d.processing_get_task("foo") == t4
+        assert self.d.processing_get_task("foo") == t6
+        assert self.d.processing_get_task("foo") == t7
         assert self.d.processing_get_task("foo") is None
 
     def test_error_exists(self):
@@ -75,3 +68,15 @@ class TestDatabase:
         self.d.add_error("A"*1024, 1)
         err = self.d.view_errors(1)
         assert err and len(err[0].message) == 1024
+
+class TestSqlite3Memory(DatabaseEngine):
+    URI = "sqlite:///:memory:"
+
+class TestSqlite3File(DatabaseEngine):
+    URI = "sqlite:///%s" % tempfile.mktemp()
+
+class TestPostgreSQL(DatabaseEngine):
+    URI = "postgresql://cuckoo:cuckoo@localhost/cuckootest"
+
+class TestMySQL(DatabaseEngine):
+    URI = "mysql://cuckoo:cuckoo@localhost/cuckootest"
