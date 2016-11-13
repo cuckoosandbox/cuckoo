1e479df11b178074619c1d9b4ce4f6268bcf1776
Jurriaan Bremer <jbr@cuckoo.sh>
2016-11-14 00:54:16 +0100

    write supervisord.conf using jinja2
    Manually verified that the generated output 100% matches with what we
    wrote away before. Also lowercases all the dependencies as PyPi is
    case-insensitive anyway.

--- cuckoo/core/init.py
+++ cuckoo/core/init.py
@@ -2,64 +2,33 @@
 # This file is part of Cuckoo Sandbox - http://www.cuckoosandbox.org
 # See the file 'docs/LICENSE' for copying permission.
 
+import jinja2
 import os
-import sys
 
 from cuckoo.misc import cwd
 
 def write_supervisor_conf(username):
     """Writes supervisord.conf configuration file if it does not exist yet."""
+    # TODO Handle updates?
     if os.path.exists(cwd("supervisord.conf")):
         return
 
+    if os.environ.get("VIRTUAL_ENV"):
+        virtualenv = os.path.join(os.environ["VIRTUAL_ENV"], "bin")
+        python_path = os.path.join(virtualenv, "python")
+        cuckoo_path = os.path.join(virtualenv, "cuckoo")
+    else:
+        python_path = "python"
+        cuckoo_path = "cuckoo"
+
+    template = jinja2.Environment().from_string(
+        open(cwd("cwd", "supervisord.jinja2", private=True), "rb").read()
+    )
+
     with open(cwd("supervisord.conf"), "wb") as f:
-        # Temporarily redirect stdout to our file.
-        f, sys.stdout = sys.stdout, f
-
-        if os.environ.get("VIRTUAL_ENV"):
-            virtualenv = os.path.join(os.environ["VIRTUAL_ENV"], "bin")
-            python_path = os.path.join(virtualenv, "python")
-            cuckoo_path = os.path.join(virtualenv, "cuckoo")
-        else:
-            python_path = "python"
-            cuckoo_path = "cuckoo"
-
-        print "[supervisord]"
-        print "logfile =", cwd("supervisord", "log.log")
-        print "pidfile =", cwd("supervisord", "pidfile")
-        print "user =", username
-        print
-        print "[supervisorctl]"
-        print "serverurl = unix://%s" % cwd("supervisord", "unix.sock")
-        print
-        print "[rpcinterface:supervisor]"
-        print "supervisor.rpcinterface_factory =",
-        print "supervisor.rpcinterface:make_main_rpcinterface"
-        print
-        print "[unix_http_server]"
-        print "file =", cwd("supervisord", "unix.sock")
-        print
-        print "[program:cuckoo-daemon]"
-        print "command = %s -d -m 10000" % cuckoo_path
-        print "user =", username
-        print "startsecs = 30"
-        print "autorestart = true"
-        print
-        print "[program:cuckoo-process]"
-        print "command = %s process p%%(process_num)d" % cuckoo_path
-        print "process_name = cuckoo-process_%(process_num)d"
-        print "numprocs = 4"
-        print "user =", username
-        print "autorestart = true"
-        print
-        print "[group:cuckoo]"
-        print "programs = cuckoo-daemon, cuckoo-process"
-        print
-        print "[program:distributed]"
-        print "command = %s -m cuckoo.distributed.worker" % python_path
-        print "user =", username
-        print "autostart = false"
-        print "autorestart = true"
-        print 'environment = CUCKOO_APP="worker",CUCKOO_CWD="%s"' % cwd()
-
-        f, sys.stdout = sys.stdout, f
+        f.write(template.render({
+            "cwd": cwd,
+            "username": username,
+            "cuckoo_path": cuckoo_path,
+            "python_path": python_path,
+        }).rstrip() + "\n")
