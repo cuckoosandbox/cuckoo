45958e951b9240fb59ef62f2cbbc88a99d899a0a
Koen Houtman <automagisch@cuckoo.sh>
2016-11-18 14:26:57 +0100

    Improved the sidebar
    - now the bar always stays on the left, even when you scroll the regular page.
    - added a 'lock' feature. Cool thing: The browser reminds whether you did or did not lock the sidebar! (using local storage)
    - added some basic markup for searching

--- cuckoo/web/src/scripts/analysis_sidebar.js
+++ cuckoo/web/src/scripts/analysis_sidebar.js
@@ -1,35 +1,94 @@
 class AnalysisSidebar {
 
 	constructor(_$) {
+
 		this.$ = _$;
+		this.searchbar = this.$.find('input[name="sidebar_search"]');
+
 		this.open = false;
+		this.locked = false;
+
+		if(!window.localStorage.getItem('cuckoo-sidebar-locked')) {
+			window.localStorage.setItem('cuckoo-sidebar-locked', 'false');
+		} else {
+			window.localStorage.getItem('cuckoo-sidebar-locked') == 'true' ? this.lock() : null;
+		}
 
 		this.activateListeners();
+		this.scrollHandler();
+
 	}
 
 	activateListeners() {
 
+		let self = this;
+
+		// enable mouse opening
 		this.$.bind('mouseenter', function(e) {
-			this.onMouseEnter(e);
-		}.bind(this)).bind('mouseleave', function(e) {
-			this.onMouseOut(e);
-		}.bind(this));
+			self.onMouseEnter(e);
+		}).bind('mouseleave', function(e) {
+			self.onMouseOut(e);
+		});
+
+		// disable scrolling the nav
+		$(document).on('scroll', function(e) {
+			e.preventDefault();
+			return self.scrollHandler(e);
+		});	
+
+		this.$.find('[href^=sidebar]').bind('click', function(e) {
+			e.preventDefault();
+			var action = $(this).attr('href').split(':')[1];
+
+			switch(action) {
+				case 'toggle-lock':
+					self.toggleLock();
+				break;
+			}
+		});
 
 	}
 
 	onMouseEnter(e) {
+		this.open = true;
 		this.$.addClass('open');
 	}
 
 	onMouseOut(e) {
+		this.open = false;
 		this.$.removeClass('open');
+	}
+
+	scrollHandler(e) {
+		var top = $(window).scrollTop();
+		this.$.find('.cuckoo-nav').css('transform','translate3d(0,'+top+'px,0)');
+	}
+
+	lock() {
+		this.locked = true;
+		this.$.addClass('locked');
+		window.localStorage.setItem('cuckoo-sidebar-locked', true);
+	}
+
+	unlock() {
+		this.locked = false;
+		this.$.removeClass('locked');
+		window.localStorage.setItem('cuckoo-sidebar-locked', false);
+	}
+
+	toggleLock() {
+		if(this.locked) {
+			this.unlock();
+		} else {
+			this.lock();
+		}
 	}
 
 }
 
 $(function() {
 
-	var sidebar = new AnalysisSidebar($('#analysis-nav'));
-	console.log(sidebar);
+	var sidebar;
+	if($("#analysis-nav").length) sidebar = new AnalysisSidebar($('#analysis-nav'));
 
 });
--- cuckoo/web/static/js/cuckoo/analysis_sidebar.js
+++ cuckoo/web/static/js/cuckoo/analysis_sidebar.js
@@ -9,30 +9,91 @@ var AnalysisSidebar = function () {
 		_classCallCheck(this, AnalysisSidebar);
 
 		this.$ = _$;
+		this.searchbar = this.$.find('input[name="sidebar_search"]');
+
 		this.open = false;
+		this.locked = false;
+
+		if (!window.localStorage.getItem('cuckoo-sidebar-locked')) {
+			window.localStorage.setItem('cuckoo-sidebar-locked', 'false');
+		} else {
+			window.localStorage.getItem('cuckoo-sidebar-locked') == 'true' ? this.lock() : null;
+		}
 
 		this.activateListeners();
+		this.scrollHandler();
 	}
 
 	_createClass(AnalysisSidebar, [{
 		key: 'activateListeners',
 		value: function activateListeners() {
 
+			var self = this;
+
+			// enable mouse opening
 			this.$.bind('mouseenter', function (e) {
-				this.onMouseEnter(e);
-			}.bind(this)).bind('mouseleave', function (e) {
-				this.onMouseOut(e);
-			}.bind(this));
+				self.onMouseEnter(e);
+			}).bind('mouseleave', function (e) {
+				self.onMouseOut(e);
+			});
+
+			// disable scrolling the nav
+			$(document).on('scroll', function (e) {
+				e.preventDefault();
+				return self.scrollHandler(e);
+			});
+
+			this.$.find('[href^=sidebar]').bind('click', function (e) {
+				e.preventDefault();
+				var action = $(this).attr('href').split(':')[1];
+
+				switch (action) {
+					case 'toggle-lock':
+						self.toggleLock();
+						break;
+				}
+			});
 		}
 	}, {
 		key: 'onMouseEnter',
 		value: function onMouseEnter(e) {
+			this.open = true;
 			this.$.addClass('open');
 		}
 	}, {
 		key: 'onMouseOut',
 		value: function onMouseOut(e) {
+			this.open = false;
 			this.$.removeClass('open');
+		}
+	}, {
+		key: 'scrollHandler',
+		value: function scrollHandler(e) {
+			var top = $(window).scrollTop();
+			this.$.find('.cuckoo-nav').css('transform', 'translate3d(0,' + top + 'px,0)');
+		}
+	}, {
+		key: 'lock',
+		value: function lock() {
+			this.locked = true;
+			this.$.addClass('locked');
+			window.localStorage.setItem('cuckoo-sidebar-locked', true);
+		}
+	}, {
+		key: 'unlock',
+		value: function unlock() {
+			this.locked = false;
+			this.$.removeClass('locked');
+			window.localStorage.setItem('cuckoo-sidebar-locked', false);
+		}
+	}, {
+		key: 'toggleLock',
+		value: function toggleLock() {
+			if (this.locked) {
+				this.unlock();
+			} else {
+				this.lock();
+			}
 		}
 	}]);
 
@@ -41,7 +102,7 @@ var AnalysisSidebar = function () {
 
 $(function () {
 
-	var sidebar = new AnalysisSidebar($('#analysis-nav'));
-	console.log(sidebar);
+	var sidebar;
+	if ($("#analysis-nav").length) sidebar = new AnalysisSidebar($('#analysis-nav'));
 });
 //# sourceMappingURL=analysis_sidebar.js.map
