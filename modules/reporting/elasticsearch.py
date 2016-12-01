552c3e943a756cdf35e2910d44aa560fa33bb00a
Allen Swackhamer <allen.swackhamer@gmail.com>
2016-11-30 20:25:22 -0600

    removing explicit operation type from the bulk helper as it defaults to index

--- modules/reporting/elasticsearch.py
+++ modules/reporting/elasticsearch.py
@@ -194,7 +194,6 @@ class ElasticSearch(Report):
                 call_document.update(call)
                 call_document.update(base_document)
                 bulk_index.append({
-                    "_op_type": "index",
                     "_index": self.dated_index,
                     "_type": self.call_type,
                     "_source": call_document
