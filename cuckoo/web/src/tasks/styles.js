eafea638a74bd49cec1bcbee20ab438bf1844e7f
Koen Houtman <automagisch@cuckoo.sh>
2016-12-05 17:08:37 +0100

    Implemented and compiled new SCSS files for the submission layout. - created files - copied local contents to repo contents - imported in main.scss - since I use @extend with font-awesome, I initialized bower alongside node. -> http://www.bower.io - A few notes on bower: Bower is NOT needed for production use. so nothing changes there, bower is a simple package manager for front-end assets (like font-awesome). In a nearby future, I hope migrating all currently used third-party assets from static (what we do now) to bower. But for now this is OK.

--- cuckoo/web/src/tasks/styles.js
+++ cuckoo/web/src/tasks/styles.js
@@ -25,7 +25,8 @@ module.exports = function() {
 
 	return sass('./scss/main.scss', { 
 			sourcemap: true, 
-			style: 'expanded' 
+			style: 'expanded',
+			loadPath: ['./bower_components/font-awesome/scss']
 		})
 		// enables verbose logging of SASS errors
 		.on('error', sass.logError)
