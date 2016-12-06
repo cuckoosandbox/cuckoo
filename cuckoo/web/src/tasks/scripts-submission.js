3bf81764297b00bdcc80f3bae4dded44718f934c
Koen Houtman <automagisch@cuckoo.sh>
2016-12-06 15:58:47 +0100

    fixed a bug where submission.js was compiled to the root src folder

--- cuckoo/web/src/tasks/scripts-submission.js
+++ cuckoo/web/src/tasks/scripts-submission.js
@@ -1,5 +1,6 @@
 var fs = require('fs');
 var gulp = require('gulp');
+var sourcemaps = require('gulp-sourcemaps');
 var browserify = require('browserify');
 var babelify = require('babelify');
 var source = require('vinyl-source-stream');
@@ -21,6 +22,8 @@ module.exports = function() {
 		.on('error', function(err) { console.log(err); })
 		.pipe(source('submission.js'))
 		.pipe(buffer())
+		.pipe(sourcemaps.init({loadMaps: true}))
+		.pipe(sourcemaps.write('./'))
 		.pipe(gulp.dest('../static/js/cuckoo'));
 
 }
