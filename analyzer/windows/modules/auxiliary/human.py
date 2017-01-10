d7fb588ce507447be33a1bd13ce2f226a377cd43
kevross33 <kevross33@gmail.com>
2017-01-10 14:13:45 +0000

    Update human.py with more buttons to click
    From spender-sandbox human.py https://github.com/spender-sandbox/cuckoo-modified/blob/master/analyzer/windows/modules/auxiliary/human.py

--- analyzer/windows/modules/auxiliary/human.py
+++ analyzer/windows/modules/auxiliary/human.py
@@ -36,14 +36,21 @@ def foreach_child(hwnd, lparam):
         "agree", "j'accepte",
         "enable", "activer",
         "don't send", "ne pas envoyer",
+        "don't save",
         "continue", "continuer",
         "unzip", "dezip",
         "open", "ouvrir",
+        "close the program",
         "execute", "executer",
         "launch", "lancer",
+        "later",
+        "finish",
+        "end",
+        "allow access",
+        "remind me later",
         "save", "sauvegarder"
     ]
-
+    
     # List of buttons labels to not click.
     dontclick = [
         "don't run",
