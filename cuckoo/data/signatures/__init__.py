7021aff60ecc81c73c6a96906a29e3edb055e8a3
Jurriaan Bremer <jbr@cuckoo.sh>
2017-01-17 15:30:39 +0100

    forgot to commit crucial signature import file

---
+++ cuckoo/data/signatures/__init__.py
@@ -0,0 +1,6 @@
+# Copyright (C) 2010-2013 Claudio Guarnieri.
+# Copyright (C) 2014-2017 Cuckoo Foundation.
+# This file is part of Cuckoo Sandbox - http://www.cuckoosandbox.org
+# See the file 'docs/LICENSE' for copying permission.
+
+from . import android, cross, darwin, network, windows
