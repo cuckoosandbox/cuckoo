d537937b4667b9ddd58b2019c7adaa45fe9bcda0
Jurriaan Bremer <jbr@cuckoo.sh>
2017-02-07 02:22:21 +0100

    always bundle flask-sqlalchemy
    Conflicts:
    	setup.py

--- cuckoo/apps/distributed.py
+++ cuckoo/apps/distributed.py
@@ -1,40 +1,22 @@
-# Copyright (C) 2016 Cuckoo Foundation.
+# Copyright (C) 2016-2017 Cuckoo Foundation.
 # This file is part of Cuckoo Sandbox - http://www.cuckoosandbox.org
 # See the file 'docs/LICENSE' for copying permission.
 
 import os
-import sys
 
+from cuckoo.distributed.app import create_app
+from cuckoo.distributed.instance import (
+    scheduler, status_caching, handle_node
+)
 from cuckoo.misc import decide_cwd
-
-try:
-    from cuckoo.distributed.app import create_app
-    from cuckoo.distributed.instance import (
-        scheduler, status_caching, handle_node
-    )
-    HAVE_FLASKSQLA = True
-except ImportError:
-    HAVE_FLASKSQLA = False
 
 app = None
 
 def cuckoo_distributed(hostname, port, debug):
-    if not HAVE_FLASKSQLA:
-        sys.exit(
-            "Please install flask-sqlalchemy (through "
-            "`pip install cuckoo[distributed]`)"
-        )
-
     app = create_app()
     app.run(host=hostname, port=port, debug=debug)
 
 def cuckoo_distributed_instance(name):
-    if not HAVE_FLASKSQLA:
-        sys.exit(
-            "Please install flask-sqlalchemy (through "
-            "`pip install cuckoo[distributed]`)"
-        )
-
     app = create_app()
 
     with app.app_context():
@@ -47,11 +29,4 @@ def cuckoo_distributed_instance(name):
 
 if os.environ.get("CUCKOO_APP") == "dist":
     decide_cwd(exists=True)
-
-    if not HAVE_FLASKSQLA:
-        sys.exit(
-            "Please install flask-sqlalchemy (through "
-            "`pip install cuckoo[distributed]`)"
-        )
-
     app = create_app()
