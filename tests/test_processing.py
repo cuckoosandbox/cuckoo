d0e3f58d8e81ddceacab6850f18b5ad5e25788d8
RicoVZ <ricardo@cuckoo.sh>
2017-03-04 05:20:51 +0100

    Added more tests for processing/network/Pcap

--- tests/test_processing.py
+++ tests/test_processing.py
@@ -193,9 +193,179 @@ class TestProcessing:
 
 class TestProcessingNetwork(object):
 
-    def test_create_dns_server_list(self):
-        res = Pcap("tests/files/pcap/used_dns_server.pcap", {}).run()
+    data = None
+
+    def get_mixed_network(self):
+        print("Call")
+        if TestProcessingNetwork.data is None:
+            path = "tests/files/pcap/mixed-traffic.pcap"
+            print("Aw yiss")
+            TestProcessingNetwork.data = Pcap(path, {}).run()
+        return TestProcessingNetwork.data
+
+    def test_dns_server_list(self):
+        res = self.get_mixed_network()
         assert res["dns_servers"] == ["8.8.8.8"]
+
+    def test_network_tls(self):
+        res = self.get_mixed_network()
+
+        expected = {
+            "5125e361db3498ad5582861bd3e7d2833720dc2667e84898a1a9809ca5d8b026": "74df546aa33ce1cd4d4c70560c46517f4d82ff3f453bc5fada7571a11b0b40e7",
+            "e06e26040000541f575837fe59fcffc048d2da914bb592947fb00d93fe3096c1": "3fc88fa50ae338e96dbadcce9d1a4a39d3ab4082bdb8486d3798b43f8de35828",
+            "509c7d7cb1fbf738275005e5b8da6870ed4364334b8cc3113076094353418ef3": "a21bc2564a1317330c2896277c1a86679ace3c19ac4bb995db98543c2699b484",
+            "0c2bb706845f1ffbdd2b35727671269bc1888eeb76dbfb1380bf254f66b1f304": "27f1291c893f6bdb50791667e14d54f13b49f758cdd51bfb3eef4e025c1455fa",
+            "0823eb76d5fe02028d95a195e7242d4e0af2ca7d2c1e00060a3147ca51bb2fa9": "7eaecad65d025e141acf0bf63e85fc996751f2fea0a11792619d27d7116fa930",
+            "042cfc85c16f9d6a357667f3fbb2424fefafce8b6e012e986ecd8f3026b8178c": "e5c7af9b9996f81c306ac0c8056d62baa20a56b0c4b44085c1b69fc49afcbab7",
+            "43afb7d851b8fd110ffe10981e126c9fc698aae468ef9f824c13b5aeff0a78f7": "be044830cd9c5997982466d85e1b6653cbd7110fed56be1f326346854a216357",
+            "58b9e2761e4312136d929fb08dcf83ab89547bda5f4f1a81dc78be4a511b928b": "dccc6f639bbc3f5f78df6ac3973d9f4a29d2cf4128d941182de53a546558f122",
+            "58b9e2769b0988e851772e4e80a791bca639c6300e3a88765213f1c523519743": "dbba5aafa1ef148ee83a1cf8953d26a52b32e148cdd42bc24227334fa4f43fdb",
+            "58b9e27681f5dcd7ab61f24cc722f8f175696e8b7558bbd519d8cde6bda9bbf1": "e73e8952daf2350fd5110cd71059eea5cb7680abbc84adb2ceeb14c81875ed4f",
+            "58b9e2768f36a05b83119cce56549d8c895f58ac0d354b99fab5ba0afccfef48": "6f65a4033e33c15ae0e0380eef1647027dea3bd522c8e51f7fa81d7b7606097b",
+            "58b9e276c62992aec3e2619a051f39839bf775d02937d8775e5f0585a81aff83": "86ea041b0cc229d9de9a9820a0b22fdf8f7f9c03753e974b421d67908ccc8903",
+            "58b9e273319636975d4e105dab8cf3ab9f84b74620984ee99001b7da4c675ec6": "72739bf22f67f7ffab5df4050c82d43ae7b206c1d6a097de9f44b25601b28a0c",
+            "58b9e273eac7840222082482b162fb812bbb53d54dfbbe6f610c3fc696863eff": "112f4ceae25c9596d88aa032fc64192711407332c7e89ae797763cf93e08bd27",
+            "58b9e2735302abf0b50fea45a00c17c136413321c66614f58cd33ecaf697ed0c": "24de45a373da296fb1a80c0111d339bc4ee3859e496e971344bce56adba3feba"
+        }
+
+        found = {}
+        for tls in res["tls"]:
+            found[tls["server_random"]] = tls["session_id"]
+
+        assert expected == found
+
+    def test_network_udp(self):
+        res = self.get_mixed_network()
+
+        expected_src = ["192.168.56.110"]
+        expected_dst = [
+            "224.0.0.252", "8.8.8.8", "192.168.56.255",
+            "239.255.255.250", "52.178.223.23"
+        ]
+
+        sources = []
+        destinations = []
+
+        for data in res["udp"]:
+            if data["src"] not in sources:
+                sources.append(data["src"])
+            if data["dst"] not in destinations:
+                destinations.append(data["dst"])
+
+        assert len(res["udp"]) == 69
+        assert sources == expected_src
+        assert destinations == expected_dst
+
+    def test_network_tcp(self):
+        res = self.get_mixed_network()
+
+        expected_src = ["192.168.56.110"]
+        expected_dst = [
+            "149.210.181.54",
+            "178.255.83.1",
+            "104.16.93.188",
+            "216.58.212.202",
+            "23.51.117.163",
+            "23.51.123.27",
+            "216.58.212.206",
+            "216.58.212.195",
+            "93.184.221.200",
+            "204.79.197.200",
+            "131.253.61.64",
+            "93.184.220.20",
+            "52.58.57.251",
+            "40.86.224.10",
+            "54.174.33.196",
+            "138.91.83.37"
+        ]
+
+        sources = []
+        destinations = []
+
+        for data in res["tcp"]:
+            if data["src"] not in sources:
+                sources.append(data["src"])
+            if data["dst"] not in destinations:
+                destinations.append(data["dst"])
+
+        assert len(res["tcp"]) == 51
+        assert sources == expected_src
+        assert destinations == expected_dst
+
+    def test_network_icmp(self):
+        res = self.get_mixed_network()
+
+        expected_types = {0:4, 8:4}
+        expected_src = ["192.168.56.110", "149.210.181.54"]
+        expected_dst = ["149.210.181.54", "192.168.56.110"]
+        expected_mes = ["abcdefghijklmnopqrstuvwabcdefghi"]*8
+
+        sources = []
+        destinations = []
+        messages = []
+        types = {}
+
+        for data in res["icmp"]:
+            if data["src"] not in sources:
+                sources.append(data["src"])
+            if data["dst"] not in destinations:
+                destinations.append(data["dst"])
+            if data["type"] in types:
+                types[data["type"]] += 1
+            else:
+                types[data["type"]] = 1
+            messages.append(data["data"])
+
+        assert len(res["icmp"]) == 8
+        assert expected_types == types
+        assert expected_src == sources
+        assert expected_dst == destinations
+        assert expected_mes == messages
+
+    def test_network_hosts(self):
+        res = self.get_mixed_network()
+        expected_hosts = [
+            "8.8.8.8", "149.210.181.54", "178.255.83.1",
+            "104.16.93.188", "216.58.212.202", "23.51.117.163",
+            "23.51.123.27", "216.58.212.206", "216.58.212.195",
+            "93.184.221.200", "52.178.223.23", "204.79.197.200",
+            "131.253.61.64", "93.184.220.20", "52.58.57.251",
+            "40.86.224.10", "54.174.33.196", "138.91.83.37",
+            "55.119.32.91", "5.5.119.1"
+        ]
+        assert res["hosts"] == expected_hosts
+
+    def test_network_dead_hosts(self):
+        res = self.get_mixed_network()
+        expected_dead = [
+            ("55.119.32.91", 1234),
+            ("5.5.119.1", 9836)
+        ]
+
+        assert res["dead_hosts"] == expected_dead
+
+    def test_network_domains(self):
+        res = self.get_mixed_network()
+        assert len(res["domains"]) == 23
+
+    def test_network_dns(self):
+        res = self.get_mixed_network()
+
+        expected_types = {
+            'A': 23, 'SOA': 1, 'TXT': 1,
+            'PTR': 3, 'AAAA': 2, 'CNAME': 1,
+            'SRV': 1, 'HINFO': 1, 'NS': 1, 'MX': 1
+        }
+
+        types = {}
+        for res in res["dns"]:
+            t = res["type"]
+            if t in types:
+                types[t] += 1
+            else:
+                types[t] = 1
+
+        assert expected_types == types
 
 class TestPcap2(object):
 
