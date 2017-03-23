9b079cece16e612d8edd4a04773dcbcde69305a5
doomedraven <doommedraven@gmail.com>
2017-03-23 02:18:44 +0100

    Fix rooter, add memory profile per vm, and linux analyzer related fixes

--- cuckoo/core/guest.py
+++ cuckoo/core/guest.py
@@ -337,7 +337,10 @@ class GuestManager(object):
     def determine_analyzer_path(self):
         """Determine the path of the analyzer. Basically creating a temporary
         directory in the systemdrive, i.e., C:\\."""
-        systemdrive = "%s\\" % self.environ["SYSTEMDRIVE"]
+        if self.platform == "windows":
+            systemdrive = "%s\\" % self.environ["SYSTEMDRIVE"]
+        elif self.platform in ("linux", "darwin"):
+            systemdrive = self.environ["HOME"]
 
         options = parse_options(self.options["options"])
         if options.get("analpath"):
@@ -460,9 +463,15 @@ class GuestManager(object):
 
         # If the target is a file, upload it to the guest.
         if options["category"] == "file" or options["category"] == "archive":
+
+            if self.platform in ("linux", "darwin"):
+                temp = "/tmp"
+            else:
+                temp = self.environ["TEMP"]
+
             data = {
                 "filepath": os.path.join(
-                    self.environ["TEMP"], options["file_name"]
+                    temp, options["file_name"]
                 ),
             }
             files = {
