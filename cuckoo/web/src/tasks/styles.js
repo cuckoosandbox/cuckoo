114f5f3fc69bb4cd6501481f2cb3054db6b8522b
Koen Houtman <automagisch@cuckoo.sh>
2017-04-12 14:46:31 +0200

    Ran autoprefixer on custom css
    - this will fix a whole lot of cross-browser related issues
    - implemented in automated scss compilation

--- cuckoo/web/src/tasks/styles.js
+++ cuckoo/web/src/tasks/styles.js
@@ -10,13 +10,14 @@
 		gulp-ruby-sass
 		gulp-sourcemaps
  */
-var gulp 		= require('gulp');
-var gutil 		= require('gulp-util');
-var sass 		= require('gulp-ruby-sass');
-var sourcemaps 	= require('gulp-sourcemaps');
-var insert 		= require('gulp-insert');
-var clean 		= require('gulp-clean-css');
-var notify 		= require('gulp-notify');
+var gulp 		 = require('gulp');
+var gutil 		 = require('gulp-util');
+var sass 		 = require('gulp-ruby-sass');
+var sourcemaps 	 = require('gulp-sourcemaps');
+var insert 		 = require('gulp-insert');
+var clean 		 = require('gulp-clean-css');
+var notify 		 = require('gulp-notify');
+var autoprefixer = require('gulp-autoprefixer');
 
 /*
 	return Gulp function()
@@ -41,6 +42,7 @@ module.exports = function() {
 		// 	return "/* \n\n\t Copyright (C) "+ new Date().getFullYear() +" Cuckoo Foundation.\n\t This file is part of Cuckoo Sandbox - http://www.cuckoosandbox.org \n\t see the file 'docs/LICENSE' for copying permission \n\n */ \n\n " + contents;
 		// }))
 		// writes the sourcemap
+		.pipe(autoprefixer())
 		.pipe(gutil.env.production ? gutil.noop() : sourcemaps.write('./'))
 		.pipe(gutil.env.production ? clean() : gutil.noop())
 		.pipe(notify('SCSS compiled to CSS!'))
