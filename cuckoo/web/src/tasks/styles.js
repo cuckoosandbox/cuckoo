2b13fb6a73424fc34e79898ea678c1c4e68b742d
Koen Houtman <automagisch@cuckoo.sh>
2017-06-27 11:19:45 +0200

    Use libsass for compiling css
    - snoops off compilation time with approx. 6 seconds, awesome.

--- cuckoo/web/src/tasks/styles.js
+++ cuckoo/web/src/tasks/styles.js
@@ -1,53 +1,27 @@
-/*
-   Copyright (C) 2016 Cuckoo Foundation.
-   This file is part of Cuckoo Sandbox - http://www.cuckoosandbox.org
-   see the file 'docs/LICENSE' for copying permission.
- */
-
-/*
-	require node dependencies:
-		gulp
-		gulp-ruby-sass
-		gulp-sourcemaps
- */
-var gulp 		 = require('gulp');
-var gutil 		 = require('gulp-util');
-var sass 		 = require('gulp-ruby-sass');
-var sourcemaps 	 = require('gulp-sourcemaps');
-var insert 		 = require('gulp-insert');
-var clean 		 = require('gulp-clean-css');
-var notify 		 = require('gulp-notify');
+var gulp = require('gulp');
+var gutil = require('gulp-util');
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
 var autoprefixer = require('gulp-autoprefixer');
+var clean = require('gulp-clean-css');
+var notify = require('gulp-notify');
 
-/*
-	return Gulp function()
-	- cursor to main.scss
-	- catch errors and display
-	- write sourcemaps for easy css debugging in the browser
- */
 module.exports = function() {
 
-	return sass('./scss/main.scss', {
-			sourcemap: true,
-			style: 'compressed',
-			loadPath: [
+  return gulp.src('./scss/main.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass({
+      sourcemap: true,
+			outputStyle: 'expanded',
+			includePaths: [
 				'./bower_components/font-awesome/scss',
 				'./bower_components/font-roboto/src/styles'
 			]
-		})
-		// enables verbose logging of SASS errors
-		.on('error', sass.logError)
-		// adds a copyright notice to the top of the compiled document
-		// .pipe(insert.transform(function(contents) {
-		// 	return "/* \n\n\t Copyright (C) "+ new Date().getFullYear() +" Cuckoo Foundation.\n\t This file is part of Cuckoo Sandbox - http://www.cuckoosandbox.org \n\t see the file 'docs/LICENSE' for copying permission \n\n */ \n\n " + contents;
-		// }))
-		// writes the sourcemap
-		.pipe(autoprefixer())
-		.pipe(gutil.env.production ? gutil.noop() : sourcemaps.write('./'))
-		.pipe(gutil.env.production ? clean() : gutil.noop())
-		.pipe(notify('SCSS compiled to CSS!'))
-
-		// output file to static web dir
-		.pipe(gulp.dest('../static/css'));
+    }).on('error', sass.logError))
+    .pipe(autoprefixer())
+    .pipe(gutil.env.production ? gutil.noop() : sourcemaps.write('./'))
+    .pipe(gutil.env.production ? clean() : gutil.noop())
+    .pipe(notify('SCSS compiled to CSS!'))
+    .pipe(gulp.dest('../static/css'));
 
 }
