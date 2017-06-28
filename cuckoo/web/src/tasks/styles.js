a060f88546af00e1b08797c5dca34f00b28e084b
Koen Houtman <automagisch@cuckoo.sh>
2017-06-28 11:49:19 +0200

    fixup main.css output

--- cuckoo/web/src/tasks/styles.js
+++ cuckoo/web/src/tasks/styles.js
@@ -1,10 +1,10 @@
-var gulp = require('gulp');
-var gutil = require('gulp-util');
-var sass = require('gulp-sass');
-var sourcemaps = require('gulp-sourcemaps');
-var autoprefixer = require('gulp-autoprefixer');
-var clean = require('gulp-clean-css');
-var notify = require('gulp-notify');
+var gulp          = require('gulp');
+var gutil         = require('gulp-util');
+var sass          = require('gulp-sass');
+var sourcemaps    = require('gulp-sourcemaps');
+var autoprefixer  = require('gulp-autoprefixer');
+var notify        = require('gulp-notify');
+var comb          = require('gulp-csscomb');
 
 module.exports = function() {
 
@@ -19,8 +19,32 @@ module.exports = function() {
 			]
     }).on('error', sass.logError))
     .pipe(autoprefixer())
+    .pipe(comb({
+      "remove-empty-rulesets": true,
+      "always-semicolon": true,
+      "color-case": "lower",
+      "block-indent": "",
+      "color-shorthand": false,
+      "element-case": "lower",
+      "eof-newline": true,
+      "leading-zero": true,
+      "quotes": "double",
+      "sort-order-fallback": "abc",
+      "space-before-colon": "",
+      "space-after-colon": " ",
+      "space-before-combinator": " ",
+      "space-after-combinator": " ",
+      "space-between-declarations": "\n",
+      "space-before-opening-brace": " ",
+      "space-after-opening-brace": "\n",
+      "space-after-selector-delimiter": "\n",
+      "space-before-selector-delimiter": "",
+      "space-before-closing-brace": "\n",
+      "strip-spaces": true,
+      "tab-size": false,
+      "vendor-prefix-align": true
+    }))
     .pipe(gutil.env.production ? gutil.noop() : sourcemaps.write('./'))
-    .pipe(gutil.env.production ? clean() : gutil.noop())
     .pipe(notify('SCSS compiled to CSS!'))
     .pipe(gulp.dest('../static/css'));
 
