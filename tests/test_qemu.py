c5bd061e08837ccaf4a633d5779657f2901f71d7
Daan Spitz <daan@cuckoo.sh>
2017-07-05 10:39:11 +0200

    add tests for new snapshot + enable_kvm qemu config directives

--- tests/test_qemu.py
+++ tests/test_qemu.py
@@ -46,3 +46,46 @@ class TestQemu(object):
         e.match("no attribute 'qemu_img'")
         os_path_exists.assert_called_with("/home/cuckoo/images/snapshot_ubuntu32.qcow2")
         os_remove.assert_called_with("/home/cuckoo/images/snapshot_ubuntu32.qcow2")
+
+    @mock.patch("cuckoo.machinery.qemu.subprocess.Popen")
+    def test_snapshot_config(self, subprocess_popen):
+        class named_machine(object):
+            name = "ubuntu32"
+
+        self.m.db.view_machine_by_label.return_value = named_machine()
+        self.m.set_options(Dictionary({
+            "ubuntu32": Dictionary({
+                "image": "/home/cuckoo/images/ubuntu32.qcow2",
+                "arch": "x86",
+                "snapshot": "booted"
+            })
+        }))
+
+        self.m.start("label", None)
+        assert subprocess_popen.call_count == 1
+        assert all(x in subprocess_popen.call_args[0][0] for x in ("-loadvm", "booted"))
+
+    def test_kvm_config(self):
+        class named_machine(object):
+            name = "ubuntu32"
+
+        self.m.db.view_machine_by_label.return_value = named_machine()
+        self.m.set_options(Dictionary({
+            "ubuntu32": Dictionary({
+                "image": "/home/cuckoo/images/ubuntu32.qcow2",
+                "arch": "x86",
+                "enable_kvm": True
+            })
+        }))
+
+        self.m.qemu_img = mock.MagicMock()
+
+        with mock.patch("cuckoo.machinery.qemu.subprocess.Popen") as subprocess_popen:
+            proc = mock.MagicMock()
+            proc.communicate.return_value = "", ""
+            subprocess_popen.side_effect = proc, mock.MagicMock()
+
+            self.m.start("label", None)
+
+        assert subprocess_popen.call_count == 2
+        assert "-enable-kvm" in subprocess_popen.call_args[0][0]
