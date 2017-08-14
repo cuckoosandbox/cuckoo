95ec800d59e48def4248297f5f722077b00dd9a4
Xiangyang Zhang <xyz.sun.ok@gmail.com>
2017-08-14 21:43:50 +0800

    enlarge shellcode size limit, and wait while shellcode is running

--- stuff/execsc/execsc.c
+++ stuff/execsc/execsc.c
@@ -4,19 +4,22 @@
 
 int main (int argc, char ** argv) {
 	int fd;
-	char buf[2048] = {0};
+	void *buf = malloc(1024*1024);
 
 	if (argc < 2) return 1;
 
 	// read in shellcode from analysis target file
 	fd = open(argv[1], 0);
-	read(fd, buf, 2048);
+	read(fd, buf, 1024*1024);
 	close(fd);
 
 	// jump into shellcode
 	int (*func)();
 	func = (int (*)()) buf;
 	(int)(*func)();
+
+	getchar();
+	free(buf);
 
 	return 0;
 }
