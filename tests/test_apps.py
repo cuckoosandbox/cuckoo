cc1a8eff1cfa0265f5e96752ceee4c78e8813833
RicoVZ <ricardo@cuckoo.sh>
2017-08-31 02:21:17 +0200

    Update apps test for add machine and del machine A new optional argument 'rdp_port' can now be passed to 'cuckoo_machine'

--- tests/test_apps.py
+++ tests/test_apps.py
@@ -165,7 +165,7 @@ class TestAppsWithCWD(object):
     def test_machine_add(self):
         cuckoo_machine(
             "cuckoo2", "add", "1.2.3.4", "windows",
-            None, None, None, None, None
+            None, None, None, None, None, None
         )
         assert config("virtualbox:virtualbox:machines") == [
             "cuckoo1", "cuckoo2",
@@ -175,7 +175,7 @@ class TestAppsWithCWD(object):
 
     def test_machine_delete(self):
         cuckoo_machine(
-            "cuckoo1", "delete", None, None, None, None, None, None, None
+            "cuckoo1", "delete", None, None, None, None, None, None, None, None
         )
         assert config("virtualbox:virtualbox:machines") == []
 
