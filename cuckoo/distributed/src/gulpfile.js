6be307ff0235447288957fbb761472f1cb632a21
Koen Houtman <automagisch@cuckoo.sh>
2017-09-26 17:23:05 +0200

    Moved back to node v4.
    it gave some super messed up errors (usually never see them though...) but this took way too long.

--- cuckoo/distributed/src/gulpfile.js
+++ cuckoo/distributed/src/gulpfile.js
@@ -1,16 +1,16 @@
-const path       = require('path');
-const chalk      = require('chalk');
-const gulp       = require('gulp');
-const sass       = require('gulp-sass');
-const browserify = require('browserify');
-const babelify   = require('babelify');
-const source     = require('vinyl-source-stream');
-const buffer     = require('vinyl-buffer');
+var path       = require('path');
+var chalk      = require('chalk');
+var gulp       = require('gulp');
+var sass       = require('gulp-sass');
+var browserify = require('browserify');
+var babelify   = require('babelify');
+var source     = require('vinyl-source-stream');
+var buffer     = require('vinyl-buffer');
 
 /*
   Sass rendering task
  */
-gulp.task('sass', () => {
+gulp.task('sass', function() {
   return gulp.src('./scss/main.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('../static/css'));
@@ -19,38 +19,43 @@ gulp.task('sass', () => {
 /*
   browserify/babelify task
  */
-gulp.task('babel', () => {
+gulp.task('babel', function() {
 
   return browserify({
-    entries: ['./scripts/main.babel'],
-    extensions: ['.babel'],
-    debug: true
-  }).transform(babelify, {
-    presets: ["env"],
-    sourceRoot: './scripts/'
-  }).bundle().on('error', err => {
+			entries: ['./scripts/main.babel'],
+			extensions: ['.babel'],
+			debug: true
+		})
+		.transform("babelify", {
+			extensions: ['.babel'],
+			presets: ["es2015"],
+			sourceRoot: './scripts/'
+		})
+		.bundle().on('error', function(err) {
 
-    if(err.loc) {
-      // assume typo error
-      console.log(chalk.red('\nOopsie-daysee! You made an unforgivable typo:\n'));
-      console.log(`${err.codeFrame}\n`);
-      console.log(`>>> ${path.basename(err.fileName)} (line ${err.loc.line}, col ${err.loc.column} ]\n`);
-    } else {
-      // assume other error
-      console.log(chalk.red(`${err}`));
-    }
+			if(err.loc) {
+				// assume typo error
+				console.log(chalk.red('\nOopsie-daysee! You made an unforgivable typo:\n'));
+				console.log(`${err.codeFrame}\n`);
+				console.log(`>>> ${path.basename(err.fileName)} (line ${err.loc.line}, col ${err.loc.column} ]\n`);
+			} else {
+				// assume other error
+				console.log(chalk.red(`${err}`));
+			}
 
-    this.emit('end');
-  })
-  .pipe(source('main.js'))
-  .pipe(gulp.dest('../static/js'));
+			this.emit('end');
+
+		})
+		.pipe(source('main.js'))
+		.pipe(buffer())
+		.pipe(gulp.dest('../static/js'));
 
 });
 
 /*
   Gulp system watchers
  */
-gulp.task('watch', () => {
+gulp.task('watch', function() {
   gulp.watch('scripts/**/*.babel', ['babel']);
   gulp.watch('scss/**/*.scss', ['sass']);
 });
