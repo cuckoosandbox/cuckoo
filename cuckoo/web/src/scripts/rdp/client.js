a0c6084bcec6395e1ba3b6b704899a84bf766f57
Koen Houtman <automagisch@cuckoo.sh>
2017-11-01 17:01:07 +0200

    wire up the interface to javascript
    - implements some sub classes for the toolbar
    - uses my hookable class for easy custom event dispatching

--- cuckoo/web/src/scripts/rdp/client.js
+++ cuckoo/web/src/scripts/rdp/client.js
@@ -1,1 +1,20 @@
-console.log('rdp-client');
+import Hookable from './Hookable';
+import RDPToolbar from './RDPToolbar';
+
+// RDP Client wrapper for collecting all sub classes that belong to this interface
+// - can be treated like a controller. Any processes are catched up on here.
+class RDPClient extends Hookable {
+  constructor(el) {
+    super();
+    this.$ = el || null;
+    this.toolbar = new RDPToolbar(this);
+  }
+}
+
+// initialize the classes and construct the interface
+$(function() {
+  if($("#rdp-client").length) {
+    let rdpClient = new RDPClient($("#rdp-client"));
+    console.log(rdpClient);
+  }
+});
