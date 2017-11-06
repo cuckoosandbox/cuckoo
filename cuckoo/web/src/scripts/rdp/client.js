dffa007a4ae8dbdb39e752163cf910b846c92da5
Koen Houtman <automagisch@cuckoo.sh>
2017-11-06 16:16:20 +0100

    Created dialog controller
    - custom micro framework to describe 'simply' what a dialog is, what is shows and what happens when you click on a button in that dialog.

--- cuckoo/web/src/scripts/rdp/client.js
+++ cuckoo/web/src/scripts/rdp/client.js
@@ -1,6 +1,7 @@
 import Hookable from './Hookable';
 import RDPToolbar from './RDPToolbar';
 import RDPSnapshotService from './RDPSnapshotService';
+import RDPDialog from './RDPDialog';
 
 // RDP Client wrapper for collecting all sub classes that belong to this interface
 // - can be treated like a controller. Any processes are catched up on here.
@@ -10,6 +11,38 @@ class RDPClient extends Hookable {
     this.$ = el || null;
     this.snapshots = new RDPSnapshotService(this);
     this.toolbar = new RDPToolbar(this);
+
+    this.dialog = new RDPDialog(this, {
+      el: el.find('#rdp-dialog'),
+      dialogs: {
+        reboot: {
+          template: $('template#rdp-dialog-reboot'),
+          interactions: {
+            cancel: dialog => {
+              console.log('Will not reboot.');
+              dialog.close();
+            },
+            proceed: dialog => {
+              console.log('Will reboot.');
+              dialog.close();
+            }
+          }
+        },
+        close: {
+          template: $('template#rdp-dialog-close'),
+          interactions: {
+            cancel: dialog => {
+              console.log('Will not close');
+              dialog.close();
+            },
+            proceed: dialog => {
+              console.log('Will close');
+              dialog.close();
+            }
+          }
+        }
+      }
+    });
 
     // bind snapshot interactions
     this.snapshots.on('create', snapshot => {
@@ -27,6 +60,5 @@ class RDPClient extends Hookable {
 $(function() {
   if($("#rdp-client").length) {
     let rdpClient = new RDPClient($("#rdp-client"));
-    console.log(rdpClient);
   }
 });
