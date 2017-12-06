582f1babf6899e7b05e6f04ef82657f83f4dfe4e
Koen Houtman <automagisch@cuckoo.sh>
2017-12-06 14:05:08 +0100

    Create keyboard and mouse listeners in the client

--- cuckoo/web/src/scripts/rdp/GuacWrap.js
+++ cuckoo/web/src/scripts/rdp/GuacWrap.js
@@ -23,9 +23,15 @@ class GuacamoleWrapper extends Hookable {
     this.display = props.display;
     this.parent = props.client; // 'parent' client wrapper
     this.client = null; // reserved for the Guacamole client (created on connect)
+    this._mouse = null;
+    this._keyboard = null;
 
   }
 
+  /*
+    GuacamoleWrapper.connect
+    - connects to the RDP server
+   */
   connect() {
 
     // create the client
@@ -43,6 +49,36 @@ class GuacamoleWrapper extends Hookable {
     guac.connect();
     this.dispatchHook('connect', guac);
 
+    this.mouse();
+    this.keyboard();
+
+  }
+
+  /*
+    GuacamoleWrapper.mouse
+    - handles mouse interaction
+   */
+  mouse() {
+    if(!this.client) return;
+
+    this._mouse = new Guacamole.Mouse(this.client.getDisplay().getElement());
+    let sendState = state => this.client.sendMouseState(state);
+
+    // apply sendState function
+    this._mouse.onmousedown =
+    this._mouse.onmouseup =
+    this._mouse.onmousemove = sendState;
+  }
+
+  /*
+    GuacamoleWrapper.keyboard
+    - handles keyboard interaction
+   */
+  keyboard() {
+    if(!this.client) return;
+    this._keyboard = new Guacamole.Keyboard(document);
+    this._keyboard.onkeydown = (keysym) => this.client.sendKeyEvent(1, keysym);
+    this._keyboard.onkeyup = (keysym) => this.client.sendKeyEvent(0, keysym);
   }
 
 }
