7636b996a8b0466f32a504bb94f5bb72a7d7f6ff
Koen Houtman <automagisch@cuckoo.sh>
2017-12-12 17:40:56 +0100

    progress

--- cuckoo/web/static/js/cuckoo/analysis_feedback.js
+++ cuckoo/web/static/js/cuckoo/analysis_feedback.js
@@ -12,50 +12,7 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
  *
  */
 
-// will deprecate
-var feedbackFormSubmitted = false;
-
-function send_button_toggle() {
-
-  var sel = $('#modal_feedback');
-
-  sel.find("input#submit").toggleClass('sending');
-
-  if (sel.find('input#submit').hasClass('sending')) {
-    sel.find('input#submit').attr('disabled', true);
-    $("#modal_feedback").find("#result").html('Sending feedback... a moment.');
-  } else {
-    sel.find('input#submit').attr('disabled', false);
-  }
-}
-
-// will deprecate
-function feedback_send(task_id, name, email, company, message, include_analysis, include_memdump, callback) {
-
-  var params = {
-    "task_id": task_id,
-    "email": email,
-    "message": message,
-    "name": name,
-    "company": company,
-    "include_memdump": include_memdump,
-    "include_analysis": include_analysis
-  };
-
-  CuckooWeb.api_post("/analysis/api/task/feedback_send/", params, function (data) {
-    callback(data);
-  }, function (err) {
-    if (err.responseJSON.hasOwnProperty("message")) {
-      var _message = err.responseJSON.message;
-      $("#modal_feedback").find("#result").html(_message);
-      send_button_toggle();
-      feedbackFormSubmitted = false;
-    }
-  });
-}
-
 // a simple wrapper around the feedback functionality
-
 var FeedbackForm = function () {
   function FeedbackForm(el) {
     var _this = this;
