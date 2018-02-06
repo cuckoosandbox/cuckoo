83544e79c64013d600f18356563aa7dfb95504a4
Daan Spitz <daan@cuckoo.sh>
2018-02-06 16:00:48 +0100

    remove redundant task status endpoint

--- cuckoo/web/analysis/urls.py
+++ cuckoo/web/analysis/urls.py
@@ -22,7 +22,6 @@ urlpatterns = [
     url(r"^(?P<task_id>\d+)/reboot/$", SubmissionRoutes.reboot, name="analysis/reboot"),
     url(r"^(?P<task_id>\d+)/control/$", AnalysisControlRoutes.player, name="analysis/control/player"),
     url(r"^(?P<task_id>\d+)/control/screenshots/$", ControlApi.store_screenshots, name="analysis/control/screenshots"),
-    url(r"^(?P<task_id>\d+)/control/status/$", ControlApi.task_status, name="analysis/control/status"),
     url(r"^(?P<task_id>\d+)/control/tunnel/.*", ControlApi.tunnel, name="analysis/control/tunnel"),
     url(r"^(?P<task_id>\d+)/compare/$", AnalysisCompareRoutes.left, name="analysis/compare/left"),
     url(r"^(?P<task_id>\d+)/compare/(?P<compare_with_task_id>\d+)/$", AnalysisCompareRoutes.both, name="analysis/compare/both"),
