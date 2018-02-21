cd43ccd92affb63b10f111cea4213b05c74c5b99
root <root@cuckootest.cert.local>
2018-02-21 15:24:36 +0100

    cleanup of community files from commit

--- signatures/__init__.py
+++
@@ -1,5 +0,0 @@
-# Copyright (C) 2017 Cuckoo Foundation.
-# This file is part of Cuckoo Sandbox - http://www.cuckoosandbox.org
-# See the file 'docs/LICENSE' for copying permission.
-
-from . import android, cross, darwin, extractor, linux, network, windows
