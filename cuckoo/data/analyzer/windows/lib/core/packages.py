2789165e2a8a951b27173ebc18266e64c9c7996e
Ricardo van Zutphen <ricardo@cuckoo.sh>
2018-05-27 01:02:52 +0200

    Add iqy and slk to auto package selector

--- cuckoo/data/analyzer/windows/lib/core/packages.py
+++ cuckoo/data/analyzer/windows/lib/core/packages.py
@@ -50,7 +50,7 @@ def choose_package(file_type, file_name, exports):
         return "doc"
     elif "Microsoft Office Excel" in file_type or \
             "Microsoft Excel" in file_type or \
-            file_name.endswith((".xls", ".xlsx", ".xlt", ".xlsm")):
+            file_name.endswith((".xls", ".xlsx", ".xlt", ".xlsm", ".iqy", ".slk")):
         return "xls"
     elif "Microsoft PowerPoint" in file_type or \
             file_name.endswith((".ppt", ".pptx", ".pps", ".ppsx", ".pptm", ".potm", ".potx", ".ppsm")):
