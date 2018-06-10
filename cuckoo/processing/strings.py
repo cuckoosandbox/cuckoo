40ccee710a0a39bdd97059da2693c91a0a9935e3
Andrew LeFevre <jalefevre@liberty.edu>
2018-06-10 15:30:28 -0400

    Started work on using floss for extracting strings

--- cuckoo/processing/strings.py
+++ cuckoo/processing/strings.py
@@ -5,22 +5,32 @@
 
 import os.path
 import re
+import floss
+import vivisect
 
 from cuckoo.common.abstracts import Processing
 from cuckoo.common.exceptions import CuckooProcessingError
 
 class Strings(Processing):
-    """Extract strings from analyzed file."""
+    """Extract encoded & static strings from analyzed file with floss."""
     MAX_FILESIZE = 16*1024*1024
     MAX_STRINGCNT = 2048
     MAX_STRINGLEN = 1024
+    MIN_STRINGLEN = 4
 
     def run(self):
-        """Run extract of printable strings.
-        @return: list of printable strings.
+        """Run floss on analyzed file.
+        @return: floss results dict.
         """
         self.key = "strings"
-        strings = []
+        recovered_strings = {}
+
+        STRING_TYPES = [
+            "acsii",
+            "utf16",
+            "decoded",
+            "stack"
+        ]
 
         if self.task["category"] == "file":
             if not os.path.exists(self.file_path):
@@ -33,13 +43,48 @@ class Strings(Processing):
             except (IOError, OSError) as e:
                 raise CuckooProcessingError("Error opening file %s" % e)
 
-            strings = re.findall("[\x1f-\x7e]{6,}", data)
-            for s in re.findall("(?:[\x1f-\x7e][\x00]){6,}", data):
-                strings.append(s.decode("utf-16le"))
+            # Extract static strings
+            acsii_strings = floss.strings.extract_ascii_strings(
+                data,
+                self.MIN_STRINGLEN
+            )
+            uft16_strings = floss.strings.extract_unicode_strings(
+                data,
+                self.MIN_STRINGLEN
+            )
+
+            # Prepare FLOSS for extracting hidden & encoded strings
+            vw = vivisect.VivWorkspace()
+            vw.loadFromFile(self.file_path)
+            vw.analyze()
+
+            selected_functions = floss.main.select_functions(vw, None)
+            decoding_functions_candidates = floss.identification_manager.identify_decoding_functions(
+                vw,
+                floss.main.get_all_plugins(),
+                selected_functions
+            )
+
+            # Decode & extract hidden & encoded strings
+            decoded_strings = floss.main.decode_strings(
+                vw,
+                decoding_functions_candidates,
+                self.MIN_STRINGLEN
+            )
+            stack_strings = floss.stackstrings.extract_stackstrings(
+                vw,
+                selected_functions,
+                self.MIN_STRINGLEN
+            )
 
         # Now limit the amount & length of the strings.
-        strings = strings[:self.MAX_STRINGCNT]
-        for idx, s in enumerate(strings):
-            strings[idx] = s[:self.MAX_STRINGLEN]
+        results = [acsii_strings, uft16_strings, decoded_strings, stack_strings]
+        for strings in results:
+            strings = strings[:self.MAX_STRINGCNT]
+            for idx, s in enumerate(strings):
+                strings[idx] = s[:self.MAX_STRINGLEN]
 
-        return strings
+        for i in len(STRING_TYPES):
+            recovered_strings[STRING_TYPES[i]] = results[i]
+
+        return recovered_strings
