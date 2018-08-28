50cf77387484fb0a56ac04d895caf7e5cb038b1f
Fernando Domínguez <fernando.dom.del@gmail.com>
2018-08-28 14:25:44 +0200

    Add missing .vbs extension if needed in vbs package

--- cuckoo/data/analyzer/windows/modules/packages/vbs.py
+++ cuckoo/data/analyzer/windows/modules/packages/vbs.py
@@ -3,7 +3,12 @@
 # This file is part of Cuckoo Sandbox - http://www.cuckoosandbox.org
 # See the file 'docs/LICENSE' for copying permission.
 
+import os
+import logging
+
 from lib.common.abstracts import Package
+
+log = logging.getLogger(__name__)
 
 class VBS(Package):
     """VBS analysis package."""
@@ -13,4 +18,9 @@ class VBS(Package):
 
     def start(self, path):
         wscript = self.get_path("WScript")
+        if not path.endswith(".vbs"):
+            os.rename(path, path + ".vbs")
+            path += ".vbs"
+            log.info("Submitted file is missing extension, added .vbs")
+
         return self.execute(wscript, args=[path], trigger="file:%s" % path)
