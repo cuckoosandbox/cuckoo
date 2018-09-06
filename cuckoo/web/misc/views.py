4b9b340b850fde2789f6bd4fb8142f0e178fe30a
Koen Houtman <automagisch@hatching.io>
2018-09-06 11:20:31 +0200

    Initial files for the secret screen

---
+++ cuckoo/web/misc/views.py
@@ -0,0 +1,10 @@
+# Copyright (C) 2014-2017 Cuckoo Foundation.
+# This file is part of Cuckoo Sandbox - http://www.cuckoosandbox.org
+# See the file 'docs/LICENSE' for copying permission.
+
+from django.views.decorators.http import require_safe
+from cuckoo.web.utils import render_template
+
+@require_safe
+def secret(request):
+    return render_template(request, "secret.html")
---
+++ cuckoo/web/static/js/cuckoo/secret.js
@@ -0,0 +1,2 @@
+"use strict";
+//# sourceMappingURL=secret.js.map
