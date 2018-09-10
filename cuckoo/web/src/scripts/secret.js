0a94e51211e0794de7782472a78c705a146acad2
Koen Houtman <automagisch@hatching.io>
2018-09-10 10:18:51 +0200

    Progress with secret page form
    + UI interactions embedded
    + form POST listener handle

--- cuckoo/web/src/scripts/secret.js
+++ cuckoo/web/src/scripts/secret.js
@@ -1,12 +1,47 @@
 window.addEventListener('DOMContentLoaded', e => {
 
-  const modal  = document.querySelector('.modal-cuckoo');
-  const form   =    modal.querySelector('form.modal-dialog');
-  const secret =     form.querySelector('input#cuckoo-secret');
+  const modal   = document.querySelector('.modal-cuckoo');
+  const form    = modal.querySelector('form.modal-dialog');
+  const secret  = form.querySelector('input#cuckoo-secret');
+  const smsg    = secret.parentNode.querySelector('label .input-message');
+  const action  = modal.querySelectorAll('a[href^="action:"]');
+  const more    = modal.querySelector('[data-toggleable-col]');
 
+  const actions = {
+    leave: () => window.close(),
+    'toggle-info': el => {
+      more.classList.toggle('hidden', !more.classList.contains('hidden'));
+      if(more.classList.contains('hidden')) {
+        el.classList.replace('variation-blue','variation-grey');
+      } else {
+        el.classList.replace('variation-grey','variation-blue');
+      }
+    }
+  };
+
+  // easy-fied action toggling
+  [].forEach.call(action, link => {
+    link.addEventListener('click', e => {
+      e.preventDefault();
+      let target = e.currentTarget.href.split(':')[1];
+      if(actions[target]) actions[target](e.currentTarget);
+    });
+  });
+
+  // appends a message to the input label
+  function handleInvalidInput(message = "") {
+    smsg.textContent = message;
+  }
+
+  // validates the user input / processes the input
   form.addEventListener('submit', e => {
     e.preventDefault();
-    console.log('lets verify that key.');
+    let value = secret.value;
+    if(!value.length) {
+      handleInvalidInput('You have to enter a key. This field is empty.')
+    } else {
+      handleInvalidInput('This is not a good key.')
+    }
   });
 
 });
--- cuckoo/web/static/js/cuckoo/secret.js
+++ cuckoo/web/static/js/cuckoo/secret.js
@@ -5,10 +5,49 @@ window.addEventListener('DOMContentLoaded', function (e) {
   var modal = document.querySelector('.modal-cuckoo');
   var form = modal.querySelector('form.modal-dialog');
   var secret = form.querySelector('input#cuckoo-secret');
+  var smsg = secret.parentNode.querySelector('label .input-message');
+  var action = modal.querySelectorAll('a[href^="action:"]');
+  var more = modal.querySelector('[data-toggleable-col]');
 
+  var actions = {
+    leave: function leave() {
+      return window.close();
+    },
+    'toggle-info': function toggleInfo(el) {
+      more.classList.toggle('hidden', !more.classList.contains('hidden'));
+      if (more.classList.contains('hidden')) {
+        el.classList.replace('variation-blue', 'variation-grey');
+      } else {
+        el.classList.replace('variation-grey', 'variation-blue');
+      }
+    }
+  };
+
+  // easy-fied action toggling
+  [].forEach.call(action, function (link) {
+    link.addEventListener('click', function (e) {
+      e.preventDefault();
+      var target = e.currentTarget.href.split(':')[1];
+      if (actions[target]) actions[target](e.currentTarget);
+    });
+  });
+
+  // appends a message to the input label
+  function handleInvalidInput() {
+    var message = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : "";
+
+    smsg.textContent = message;
+  }
+
+  // validates the user input / processes the input
   form.addEventListener('submit', function (e) {
     e.preventDefault();
-    console.log('lets verify that key.');
+    var value = secret.value;
+    if (!value.length) {
+      handleInvalidInput('You have to enter a key. This field is empty.');
+    } else {
+      handleInvalidInput('This is not a good key.');
+    }
   });
 });
 //# sourceMappingURL=secret.js.map
