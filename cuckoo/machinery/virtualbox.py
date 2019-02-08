f5b7268db3ec2e2deb9eb2431812e4a4e13e1a6c
Malware Utkonos <utkonos@users.noreply.github.com>
2019-02-07 23:51:20 -0500

    Update virtualbox.py

--- cuckoo/machinery/virtualbox.py
+++ cuckoo/machinery/virtualbox.py
@@ -358,7 +358,7 @@ class VirtualBox(Machinery):
             )
 
         # VirtualBox version 4, 5, and 6.
-        if output.startswith("5") output.startswith("6"):
+        if output.startswith("5") or output.startswith("6"):
             dumpcmd = "dumpvmcore"
         else:
             dumpcmd = "dumpguestcore"
