d21554f159d2c12f8750051aab40d9b9b8ba1531
Ants Madisson <ants@cert.ee>
2019-02-28 13:25:54 +0200

    block public file download

--- cuckoo/web/utils.py
+++ cuckoo/web/utils.py
@@ -132,3 +132,14 @@ def normalize_task(task):
             os.path.basename(task["target"])
         )
     return task
+
+def get_client_ip(request):
+    try:
+        x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+        if x_forwarded_for:
+            ip = x_forwarded_for.split(',')[0]
+        else:
+            ip = request.META.get('REMOTE_ADDR')
+        return ip
+    except Exception as e:
+        return None
