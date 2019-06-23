08e506b4212802cf146acefa7dca41b736687dd1
muhzii <airomyst517@gmail.com>
2019-06-23 16:42:28 +0200

    Add agent runner script for Android guests

---
+++ cuckoo/data/agent/android-agent.sh
@@ -0,0 +1,8 @@
+# Copyright (C) 2019 Muhammed Ziad <airomyst517@gmail.com>
+# This file is part of Cuckoo Sandbox - http://www.cuckoosandbox.org
+# See the file 'docs/LICENSE' for copying permission.
+
+export TMPDIR=/data/local/tmp
+cd $TMPDIR
+
+./usr/bin/python3 ./agent.py
