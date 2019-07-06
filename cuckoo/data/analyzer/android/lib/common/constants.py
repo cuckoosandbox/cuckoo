1190ce0fe99f5986e8dbb4f8f81ae3d14ff160ac
muhzii <airomyst517@gmail.com>
2019-07-06 13:10:44 +0200

    delete constants.py

--- cuckoo/data/analyzer/android/lib/common/constants.py
+++
@@ -1,7 +0,0 @@
-# Copyright (C) 2014-2019 Cuckoo Foundation.
-# This file is part of Cuckoo Sandbox - http://www.cuckoosandbox.org
-# See the file 'docs/LICENSE' for copying permission.
-# Originally contributed by Check Point Software Technologies, Ltd.
-
-ROOT = "/data/local/tmp"
-CONFIG_PATH = "config/"
