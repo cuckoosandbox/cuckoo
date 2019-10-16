004df6012c39e3bd52375497e38f2fae865425ad
muhzii <airomyst517@gmail.com>
2019-10-16 23:48:41 +0200

    Update avd tests

--- tests/test_avd.py
+++ tests/test_avd.py
@@ -31,13 +31,13 @@ class TestAvd(object):
         self.m.set_options(Config("avd"))
 
     def test_stop_no_emu_labels(self):
-        self.m._emulators = mock.MagicMock(return_value={})
+        self.m._emulator_labels = {}
         with pytest.raises(CuckooMachineError) as e:
             self.m.stop("cuckoo")
         e.match("Trying to stop a machine that wasn't started")
 
     def test_stop_success(self):
-        self.m._emulators = {"cuckoo": "emu_label"}
+        self.m._emulator_labels = {"cuckoo": "emulator-1337"}
 
         with mock.patch("cuckoo.machinery.avd.subprocess.Popen") as p:
             proc = mock.MagicMock()
@@ -48,14 +48,15 @@ class TestAvd(object):
             self.m.stop("cuckoo")
 
         p.assert_called_once_with([
-            config("avd:avd:adb_path"),
-            "-s", "emu_label",
+            "sudo", config("avd:avd:adb_path"),
+            "-s", "emulator-1337",
             "emu", "kill"
             ], stdout=subprocess.PIPE, stderr=subprocess.PIPE
         )
+        assert self.m._emulator_labels == {}
 
     def test_stop_failure(self):
-        self.m._emulators = {"cuckoo": "emu_label"}
+        self.m._emulator_labels = {"cuckoo": "emulator-1337"}
 
         with mock.patch("cuckoo.machinery.avd.subprocess.Popen") as p:
             proc = mock.MagicMock()
@@ -65,52 +66,17 @@ class TestAvd(object):
 
             with pytest.raises(CuckooMachineError) as e:
                 self.m.stop("cuckoo")
+
         e.match("Emulator failed to stop the machine")
 
-    def test_start_headless(self):
-        self.m._emulators = {}
-        self.m._wait_status_ready = mock.MagicMock()
-
-        class named_machine(object):
-            name = "cuckoo"
-
-        self.m.db.view_machine_by_label.return_value = named_machine()
-        self.m.set_options(Dictionary({
-            "avd": Dictionary({
-                "mode": "headless"
-            })
-        }))
-
-        with mock.patch("cuckoo.machinery.avd.subprocess.Popen") as subprocess_popen:
-            proc = mock.MagicMock()
-            proc.poll.return_value = None
-            proc.communicate.return_value = "", ""
-            subprocess_popen.return_value = proc
-
-            with mock.patch("cuckoo.machinery.avd.socket.socket") as s:
-                emu_sock = mock.MagicMock()
-                emu_sock.fileno.return_value = sys.stdout.fileno()
-                emu_sock.recv.return_value = 1337
-
-                s.return_value = mock.MagicMock()
-                s.return_value.fileno.return_value = sys.stdout.fileno()
-                s.return_value.accept.return_value = [emu_sock, None]
-
-                self.m.start("cuckoo", None)
-
-        assert subprocess_popen.call_count == 1
-        assert s.return_value.accept.call_count == 1
-        assert emu_sock.recv.call_count == 1
-        assert self.m._emulators <= set(['cuckoo', 'emulator-1337'])
-        assert set(["-no-audio", "-no-window"]) <= set(subprocess_popen.call_args[0][0])
-
-    def test_start_snapshot(self):
-        self.m._emulators = {}
+    def test_start_snapshot_headless(self):
+        self.m._emulator_labels = {}
         self.m._wait_status_ready = mock.MagicMock()
 
         class named_machine(object):
             name = "cuckoo"
             label = "cuckoo"
+            options = ["headless"]
             snapshot = "cuckoo_snapshot"
 
         self.m.db.view_machine_by_label.return_value = named_machine()
@@ -136,5 +102,6 @@ class TestAvd(object):
         assert subprocess_popen.call_count == 1
         assert s.return_value.accept.call_count == 1
         assert emu_sock.recv.call_count == 1
-        assert self.m._emulators <= set(['cuckoo', 'emulator-1337'])
+        assert self.m._emulator_labels <= set(["cuckoo", "emulator-1337"])
+        assert set(["-no-audio", "-no-window"]) <= set(subprocess_popen.call_args[0][0])
         assert set(["-snapshot", "cuckoo_snapshot"]) <= set(subprocess_popen.call_args[0][0])
