ef86bcd41a2c343f43348705cbe7a8dd6f7e6bdc
Malware Utkonos <utkonos@malwarolo.gy>
2020-01-08 00:31:50 -0500

    Update documentation and install reqs for cryptography library.

--- setup.py
+++ setup.py
@@ -190,6 +190,7 @@ do_setup(
         "beautifulsoup4==4.5.3",
         "chardet==2.3.0",
         "click==6.6",
+        "cryptography",
         "django==1.8.4",
         "django_extensions==1.6.7",
         "dpkt==1.8.7",
