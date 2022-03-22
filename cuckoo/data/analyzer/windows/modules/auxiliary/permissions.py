1ec61faaf1083e4d675490dfa5d6f3c1ff6725ea
cccs-kevin <59843993+cccs-kevin@users.noreply.github.com>
2022-03-22 11:38:03 -0400

    Fixing usage of timeouts

--- cuckoo/data/analyzer/windows/modules/auxiliary/permissions.py
+++ cuckoo/data/analyzer/windows/modules/auxiliary/permissions.py
@@ -1,5 +1,6 @@
 import logging
 from subprocess import call, STARTUPINFO, STARTF_USESHOWWINDOW
+from threading import Thread
 
 from lib.common.abstracts import Auxiliary
 
@@ -23,12 +24,20 @@ class Permissions(Auxiliary):
             # First add a non-inherited permission for Admin Read+Execute
             # icacls <location> /grant:r "BUILTIN\Administrators:(OI)(CI)(RX)" "BUILTIN\\Administrators:(RX)" /t /c /q
             modify_admin_params = ["icacls", location, "/grant:r", "BUILTIN\\Administrators:(OI)(CI)(RX)", "BUILTIN\\Administrators:(RX)", "/t", "/c", "/q"]
-            call(modify_admin_params, startupinfo=self.startupinfo, timeout=15)
+            t1 = Thread(target=call, args=(modify_admin_params,), kwargs={"startupinfo": self.startupinfo})
+            t1.start()
+            t1.join(timeout=15)
+            if t1.is_alive():
+                log.warning("'Modify admin' call was unable to complete in 15 seconds")
 
             # Then remove all inherited permissions so that only SYSTEM has Write access
             # icacls <location> /inheritancelevel:r /t /c /q
             inheritance_params = ["icacls", location, "/inheritancelevel:r", "/t", "/c", "/q"]
-            call(inheritance_params, startupinfo=self.startupinfo, timeout=15)
+            t2 = Thread(target=call, args=(,), kwargs={"pinfo})
+            t2.start()
+            t2.join(timeout=15)
+            if t2.is_alive():
+                log.warning("'Inheritance' call was unable to complete in 15 seconds")
 
     def __init__(self, options={}, analyzer=None):
         Auxiliary.__init__(self, options, analyzer)
